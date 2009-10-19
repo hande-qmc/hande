de60fc0def4b1f1bd46d90e7c20848cffb51431d
James Spencer <jspencer@tycpc19.cmth.ph.ic.ac.uk>
2009-10-19 21:34:03 +0100

    intro notes..

--- hubbard.f90
+++ hubbard.f90
@@ -1,3 +1,21 @@
 module hubbard
 
+use kpoints
+use system
+
+implicit none
+
+type(kpoint), allocatable :: basis_fns(:)
+
+contains
+
+    subroutine init_basis_fns()
+    ! Produce the basis functions.  The number of basis functions is
+    ! equal to the number of sites in the crystal cell (ie the number
+    ! of k-points used to sample the FBZ of the primitive cell).
+    ! From the cell parameters and the "tilt" used (if any) generate
+    ! the list of wavevectors and hence the kinetic energy associated
+    ! with each basis function (one per wavevector).
+    end subroutine init_basis_fns()
+
 end module hubbard
