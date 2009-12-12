09415611e28238ac7273dd1e3f00539c46af6f1b
James Spencer <jss43@obex.ch.cam.ac.uk>
2009-12-12 16:20:17 +0000

    Procedure to obtain information about how a matrix is distributed by blacs.

--- lib/parallel.F90
+++ lib/parallel.F90
@@ -17,9 +17,31 @@ integer :: iproc
 ! Number of processors.
 integer :: nprocs
 
+! Processor grid dimensions (for use with scalapack and blacs)
+integer :: nproc_cols, nproc_rows
+
 ! True if the processor is the root (i.e. iproc==0) processor.
 ! In particular, output should only be performed on the parent processor.
 logical :: parent
+
+! blacs and scalapack split a matrix up into n x n blocks which are then
+! distributed around the processors in a cyclic fashion.
+! The block size is critical to performance.  64 seems to be a good value (see
+! scalapack documentation).
+integer, parameter :: block_size = 64
+
+! Type for storing information about a processor as used in blacs and scalapack.
+type blacs_info
+    ! Location of the processor within the grid.
+    integer :: procx, procy
+    ! Number of rows and columns of the global matrix stored on the processor.
+    integer :: nrows, ncols
+    ! blacs and scalapack use a 9 element integer array as a description of how
+    ! a matrix is distributed throughout the processor grid.
+    ! Store two descriptors: desca refers to the matrix and descz to the output
+    ! eigenvector matrix.
+    integer :: my_desca(9), my_descz(9)
+end type blacs_info
 
 contains
 
@@ -59,5 +81,65 @@ contains
 #endif
 
     end subroutine end_parallel
+
+    function get_blacs_info(matrix_size) result(my_blacs_info)
+
+        ! In:
+        !    matrix_size: leading dimension of the square array to be
+        !                 distributed amongst the processor mesh.
+        ! Returns:
+        !    my_blacs_info: derived type containing information about the
+        !                   processor within the blacs mesh.
+        !                   In serial mode a dummy result is returned containing
+        !                   the appropriate information apart from the
+        !                   descriptor arrays, which are set to zero.
+
+        type(blacs_info) :: my_blacs_info
+        integer, intent(in) :: matrix_size
+        integer :: context
+        integer :: i, j, k
+        integer :: numroc ! scalapack function 
+        integer :: procy, procx, nrows, ncols
+        integer :: desca(9), descz(9)
+        integer :: ierr
+
+        ! Set processor grid dimensions.
+        ! Square is good.  Make it as "square" as possible.
+        i = int(sqrt(real(nprocs,8)))
+        do j = i, 1, -1
+            k = nprocs/j
+            if (j*k == nprocs) then
+                nproc_cols = j
+                nproc_rows = k
+                exit
+            end if
+        end do
+
+#ifdef _PARALLEL
+        ! Initialise a single BLACS context.
+        call blacs_get(0, 0, context)
+        ! Initialise processor grid, nproc_rows x nproc_cols
+        call blacs_gridinit(context, 'Row-major', nproc_rows, nproc_cols)
+
+        ! Get the information about this processor: what is its coordinate
+        ! (procx, procy) in the processor grid?
+        call blacs_gridinfo(context, nproc_rows, nproc_cols, procx, procy)
+
+        ! Find how many rows and columns are owned by the processor.
+        nrows = numroc(matrix_size, block_size, procx, 0, nproc_rows)
+        ncols = numroc(matrix_size, block_size, procy, 0, nproc_cols)
+
+        ! Initialise the descriptor vectors needed for scalapack procedures.
+        call descinit(desca, matrix_size, matrix_size, block_size, block_size, 0, 0, context, nrows, ierr)
+        call descinit(descz, matrix_size, matrix_size, block_size, block_size, 0, 0, context, nrows, ierr)
+
+        my_blacs_info = blacs_info(procx, procy, nrows, ncols, desca, descz)
+#else
+        desca = 0
+        descz = 0
+        my_blacs_info = blacs_info(0, 0, 1, 1, desca, descz)
+#endif
+
+    end function get_blacs_info
 
 end module parallel
