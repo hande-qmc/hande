4a0cde1716f136096548296a85b6ec1e99df6062
James Spencer <james@mini-pintsize.(none)>
2010-02-28 22:10:12 +0000

    Arrays to store walker info.

--- src/fciqmc_data.f90
+++ src/fciqmc_data.f90
@@ -6,4 +6,20 @@ implicit none
 ! timestep
 real(dp) :: tau
 
+! Walker information: main list.
+! a) determinants
+integer(i0), allocatable :: walker_dets(:,:) ! (basis_length, walker_length)
+! b) walker population
+integer, allocatable :: walker_population(:) ! (walker_length)
+! c) Diagonal matrix elements, K_ii.  Storing them avoids recalculation.
+! K_ii = < D_i | H | D_i > - E_0, where E_0 = <D_0 | H | D_0> and |D_0> is the
+! reference determinant.
+real(dp), allocatable :: walker_energies(:)
+
+! Walker information: spawned list.
+! a) determinants
+integer(i0), allocatable :: spawned_walker_dets(:,:) ! (basis_length, walker_length)
+! b) walker population
+integer, allocatable :: spawned_walker_population(:) ! (walker_length)
+
 end module fciqmc_data
