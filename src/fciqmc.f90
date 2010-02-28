d18d35e1ff09b8b394572b520097da59a6d4c08e
James Spencer <james@mini-pintsize.(none)>
2010-02-28 22:11:11 +0000

    Layout for fciqmc module.

---
+++ src/fciqmc.f90
@@ -0,0 +1,53 @@
+module fciqmc
+
+use fciqmc_data
+
+contains
+
+    subroutine init_fciqmc()
+
+        ! Allocate walker lists.
+
+        ! Set initial walker population.
+
+    end subroutine init_fciqmc
+
+    subroutine end_fciqmc()
+
+        ! Deallocate walker lists.
+
+    end subroutine end_fciqmc
+
+    subroutine do_fciqmc()
+
+        ! Main fciqmc routine.
+
+        do ! report loops
+
+            do ! cycles
+
+                do ! loop over dets
+
+                    ! decode
+
+                    do ! loop over particles
+                        
+                        ! spawn
+
+                    end do
+
+                    ! clone
+
+                end do
+
+                ! annihilate
+
+            end do
+
+            ! report
+
+        end do
+
+    end subroutine do_fciqmc
+
+end module fciqmc
