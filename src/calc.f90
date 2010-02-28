7ce577330e8653001e5a7687c225736e97c54bd2
James Spencer <james@mini-pintsize.(none)>
2010-02-28 22:09:20 +0000

    Reorder statements for clarity.  Add flag for fciqmc calc.

--- src/calc.f90
+++ src/calc.f90
@@ -5,17 +5,22 @@ use parallel, only: blacs_info
 
 implicit none
 
+!--- Calculation info ---
+
 ! Flags for doing exact and/or Lanczos diagonalisation.
-logical :: t_exact = .false., t_lanczos = .false.
+logical :: t_exact = .false., t_lanczos = .false., t_fciqmc = .false.
 
 ! Ms of determinants.  If not set, then all possible values of Ms are considered
-! in FCI.
+! in FCI.  FCIQMC assumes ms = 0 if not given in input.
 integer :: ms_in = huge(1)
 
-! Symmetry block of determinants.
-! Ignored for real space formulation.
-! Refers to a wavevector in momentum space formulation.
+! Symmetry block of determinants.  Ignored for real space formulation.  Refers
+! to a wavevector in momentum space formulation.  If not set, then determinants
+! of all possible momenta are considered in FCI.  FCIQMC assumes determinants
+! with 0 momentum are to be considered if not specified in input.
 integer :: sym_in = huge(1)
+
+!--- Info for FCI calculations ---
 
 ! Hamiltonian matrix.  Clearly the scaling of the memory demands with system
 ! size is horrendous.  We only store one symmetry block at a time though.
@@ -26,10 +31,6 @@ real(dp), allocatable :: hamil(:,:) ! (ndets, ndets)
 ! If true, then the eigenvectors are found during exact diagonalisation as well
 ! as the eigenvalues.  Doing this is substantially more expensive.
 logical :: find_eigenvectors = .false.
-
-! If true then the non-zero elements of the Hamiltonian matrix are written to hamiltonian_file.
-logical :: write_hamiltonian = .false.
-character(255) :: hamiltonian_file = 'HAMIL'
 
 ! BLACS info for diagonalisation
 type(blacs_info) :: proc_blacs_info
@@ -45,5 +46,9 @@ integer, parameter :: distribute_cols = 2
 
 ! Flag which stores which distribution mode is in use.
 integer :: distribute = distribute_off
+
+! If true then the non-zero elements of the Hamiltonian matrix are written to hamiltonian_file.
+logical :: write_hamiltonian = .false.
+character(255) :: hamiltonian_file = 'HAMIL'
 
 end module calc
