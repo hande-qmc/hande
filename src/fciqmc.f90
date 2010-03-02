5c1775553bfad0d6f14db09f48eaccfa7f6d5930
James Spencer <j.spencer@imperial.ac.uk>
2010-03-02 12:26:00 +0000

    Basis outline of fciqmc algorithm.

--- src/fciqmc.f90
+++ src/fciqmc.f90
@@ -1,42 +1,109 @@
 module fciqmc
 
 use fciqmc_data
+implicit none
 
 contains
 
     subroutine init_fciqmc()
 
-        ! Allocate walker lists.
+        use basis, only: basis_length
+        use determinants, only: encode_det, set_spin_polarisation
+        use hamiltonian, only: get_hmatel_k
+        use system, only: nel
+        use hubbard_k
+
+        integer :: ierr
+        integer :: i, occ_list(nel)
+        real(dp) :: hmatel
+
+        ! Allocate main walker lists.
+        allocate(walker_dets(basis_length,walker_length), stat=ierr)
+        allocate(walker_population(walker_length), stat=ierr)
+        allocate(walker_energies(walker_length), stat=ierr)
+
+        ! Allocate spawned walker lists.
+        allocate(spawned_walker_dets(basis_length,spawned_walker_length), stat=ierr)
+        allocate(spawned_walker_population(spawned_walker_length), stat=ierr)
+
+        ! Just for testing...
+        ! Ms should become an input option.
+        call set_spin_polarisation(0)
 
         ! Set initial walker population.
+        tot_walkers = 1
+        walker_population(tot_walkers) = 1
+        ! Arbitrary choice initially just for testing!
+        forall (i=1:nel) occ_list(i) = i
+        walker_dets(:,tot_walkers) = encode_det(occ_list)
+
+        walker_energies(tot_walkers) = 0.0_dp
+        
+        write (6,*) 'energy',walker_energies(tot_walkers)
 
     end subroutine init_fciqmc
 
     subroutine end_fciqmc()
 
         ! Deallocate walker lists.
+        
+        integer :: ierr
+
+        deallocate(walker_dets, stat=ierr)
+        deallocate(walker_population, stat=ierr)
+        deallocate(walker_energies, stat=ierr)
+        deallocate(spawned_walker_dets, stat=ierr)
+        deallocate(spawned_walker_population, stat=ierr)
 
     end subroutine end_fciqmc
 
     subroutine do_fciqmc()
 
-        ! Main fciqmc routine.
+        use basis, only: basis_length
+        use death, only: stochastic_death
+        use determinants, only: det_info, decode_det_spinocc_spinunocc
+        use spawning, only: spawn_hub_k
+        use system, only: nel, nalpha, nbeta, nvirt_alpha, nvirt_beta
 
-        do ! report loops
+        integer :: ierr
+        integer :: idet, ireport, icycles, iparticles
+        integer :: spos ! Current index in spawning array.
+        type(det_info) :: cdet
 
-            do ! cycles
+        ! Allocate det_info components.
+        allocate(cdet%f(basis_length), stat=ierr)
+        allocate(cdet%occ_list(nel), stat=ierr)
+        allocate(cdet%occ_list_alpha(nalpha), stat=ierr)
+        allocate(cdet%occ_list_beta(nbeta), stat=ierr)
+        allocate(cdet%unocc_list_alpha(nvirt_alpha), stat=ierr)
+        allocate(cdet%unocc_list_beta(nvirt_beta), stat=ierr)
 
-                do ! loop over dets
+        ! Main fciqmc loop.
 
-                    ! decode
+        do ireport = 1, nreport
 
-                    do ! loop over particles
+            do icycles = 1, ncycles
+
+                ! Reset the position in the spawning array to be the first slot.
+                spos = 1
+
+                do idet = 1, tot_walkers ! loop over walkers/dets
+
+                    write (6,*) 'idet',idet,spos,walker_population(idet)
+
+                    cdet%f = walker_dets(:,idet)
+
+                    call decode_det_spinocc_spinunocc(cdet%f, cdet)
+
+                    do iparticles = 1, abs(walker_population(idet))
                         
                         ! spawn
+                        call spawn_hub_k(spos, cdet)
 
                     end do
 
-                    ! clone
+                    ! Clone or die.
+                    call stochastic_death(idet)
 
                 end do
 
