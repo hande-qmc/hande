6d4a8e6f3a60bcf5093de4ac7e88c399a0f5fb03
James Spencer <j.spencer@imperial.ac.uk>
2010-04-01 14:31:30 +0100

    extract.py now only finds the unique data by default.
    No need to have E0, shift and E0+shift all the time and the line-wrapping
    when running the test suite was annoying me.

--- tools/extract.py
+++ tools/extract.py
@@ -1,38 +1,11 @@
 #!/usr/bin/env python
-'''Extract data from the output of a hubbard_fciqmc calculation.
+'''Usage: extract.py filename
 
-Usage: extract.py filename
-'''
+Extract data from the output of a hubbard_fciqmc calculation.'''
 
+import optparse
 import re
 import sys
-
-class DataElement(object):
-    '''Holder for a data element which is searched for in an output file.'''
-    def __init__(self, name, regex, line_index, regex_flags=None):
-        '''Initialise a DataElement object.
-
-name: name of data item.  Used in output table.  Should not contain spaces for testcode compatibility.
-regex: regular expression used to find the data element.  re.match is used, so the regular expression should start from the beginning of the line.
-line_index: the index of the data value in the line, when split by spaces.
-regex_flags (optional): flags to use in the compiled regular expression.
-'''
-        self.name = name
-        if regex_flags:
-            self.regex = re.compile(regex, regex_flags)
-        else:
-            self.regex = re.compile(regex)
-        self.line_index = line_index
-        self.value = None
-    def fmt(self, padding=0):
-        '''Return a format string which will hold both the name and value of the data item without truncation.
-
-padding (optional integer): amount of space to add to format string.
-'''
-        if self.value:
-            return '%%-%is' % (max(len(str(self.value)), len(str(self.name)))+padding)
-        else:
-            return '%%-%is' % (len(str(self.name))+padding)
 
 # If using python <2.5, any doesn't exist. Define our own.
 try:
@@ -49,7 +22,36 @@ Return True if bool(x) is True for any x in the iterable.
         return False
 
 # data elements to find.
-data_elements = [
+class DataElement(object):
+    '''Holder for a data element which is searched for in an output file.'''
+    def __init__(self, name, regex, line_index, regex_flags=None, verbose=False):
+        '''Initialise a DataElement object.
+
+name: name of data item.  Used in output table.  Should not contain spaces for testcode compatibility.
+regex: regular expression used to find the data element.  re.match is used, so the regular expression should start from the beginning of the line.
+line_index: the index of the data value in the line, when split by spaces.
+regex_flags (optional): flags to use in the compiled regular expression.
+verbose (optional): flag which if true denotes data should only be included in verbose output.  Dafeult: False.
+'''
+        self.name = name
+        if regex_flags:
+            self.regex = re.compile(regex, regex_flags)
+        else:
+            self.regex = re.compile(regex)
+        self.line_index = line_index
+        self.value = None
+        self.verbose = verbose
+    def fmt(self, padding=0):
+        '''Return a format string which will hold both the name and value of the data item without truncation.
+
+padding (optional integer): amount of space to add to format string.
+'''
+        if self.value:
+            return '%%-%is' % (max(len(str(self.value)), len(str(self.name)))+padding)
+        else:
+            return '%%-%is' % (len(str(self.name))+padding)
+
+ALL_DATA_ELEMENTS = [
         DataElement('exact_ground_state','^ *Exact ground state:', -1),
         DataElement('lanczos_ground_state','^ *Lanczos ground state:', -1),
         DataElement('E0','^ *E0 = <D0|H|D0> =', -1),
@@ -57,10 +59,23 @@ data_elements = [
         DataElement('av_shift','^ av\. shift =', -1),
         DataElement('final_proj_E','^ final proj. energy =', -1),
         DataElement('av_proj_E','^ av\. proj. energy =', -1),
-        DataElement('E0+final_shift','^ E0 \+ shift =', -1),
-        DataElement('E0+av_shift','^ E0 \+ av\. shift =', -1),
-        DataElement('E0+final_proj_E','^ E0 \+ av\. proj\. energy =', -1),
+        DataElement('E0+final_shift','^ E0 \+ shift =', -1, verbose=True),
+        DataElement('E0+av_shift','^ E0 \+ av\. shift =', -1, verbose=True),
+        DataElement('E0+final_proj_E','^ E0 \+ av\. proj\. energy =', -1, verbose=True),
     ]
+
+def parse_options(args):
+    '''Parse command line options.'''
+
+    parser = optparse.OptionParser(usage = __doc__)
+    parser.add_option('-v', '--verbose', action='store_true', default=False, help='Show all extracted data (including data items which are sums of other data items. Default: %default.')
+    (options, filename) = parser.parse_args(args)
+
+    if len(filename) != 1:
+        parser.print_help()
+        sys.exit(1)
+
+    return options, filename[0]
 
 def extract(data_elements, filename):
     '''Extract the data from the file specified.
@@ -98,8 +113,9 @@ filename: file to examine.
     return None
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
-        extract(data_elements, filename)
+    (options, filename) = parse_options(sys.argv[1:])
+    if options.verbose:
+        data_elements = ALL_DATA_ELEMENTS
     else:
-        print __doc__,
+        data_elements = [d for d in ALL_DATA_ELEMENTS if not d.verbose]
+    extract(data_elements, filename)
