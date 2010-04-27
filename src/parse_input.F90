944663b2a417a950829013bd7342570acdaec503
James Spencer <j.spencer@imperial.ac.uk>
2010-04-27 14:15:02 +0100

    Add comments to parse_input.
    This is to make it easier for a script to examine what keywords are set
    by the input file.

--- src/parse_input.F90
+++ src/parse_input.F90
@@ -58,7 +58,7 @@ contains
         if (parent) write (6,'(a14,/,1X,13("-"),/)') 'Input options'
         call input_options(echo_lines=parent, skip_blank_lines=.true.)
 
-        do
+        do ! loop over lines in input file.
             call read_line(eof)
             if (eof) exit
             call readu(w)
@@ -199,7 +199,7 @@ contains
             case default
                 call report('Keyword '//trim(w)//' not recognized.', .true.)
             end select
-        end do
+        end do ! end reading of input.
 
         if (ios.gt.0) then
             if (parent) write (6,*) 'Problem reading input.'
