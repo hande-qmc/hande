4710e46556b065490330f52c2f4d17074a3ea38d
James Spencer <j.spencer@imperial.ac.uk>
2010-05-15 15:29:42 +0200

    Add utilities to communicate with running jobs at a set time.

---
+++ tools/send_softexit.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+#
+# Usage:
+# send_softexit.py [options] walltime &
+# 
+# This is a version of watchdog which will work on computers without a
+# queueing system or using a queueing system other than Torque.
+#
+# send_softexit must run in the same working directory as the NECI calculation.
+# 
+# Care must be taken that the send_softexit is terminated when the job finishes
+# (rather than waiting for the send_softexit to finish!).  send_softexit
+# does, however, listen out for the interrupt signal.  Recommended use in a
+# script is:
+#     
+# send_softexit.py [options] $PBS_JOBID &
+# [Job commands]
+# killall -2 send_softexit.py
+# 
+# or
+# 
+# send_softexit.py [options] $PBS_JOBID &
+# send_softexit_ps=$!
+# [Job commands]
+# kill -2 $send_softexit_ps
+# 
+# The latter should be used if multiple calculations are run on one computer.
+
+__author__ = 'James Spencer'
+
+from optparse import OptionParser, OptionValueError
+import signal
+import sys
+import time
+
+def signal_handler(signal, frame):
+    '''Capture signal and leave quietly.'''
+    print 'Signal has been caught.  Bye!'
+    sys.exit()
+
+def job_cleanup():
+    '''Perform specified operations to perform when the job is approaching the end of its walltime.'''
+    f = open('FCIQMC.COMM','w')
+    f.write('softexit')
+    f.close()
+
+
+def parse_timer(t):
+    '''Parse a time string and return the corresponding (integer) number of seconds.
+    
+t is a string eiher containing the number of seconds or in the format hh:mm:ss.'''
+    if ':' in t:
+        # assume hhmmss format
+        t = [int(s) for s in t.split(':')]
+        return (t[0]*60+t[1])*60+t[2]
+    else:
+        # already in seconds: just need to convert to integer.
+        return int(t)
+
+def parse_options(my_args):
+    '''Parse command line options.  Return the amount of sleep time.'''
+    parser = OptionParser(usage='''send_softexit.py [options] walltime
+
+Monitor a running job and write SOFTEXIT to CHANGEVARS in the current directory
+when the elapsed time gets to within a specified amount of the walltime allowed
+for the job.
+
+The walltime and grace period can be given either in seconds or in the format
+hh:mm:ss.''')
+    parser.add_option('-g','--grace',default='0',help='Amount of time before the walltime expires that SOFTEXIT is sent.  Default: %default.')
+    (options,args) = parser.parse_args(my_args)
+    if len(args) != 1:
+        if len(args) == 0:
+            print 'Must specify walltime.'
+        else:
+            print 'Do not understand options specified: %s.' % (' '.join(args))
+        parser.print_help()
+        sys.exit(1)
+    else:
+        walltime = parse_timer(args[0])
+        grace = parse_timer(options.grace)
+        sleep_time = walltime - grace
+    return sleep_time
+
+def main(sleep_time):
+    print 'send_softexit sleeping for %is.' % (sleep_time)
+    time.sleep(sleep_time)
+    job_cleanup()
+    sys.exit()
+
+if __name__ == '__main__':
+    sleep_time = parse_options(sys.argv[1:])
+    signal.signal(signal.SIGINT,signal_handler) # Listen out for Ctrl-C.
+    main(sleep_time)
---
+++ tools/watchdog.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+'''Monitor a running job and run a cleanup function when the elapsed time gets to within a specified amount of the walltime allowed for the job.
+
+Usage:
+watchdog.py [options] job_id &
+
+This should be called from your PBS script, so that watchdog is automatically running in the correct working directory.
+
+job_id needs to be the full job id, as used internally by torque (e.g. 47345.master.beowulf.cluster on tardis).
+
+Care must be taken that the watchdog is terminated when the job finishes (rather than PBS waiting for the watchdog to finish!).  
+watchdog does, however, listen out for the interrupt signal.  Recommended use in a PBS script is:
+    
+watchdog.py [options] $PBS_JOBID &
+[Job commands]
+killall -2 watchdog.py '''
+
+__author__ = 'James Spencer'
+
+import signal,sys,time
+from optparse import OptionParser,OptionValueError
+import PBSQuery
+
+exit_time = 900
+
+def signal_handler(signal,frame):
+    '''Capture interupt signal and leave quietly.'''
+    print 'Interrupt signal has been caught.  Bye!'
+    sys.exit()
+
+def job_cleanup():
+    '''Perform specified operations to perform when the job is approaching the end of its walltime.'''
+    f = open('FCIQMC.COMM','w')
+    f.write('softexit')
+    f.close()
+
+def hhmmss_to_seconds(hhmmss):
+    '''Convert time in the string format of hh:mm:ss to seconds.'''
+    t = map(int,hhmmss.split(':'))
+    return (t[0]*60+t[1])*60+t[2]
+
+def parse_options(my_args):
+    '''Parse command line options.'''
+    parser = OptionParser(usage=__doc__)
+    parser.add_option('-e','--exit-time',type='float',default=exit_time,help='Amount of time (in seconds) before the walltime runs out at which the job_cleanup function is called to terminate the job. Default=%defaults.')
+    (options,args) = parser.parse_args(my_args)
+    if len(args) != 1:
+        if len(args) == 0:
+            print 'Must specify the job id.'
+        else:
+            print 'Do not understand options specified: %s.' % (' '.join(args))
+        parser.print_help()
+        sys.exit()
+    else:
+        job_id = args[0]
+    return (options,job_id)
+
+def main(job_id):
+    '''Monitor a job_id and run job_cleanup when the elapsed_time is within exit_time seconds of the wall_time.'''
+    p = PBSQuery.PBSQuery()
+    if not p.getjob(job_id):
+        raise Exception,'invalid job id %s.' % job_id
+    job = p.getjob(job_id)
+    wall_time = hhmmss_to_seconds(job[job_id]['Resource_List.walltime'])
+    try:
+        sleep_time = wall_time-options.exit_time
+    except NameError:
+        # Try to use the default in case watchdog is being used as a module.
+        sleep_time = wall_time-exit_time
+    print 'Watchdog sleeping for %i' % (sleep_time)
+    time.sleep(sleep_time)
+    job_cleanup()
+    sys.exit
+
+if __name__ == '__main__':
+    (options,job_id) = parse_options(sys.argv[1:])
+    signal.signal(signal.SIGINT,signal_handler) # Listen out for Ctrl-C.
+    main(job_id)
