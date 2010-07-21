b54f11bf7a6a26578720ce6de3dc5a96060e2977
James Spencer <j.spencer@imperial.ac.uk>
2010-07-21 16:01:13 +0100

    Routines to check if allocation/deallocation was successful.

---
+++ lib/checking.f90
@@ -0,0 +1,56 @@
+module checking
+
+! Helper routines for testing whether an event was a success.
+
+implicit none
+
+contains
+
+      
+    subroutine check_allocate(array_name, array_size, ierr)
+
+        use utils, only: int_fmt
+        use parallel, only: iproc, nprocs
+        use errors, only: stop_all
+
+        character(*), intent(in) :: array_name
+        integer, intent(in) :: array_size, ierr
+
+        if (ierr /= 0) then
+            ! Error in allocating array.
+            if (nprocs > 1) then
+                write (6,'(1X,a25,1X,a,1X,a9,1X,'//int_fmt(array_size,1)//',a12,'//int_fmt(iproc,1)//')') &
+                    'Error in allocating array',trim(array_name),'with size',array_size,'on processor',iproc
+            else
+                write (6,'(1X,a25,1X,a,1X,a9,1X,'//int_fmt(array_size,1)//')') &
+                    'Error in allocating array',trim(array_name),'with size',array_size
+            end if
+            write (6,*) 'ierr',ierr
+            call stop_all('check_allocate','Allocation error')
+        end if
+
+    end subroutine check_allocate
+
+    subroutine check_deallocate(array_name, ierr)
+
+        use utils, only: int_fmt
+        use parallel, only: iproc, nprocs
+        use errors, only: stop_all
+
+        character(*), intent(in) :: array_name
+        integer, intent(in) :: ierr
+
+        if (ierr /= 0) then
+            ! Error in deallocating array.
+            if (nprocs > 1) then
+                write (6,'(1X,a25,1X,a,1X,a12,'//int_fmt(iproc,1)//')') &
+                    'Error in allocating array',trim(array_name),'on processor',iproc
+            else
+                write (6,'(1X,a25,1X,a,1X,a9)') 'Error in deallocating array',trim(array_name)
+            end if
+            call stop_all('check_deallocate','Allocation error')
+        end if
+
+    end subroutine check_deallocate
+
+end module checking
