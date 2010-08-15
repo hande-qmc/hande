b38ec019a040a067a878fcda9d3842f16389bbe7
James Spencer <j.spencer@imperial.ac.uk>
2010-08-16 00:23:35 +0100

    Create list of orbitals connected in real space.

--- src/hubbard_real.f90
+++ src/hubbard_real.f90
@@ -30,6 +30,12 @@ integer(i0), allocatable :: tmat(:,:) ! (basis_length, nbasis)
 ! excitations.
 integer(i0), allocatable :: connected_orbs(:,:) ! (basis_length, nbasis)
 
+! connected_sites(:,i) contains the list of sites connected to site i (ie is the
+! decoded/non-bit list form of connected_orbs).
+! If connected_orbs(j,i) is 0 then it means there are fewer than 2ndim sites
+! that are connected to i that are not a periodic image of i.
+integer, allocatable :: connected_sites(:,:) ! (2ndim, nbasis)
+
 ! True if any site is its own periodic image.
 ! This is the case if one dimension (or more) has only one site per crystal
 ! cell.  If so then the an orbital can incur a kinetic interaction with itself.
@@ -53,13 +59,14 @@ contains
         ! Initialise real space Hubbard model: find and store the matrix
         ! elements < i | T | j > where i and j are real space basis functions.
 
-        use basis, only: nbasis, bit_lookup, basis_length, basis_fns, set_orb
+        use basis, only: nbasis, bit_lookup, basis_lookup, basis_length, basis_fns, set_orb
+        use determinants, only: decode_det
         use system, only: lattice, ndim, box_length
         use bit_utils
         use checking, only: check_allocate
         use errors, only: stop_all
 
-        integer :: i, j, k, ierr, pos, ind, ivec
+        integer :: i, j, k, ierr, pos, ind, ivec, v
         integer :: r(ndim)
 
         integer :: lvecs(ndim, 3**ndim)
@@ -70,6 +77,8 @@ contains
         call check_allocate('tmat',basis_length*nbasis,ierr)
         allocate(connected_orbs(basis_length,nbasis), stat=ierr)
         call check_allocate('connected_orbs',basis_length*nbasis,ierr)
+        allocate(connected_sites(2*ndim,nbasis), stat=ierr)
+        call check_allocate('connected_sites',basis_length*2*ndim,ierr)
 
         tmat = 0
         connected_orbs = 0
@@ -111,38 +120,48 @@ contains
                             ! Nearest neighbours within unit cell.
                             call set_orb(tmat(:,i),j)
                             call set_orb(tmat(:,i+1),j+1)
+                        else if (.not. finite_cluster) then ! if we want inf. lattice
                             ! Nearest neighbours due to periodic boundaries.
-                        else if(.not. finite_cluster) then ! if we want inf. lattice
                             call set_orb(tmat(:,j),i)
                             call set_orb(tmat(:,j+1),i+1) 
                             ! else we just want connections to other cells to
                             ! stay as 0 
                         end if        
                        
-                        ! if we only want a discrete molecule and the lattice
+                        ! If we only want a discrete molecule and the lattice
                         ! vector connecting the 2 sites is the 0-vector then the
                         ! 2 sites are connected in a unit cell and thus are
                         ! actually connected. (If they "connect" accross cell
                         ! boundaries then they are not connected for a single
                         ! molecule).
-                        if( (finite_cluster .and. all(lvecs(:,ivec) == 0)) .or. &
+                        if ( (finite_cluster .and. all(lvecs(:,ivec) == 0)) .or. &
                              .not. finite_cluster) then
-                            call set_orb(connected_orbs(:,i),j)
-                            call set_orb(connected_orbs(:,i+1),j+1)                      
-                            call set_orb(connected_orbs(:,j),i)
-                            call set_orb(connected_orbs(:,j+1),i+1)
+                            if (i /= j) then
+                                ! connected_orbs does not contain self-connections 
+                                ! due to the periodic boundary conditions.
+                                call set_orb(connected_orbs(:,i),j)
+                                call set_orb(connected_orbs(:,i+1),j+1)                      
+                                call set_orb(connected_orbs(:,j),i)
+                                call set_orb(connected_orbs(:,j+1),i+1)
+                            end if
                         end if
- 
                     end if
                 end do
             end do
         end do
 
-        ! Information for the random excitation generators.
+        ! Decode connected_orbs to store list of connections.
+        connected_sites = 0
         do i = 1, nbasis
-            pos = bit_lookup(1,i)
-            ind = bit_lookup(2,i)
-            connected_orbs(ind,i) = ibclr(connected_orbs(ind,i),pos)
+            v = 0
+            do ind = 1, basis_length
+                do pos = 0, i0_end
+                    if (btest(connected_orbs(ind,i), pos)) then
+                        v = v + 1
+                        connected_sites(v, i) = basis_lookup(pos, ind)
+                    end if
+                end do
+            end do
         end do
 
     end subroutine init_real_space_hub
@@ -158,6 +177,14 @@ contains
         if (allocated(tmat)) then
             deallocate(tmat, stat=ierr)
             call check_deallocate('tmat',ierr)
+        end if
+        if (allocated(connected_orbs)) then
+            deallocate(connected_orbs, stat=ierr)
+            call check_deallocate('connected_orbs',ierr)
+        end if
+        if (allocated(connected_sites)) then
+            deallocate(connected_sites, stat=ierr)
+            call check_deallocate('connected_sites',ierr)
         end if
 
     end subroutine end_real_space_hub
