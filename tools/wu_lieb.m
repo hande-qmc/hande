d0f3187d10875134854bf4ac82ab9a60d7569290
James Spencer <j.spencer@imperial.ac.uk>
2011-07-22 18:48:53 +0100

    Mathematica function for exact ground state of 1D hubbard model.
    Courtesy of Wu and Lieb.

---
+++ tools/wu_lieb.m
@@ -0,0 +1,3 @@
+(* See, e.g. http://dx.doi.org/10.1016/S0378-4371(02)01785-5 *)
+(* E0 gives the exact ground state energy for a 1D Hubbard model at half-filling fir a given U and N sites in the crystal cell *)
+E0[N_,U_] := -4 N NIntegrate[BesselJ[0,w]BesselJ[1,w]/(w(1+Exp[w U/2])), {w,0,Infinity}]
