0b20725f5764ba22db31c46ce62fc47447ab5218
James Spencer <j.spencer@imperial.ac.uk>
2011-10-20 18:36:05 +0100

    Function for finding index of lower triangular arrays.

--- lib/local/utils.f90
+++ lib/local/utils.f90
@@ -183,4 +183,32 @@ contains
 
     end subroutine get_unique_filename
 
+    function tri_ind(i,j) result(indx)
+
+        ! Find the index corresponding to the (i,j)-th element of a lower
+        ! triangular array.  This maps:
+        !
+        !   1,1                   1
+        !   2,1 2,2               2  3
+        !   3,1 3,2 3,3       to  4  5  6
+        !   4,1 4,2 4,3 4,4       7  8  9 10
+        !
+        ! WARNING:
+        ! We assume that i >= j.  It is the programmer's responsibility to check
+        ! this and re-order i and j if required.
+        !
+        ! In:
+        !    i: (1-indexed) row index
+        !    j: (1-indexed) column index
+        ! Returns:
+        !    A combined (1-indexed) index for the corresponding element in
+        !    a lower triangular array.
+
+        integer :: indx
+        integer, intent(in) :: i, j
+
+        indx = ((i-1)*i)/2 + j
+
+    end function tri_ind
+
 end module utils
