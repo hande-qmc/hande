681f84976ada7c4ce85deee9d905ed1697f41bfb
James Spencer <j.spencer@imperial.ac.uk>
2011-10-23 13:25:42 +0100

    2D allocatable real base type.

--- lib/local/base_types.f90
+++ lib/local/base_types.f90
@@ -7,8 +7,13 @@ use const
 implicit none
 
 ! Allocatable 1D array.  Useful for creating triangular arrays etc.
-type alloc_r1d
+type alloc_rp1d
     real(p), allocatable :: v(:)
+end type
+
+! Allocatable 2D array.  Useful for creating triangular spin arrays etc.
+type alloc_rp2d
+    real(p), allocatable :: v(:,:)
 end type
 
 end module base_types
