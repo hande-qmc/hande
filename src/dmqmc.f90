30156b0f70f1d0531aa10891c0b558c8c260de32
Nick Blunt <nicholas.blunt08@imperial.ac.uk>
2011-11-16 19:30:16 +0000

    Main dmqmc loop added, and also a new file with dmqmc estimators in.

---
+++ src/dmqmc.f90
@@ -0,0 +1,237 @@
+module dmqmc
+
+! Main loop for performing DMQMC calculations, similar to the
+! file fciqmc.f90 which carries out the main loop in FCIQMC
+! calculations.
+
+use fciqmc_data
+use proc_pointers
+implicit none
+
+contains
+
+    subroutine dmqmc_main()
+
+        ! Wrapper around dmqmc calculation procedures to set the appropriate procedures
+        ! that are to be called for the current dmqmc calculation.
+
+        use system, only: system_type, hub_k, hub_real, heisenberg, hub_k_coulomb, hubt
+        use hamiltonian, only: slater_condon0_hub_k, slater_condon0_hub_real
+        use hamiltonian, only: diagonal_element_heisenberg
+        use heisenberg_estimators, only: update_proj_energy_heisenberg_basic
+        use dmqmc_estimators, only: call_dmqmc_estimators
+        use determinants, only: decode_det_spinocc_spinunocc, decode_det_occ
+        use energy_evaluation, only: update_proj_energy_hub_k, update_proj_hfs_hub_k
+        use energy_evaluation, only: update_proj_energy_hub_real
+        use spawning, only: spawn_hub_k, spawn_hub_real, create_spawned_particle_density_matrix
+        use spawning, only: spawn_heisenberg
+        use calc, only: dmqmc_calc
+
+        use excitations, only: enumerate_all_excitations_hub_k, enumerate_all_excitations_hub_real
+
+        real(dp) :: hub_matel
+
+        ! set function pointers
+        select case(system_type)
+        case (hub_k)
+            decoder_ptr => decode_det_spinocc_spinunocc
+            update_proj_energy_ptr => update_proj_energy_hub_k
+            spawner_ptr => spawn_hub_k
+            sc0_ptr => slater_condon0_hub_k
+            hub_matel = hub_k_coulomb
+        case (hub_real)
+            decoder_ptr => decode_det_occ
+            update_proj_energy_ptr => update_proj_energy_hub_real
+            spawner_ptr => spawn_hub_real
+            sc0_ptr => slater_condon0_hub_real
+            hub_matel = hubt
+        case (heisenberg)
+            ! Only need occupied orbitals list, as for the real Hubbard case.
+            decoder_ptr => decode_det_occ
+            update_dmqmc_energy_ptr => call_dmqmc_estimators
+            spawner_ptr => spawn_heisenberg
+            sc0_ptr => diagonal_element_heisenberg
+        end select
+
+        create_spawned_particle_dm_ptr => create_spawned_particle_density_matrix
+
+        call do_dmqmc()
+
+    end subroutine dmqmc_main
+
+    subroutine do_dmqmc()
+
+        ! Run DMQMC calculation. We run from a beta=0 to a value of beta
+        ! specified by the user and then repeat this main loop beta_loops
+        ! times, to accumulate statistics for each value for beta.
+
+        use parallel 
+        use annihilation, only: direct_annihilation
+        use basis, only: basis_length, bit_lookup, nbasis
+        use death, only: stochastic_death
+        use determinants, only: det_info, alloc_det_info, dealloc_det_info
+        use dmqmc_procedures, only: random_distribution_heisenberg
+        use dmqmc_estimators, only: update_dmqmc_estimators
+        use energy_evaluation, only: update_energy_estimators
+        use excitations, only: excit
+        use fciqmc_common
+        use fciqmc_restart, only: dump_restart
+        use interact, only: fciqmc_interact
+        use system, only: nel
+
+        integer :: i, idet, ireport, icycle, iparticle
+        integer :: beta_cycle, nparticles_old(sampling_size)
+        type(det_info) :: cdet1, cdet2
+        integer :: nspawned, nattempts, ndeath
+        type(excit) :: connection
+        integer :: bit_pos, bit_element
+        integer :: walker_pop_1, walker_pop_2, spawning_end
+        integer :: beta_index
+        logical :: soft_exit
+        real :: t1, t2
+
+        ! Allocate det_info components. We need two cdet objects
+        ! for each 'end' which may be spawned from in the DMQMC algorithm.
+        call alloc_det_info(cdet1)
+        call alloc_det_info(cdet2)
+
+        ! Main DMQMC loop.
+        if (parent) call write_fciqmc_report_header()
+        call initial_fciqmc_status()
+        ! Initialise timer.
+        call cpu_time(t1)
+
+        do beta_cycle = 1, beta_loops
+
+        ! Reset the current position in the spawning array to be the
+        ! slot preceding the first slot.
+        spawning_head = spawning_block_start
+        tot_walkers = 0
+        nparticles = 0
+ 
+        ! Need to place psips randomly along the diagonal at the
+        ! start of every iteration. Pick orbitals randomly, each
+        ! with equal probability, so that when electrons are placed
+        ! one these orbitals they will have the correct spin and symmetry.
+        call random_distribution_heisenberg()
+
+        call direct_annihilation()
+
+        nparticles_old = dmqmc_npsips
+
+        do ireport = 1, nreport
+
+            ! Zero report cycle quantities.
+            rspawn = 0.0_p
+            beta_index = 0
+            trace = 0
+            total_trace = 0
+            thermal_energy = 0
+            total_thermal_energy = 0
+
+            do icycle = 1, ncycles
+                beta_index = beta_index + 1
+                spawning_head = spawning_block_start
+
+                ! Number of spawning attempts that will be made.
+                ! Each particle and each end gets to attempt to 
+                ! spawn onto a connected determinant and a chance 
+                ! to die/clone.
+                nattempts = 4*nparticles(1)
+
+                ! Reset death counter
+                ndeath = 0
+
+                do idet = 1, tot_walkers ! loop over walkers/dets
+                    cdet1%f = walker_dets(:basis_length,idet)
+                    cdet2%f = walker_dets((basis_length+1):(2*basis_length),idet)
+                    cdet1%idet = idet
+                    cdet2%idet = idet
+
+                    ! Decode and store the the relevant information for
+                    ! both bitstrings. Both of these bitstrings are required
+                    ! to refer to the correct element in the density matrix.
+                    call decoder_ptr(cdet1%f, cdet1)
+                    call decoder_ptr(cdet2%f, cdet2)
+
+                    ! Add the contributions from this particular density matrix
+                    ! element to the quantites required to calculate the desired 
+                    ! properties, such as the trace of the density matrix.
+                    call update_dmqmc_energy_ptr(idet, beta_index)
+
+                    do iparticle = 1, abs(walker_population(1,idet))
+                        ! Spawn from the first end.
+                        spawning_end = 1
+                        ! Attempt to spawn.
+                        call spawner_ptr(cdet1, walker_population(1,idet), nspawned, connection)
+                        ! Spawn if attempt was successful.
+                        if (nspawned /= 0) then
+                            call create_spawned_particle_dm_ptr(cdet1%f, cdet2%f, connection, nspawned, spawning_end)
+                        end if
+
+                        ! Now attempt to spawn from the second end.
+                        spawning_end = 2
+                        call spawner_ptr(cdet2, walker_population(1,idet), nspawned, connection)
+                        if (nspawned /= 0) then
+                            call create_spawned_particle_dm_ptr(cdet2%f, cdet1%f, connection, nspawned, spawning_end)
+                        end if
+                    end do
+
+                    ! Clone or die.
+                    ! We have contirbutions to the clone/death step from both ends of the
+                    ! current walker. We do both of these at once by using
+                    ! (walker_energies(1,idet)+walker_energies(2,idet))/2 as the correct
+                    ! energy. 
+                    call stochastic_death((walker_energies(1,idet)+walker_energies(2,idet))/2, &
+                             walker_population(1,idet), nparticles(1), ndeath)
+                end do
+
+                ! Add the spawning rate (for the processor) to the running
+                ! total.
+                rspawn = rspawn + spawning_rate(ndeath, nattempts)
+
+                ! Perform the annihilation step where the spawned walker list is merged with
+                ! the main walker list, and walkers of opposite sign on the same sites are
+                ! annihilated.
+                call direct_annihilation()
+
+            end do
+            
+            ! Update the shift and desired thermal quantites.
+            call update_dmqmc_estimators(ireport, nparticles_old, beta_index)
+
+            call cpu_time(t2)
+
+            ! t1 was the time at the previous iteration, t2 the current time.
+            ! t2-t1 is thus the time taken by this report loop.
+            if (parent) call write_fciqmc_report(ireport, nparticles_old(1), t2-t1)
+
+            ! cpu_time outputs an elapsed time, so update the reference timer.
+            t1 = t2
+
+            call fciqmc_interact(ireport, soft_exit)
+            if (soft_exit) exit
+
+        end do
+
+        end do
+
+        if (parent) then
+            call write_fciqmc_final(ireport)
+            write (6,'()')
+        end if
+
+        call load_balancing_report()
+
+        if (dump_restart_file) call dump_restart(mc_cycles_done+ncycles*nreport, nparticles_old(1))
+
+        call dealloc_det_info(cdet1)
+        call dealloc_det_info(cdet2)
+
+        do icycle=0,(ncycles*nreport)-1
+            print *, icycle, thermal_energy(icycle)/trace(icycle)
+        end do
+
+    end subroutine do_dmqmc
+
+end module dmqmc
---
+++ src/dmqmc_estimators.f90
@@ -0,0 +1,95 @@
+module dmqmc_estimators
+
+use const
+
+implicit none
+
+contains
+
+   subroutine update_dmqmc_estimators(ireport, ntot_particles_old)
+
+        ! Update the shift and average the shift and estimators
+
+        ! Should be called every report loop in an DMQMC calculation.
+
+        ! In:
+        !    ireport: index of the current report loop.
+        ! Inout:
+        !    ntot_particles_old: total number (across all processors) of
+        !        particles in the simulation at end of the previous report loop.
+        !        Returns the current total number of particles for use in the
+        !        next report loop.
+
+        use fciqmc_data, only: nparticles, sampling_size, target_particles, rspawn
+        use fciqmc_data, only: shift, av_shift, vary_shift, start_vary_shift
+        use fciqmc_data, only: nreport, ncycles, trace, thermal_energy
+        use fciqmc_data, only: total_trace, total_thermal_energy
+        use parallel
+
+        integer, intent(in) :: ireport
+        integer, intent(inout) :: ntot_particles_old(sampling_size)
+
+#ifdef PARALLEL
+        real(dp) :: ir(sampling_size+1+(2*ncycles)), ir_sum(sampling_size+1+(2*ncycles))
+        integer :: ntot_particles(sampling_size), ierr
+
+            ! Need to sum the number of particles and other quantites over all processors.
+            ir(1:sampling_size) = nparticles
+            ir(sampling_size+1) = rspawn
+            ir(sampling_size+2:sampling_size+1+ncycles) = thermal_energy
+            ir(sampling_size+2+ncycles:sampling_size+1+(2*ncycles)) = trace
+            call mpi_allreduce(ir, ir_sum, size(ir), MPI_REAL8, MPI_SUM, MPI_COMM_WORLD, ierr)
+            ntot_particles = nint(ir_sum(1:sampling_size))
+            rspawn = ir_sum(sampling_size+1)
+            if (parent) then
+                total_trace = total_trace + ir_sum(sampling_size+2:sampling_size+1+ncycles)
+                total_thermal_energy = total_thermal_energy + ir_sum(sampling_size+2+ncycles:&
+                                                                     sampling_size+1+(2*ncycles))
+            end if
+
+#else
+            if (vary_shift) call update_shift(ntot_particles_old(1), nparticles(1), ncycles)
+            ntot_particles_old = nparticles
+            if (nparticles(1) > target_particles .and. .not.vary_shift) then
+                vary_shift = .true.
+                start_vary_shift = ireport
+            end if
+#endif
+
+            rspawn = rspawn/(ncycles*nprocs)
+
+   end subroutine update_dmqmc_estimators   
+
+   subroutine call_dmqmc_estimators(idet, beta_index)
+
+       use basis, only: basis_length, total_basis_length
+       use basis, only: bit_lookup
+       use excitations, only: get_excitation, excit
+       use fciqmc_data, only: walker_dets, walker_population, trace
+       use fciqmc_data, only: walker_energies, thermal_energy, H00
+       use hubbard_real, only: connected_orbs
+       use system, only: J_coupling
+
+       integer, intent(in) :: idet, beta_index
+       type(excit) :: excitation
+       integer :: bit_element, bit_position
+
+       excitation = get_excitation(walker_dets(:basis_length,idet), &
+                        walker_dets((1+basis_length):total_basis_length,idet))
+
+       if (excitation%nexcit == 0) then
+           thermal_energy(beta_index)=thermal_energy(beta_index) + &
+                             (walker_energies(1,idet)+H00)*walker_population(1,idet)
+           trace(beta_index)=trace(beta_index) + walker_population(1,idet)
+       else if (excitation%nexcit == 1) then
+           bit_position = bit_lookup(1,excitation%from_orb(1))
+           bit_element = bit_lookup(2,excitation%from_orb(1))
+           if (btest(connected_orbs(bit_element, excitation%to_orb(1)), bit_position)) &
+                 thermal_energy(beta_index)=thermal_energy(beta_index) - &
+                             (2.0*J_coupling*walker_population(1,idet))
+       end if
+
+
+   end subroutine call_dmqmc_estimators
+
+end module dmqmc_estimators
