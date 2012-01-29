hande-qmc/hande

commit d7b07536ab1d74496b43a3228b2b76bddeab10c1

Importance sampling added for DMQMC.

src/proc_pointers.f90
@@ -21,11 +21,12 @@ abstract interface
         implicit none
         integer, intent(in) :: idet
     end subroutine i_update_proj_energy
-    subroutine i_update_dmqmc_estimators(idet,excitation)
-        import :: excit
+    subroutine i_update_dmqmc_estimators(idet,excitation,walker_pop)
+        import :: excit, p
         implicit none
         integer, intent(in) :: idet
         type(excit), intent(in) :: excitation
+        real(p) :: walker_pop
     end subroutine i_update_dmqmc_estimators
     subroutine i_spawner(d, parent_sign, nspawned, connection)
         import :: det_info, excit

src/dmqmc_estimators.F90
@@ -91,28 +91,48 @@ contains
        use calc, only: dmqmc_energy_squared, dmqmc_correlation
        use excitations, only: get_excitation, excit
        use fciqmc_data, only: walker_dets, walker_population, trace, doing_reduced_dm
+       use fciqmc_data, only: dmqmc_sampling_prob
        use proc_pointers, only: update_dmqmc_energy_ptr, update_dmqmc_stag_mag_ptr
        use proc_pointers, only: update_dmqmc_energy_squared_ptr, update_dmqmc_correlation_ptr
 
        integer, intent(in) :: idet
        type(excit) :: excitation
+       real(p) :: unweighted_walker_pop
 
        ! Get excitation.
        excitation = get_excitation(walker_dets(:basis_length,idet), &
                         walker_dets((1+basis_length):total_basis_length,idet))
 
+       ! When performing importance sampling the result is that certain excitation
+       ! levels have smaller psips populations than the true density matrix by some
+       ! factor. In these cases, we want to multiply the psip population by this factor
+       ! to calculate the contribution from these excitation levels correctly.
+       
+       ! In the case of no importance sampling, unweighted_walker_pop = walker_population(1,idet)
+       ! and so this change can be ignored.
+       unweighted_walker_pop = walker_population(1,idet)*(dmqmc_sampling_prob**(excitation%nexcit))
+
        ! If diagonal element, add to the trace.
        if (excitation%nexcit == 0) trace = trace + walker_population(1,idet)
        ! See which estimators are to be calculated, and call the corresponding procedures.
-       if (doing_dmqmc_calc(dmqmc_energy)) call update_dmqmc_energy_ptr(idet, excitation)
-       if (doing_dmqmc_calc(dmqmc_energy_squared)) call update_dmqmc_energy_squared_ptr(idet, excitation)
-       if (doing_dmqmc_calc(dmqmc_correlation)) call update_dmqmc_correlation_ptr(idet, excitation)
-       if (doing_dmqmc_calc(dmqmc_staggered_magnetisation)) call update_dmqmc_stag_mag_ptr(idet, excitation)
+       ! Energy
+       if (doing_dmqmc_calc(dmqmc_energy)) call update_dmqmc_energy_ptr&
+               &(idet, excitation, unweighted_walker_pop)
+       ! Energy squared
+       if (doing_dmqmc_calc(dmqmc_energy_squared)) call update_dmqmc_energy_squared_ptr&
+               &(idet, excitation, unweighted_walker_pop)
+       ! Spin-spin correlation function
+       if (doing_dmqmc_calc(dmqmc_correlation)) call update_dmqmc_correlation_ptr&
+               &(idet, excitation, unweighted_walker_pop)
+       ! Staggered magnetisation
+       if (doing_dmqmc_calc(dmqmc_staggered_magnetisation)) call update_dmqmc_stag_mag_ptr&
+               &(idet, excitation, unweighted_walker_pop)
+       ! Reduced density matrix
        if (doing_reduced_dm) call update_reduced_density_matrix(idet)
 
    end subroutine call_dmqmc_estimators
 
-   subroutine dmqmc_energy_heisenberg(idet, excitation)
+   subroutine dmqmc_energy_heisenberg(idet, excitation, walker_pop)
 
        ! For the Heisenberg model only.
        ! Add the contribution from the current density matrix element
@@ -127,7 +147,7 @@ contains
        use basis, only: basis_length, total_basis_length
        use basis, only: bit_lookup
        use excitations, only: excit
-       use fciqmc_data, only: walker_dets, walker_population
+       use fciqmc_data, only: walker_dets
        use fciqmc_data, only: walker_data, H00
        use fciqmc_data, only: estimator_numerators, energy_index
        use hubbard_real, only: connected_orbs
@@ -135,13 +155,14 @@ contains
 
        integer, intent(in) :: idet
        type(excit), intent(in) :: excitation
+       real(p) , intent(in) :: walker_pop
        integer :: bit_element, bit_position
 
        ! If no excitation, we have a diagonal element, so add elements
        ! which involve the diagonal element of the Hamiltonian.
        if (excitation%nexcit == 0) then
            estimator_numerators(energy_index) = estimator_numerators(energy_index) + &
-                                    (walker_data(1,idet)+H00)*walker_population(1,idet)
+                                    (walker_data(1,idet)+H00)*walker_pop
        else if (excitation%nexcit == 1) then
        ! If not a diagonal element, but only a single excitation, then the corresponding
        ! Hamiltonian element may be non-zero. Calculate if the flipped spins are
@@ -150,12 +171,12 @@ contains
            bit_element = bit_lookup(2,excitation%from_orb(1))
            if (btest(connected_orbs(bit_element, excitation%to_orb(1)), bit_position)) &
                  estimator_numerators(energy_index) = estimator_numerators(energy_index) - &
-                                   (2.0*J_coupling*walker_population(1,idet))
+                                   (2.0*J_coupling*walker_pop)
        end if
 
    end subroutine dmqmc_energy_heisenberg
 
-   subroutine dmqmc_energy_squared_heisenberg(idet, excitation)
+   subroutine dmqmc_energy_squared_heisenberg(idet, excitation, walker_pop)
 
        ! For the Heisenberg model only.
        ! Add the contribution from the current density matrix element
@@ -170,7 +191,7 @@ contains
        use basis, only: basis_length, total_basis_length
        use basis, only: bit_lookup
        use excitations, only: excit
-       use fciqmc_data, only: walker_dets, walker_population
+       use fciqmc_data, only: walker_dets
        use fciqmc_data, only: walker_data, H00
        use fciqmc_data, only: estimator_numerators, energy_squared_index
        use hubbard_real, only: connected_orbs, next_nearest_orbs
@@ -178,6 +199,7 @@ contains
 
        integer, intent(in) :: idet
        type(excit), intent(in) :: excitation
+       real(p) , intent(in) :: walker_pop
        integer :: bit_element1, bit_position1, bit_element2, bit_position2
        real(p) :: sum_H1_H2, J_coupling_squared
 
@@ -252,11 +274,11 @@ contains
        end if
 
        estimator_numerators(energy_squared_index) = estimator_numerators(energy_squared_index) + &
-               sum_H1_H2*walker_population(1,idet)
+               sum_H1_H2*walker_pop
 
    end subroutine dmqmc_energy_squared_heisenberg
 
-   subroutine dmqmc_energy_hub_real(idet, excitation)
+   subroutine dmqmc_energy_hub_real(idet, excitation, walker_pop)
 
        ! For the Heisenberg model only.
        ! Add the contribution from the current density matrix element
@@ -270,32 +292,33 @@ contains
 
        use basis, only: basis_length, total_basis_length
        use excitations, only: excit
-       use fciqmc_data, only: walker_dets, walker_population
+       use fciqmc_data, only: walker_dets
        use fciqmc_data, only: walker_data, H00
        use fciqmc_data, only: estimator_numerators, energy_index
        use hamiltonian, only: slater_condon1_hub_real
 
        integer, intent(in) :: idet
        type(excit), intent(in) :: excitation
+       real(p) , intent(in) :: walker_pop
        real(p) :: hmatel
 
        ! If no excitation, we have a diagonal element, so add elements
        ! which involve the diagonal element of the Hamiltonian.
        if (excitation%nexcit == 0) then
            estimator_numerators(energy_index) = estimator_numerators(energy_index) + &
-                                 (walker_data(1,idet)+H00)*walker_population(1,idet)
+                                 (walker_data(1,idet)+H00)*walker_pop
        else if (excitation%nexcit == 1) then
        ! If not a diagonal element, but only a single excitation, then the corresponding
        ! Hamiltonian element may be non-zero. Calculate if the flipped spins are
        ! neighbours on the lattice, and if so, add the contirbution from this site.
            hmatel = slater_condon1_hub_real(excitation%from_orb(1), excitation%to_orb(1), excitation%perm)
            estimator_numerators(energy_index) = estimator_numerators(energy_index) + &
-                                 (hmatel*walker_population(1,idet))
+                                 (hmatel*walker_pop)
        end if
 
    end subroutine dmqmc_energy_hub_real
 
-   subroutine dmqmc_correlation_function_heisenberg(idet, excitation)
+   subroutine dmqmc_correlation_function_heisenberg(idet, excitation, walker_pop)
 
        ! For the Heisenberg model only.
        ! Add the contribution from the current density matrix element
@@ -311,7 +334,7 @@ contains
        use basis, only: bit_lookup
        use bit_utils, only: count_set_bits
        use excitations, only: excit
-       use fciqmc_data, only: walker_dets, walker_population
+       use fciqmc_data, only: walker_dets
        use fciqmc_data, only: walker_data, H00, correlation_mask
        use fciqmc_data, only: estimator_numerators, correlation_index
        use hubbard_real, only: connected_orbs
@@ -319,6 +342,7 @@ contains
 
        integer, intent(in) :: idet
        type(excit), intent(in) :: excitation
+       real(p) , intent(in) :: walker_pop
        integer(i0) :: f(basis_length)
        integer :: bit_element1, bit_position1, bit_element2, bit_position2
        integer :: sign_factor
@@ -338,7 +362,7 @@ contains
            sign_factor = (mod(sign_factor+1,2)*2)-1
            ! Hence sign_factor can be used to find the matrix element, as used below.
            estimator_numerators(correlation_index) = estimator_numerators(correlation_index) + &
-                                    (real(sign_factor*walker_population(1,idet))/4)
+                                    (sign_factor*(walker_pop/4))
        else if (excitation%nexcit == 1) then
        ! If not a diagonal element, but only a single excitation, then the corresponding
        ! matrix element will be 1/2 if and only if the two sites which are flipped are
@@ -350,12 +374,12 @@ contains
            bit_element2 = bit_lookup(2,excitation%to_orb(1))
            if (btest(correlation_mask(bit_element1), bit_position1).and.btest(correlation_mask(bit_element2), bit_position2)) &
                  estimator_numerators(correlation_index) = estimator_numerators(correlation_index) + &
-                                   (real(walker_population(1,idet))/2)
+                                   (walker_pop/2)
        end if
 
    end subroutine dmqmc_correlation_function_heisenberg
 
-   subroutine dmqmc_stag_mag_heisenberg(idet, excitation)
+   subroutine dmqmc_stag_mag_heisenberg(idet, excitation, walker_pop)
 
        ! For the Heisenberg model only.
        ! Add the contribution from the current density matrix element
@@ -371,12 +395,13 @@ contains
        use bit_utils, only: count_set_bits
        use determinants, only: lattice_mask
        use excitations, only: excit
-       use fciqmc_data, only: walker_dets, walker_population
+       use fciqmc_data, only: walker_dets
        use fciqmc_data, only: estimator_numerators, staggered_mag_index
        use system, only: nel, nsites
 
        integer, intent(in) :: idet
        type(excit), intent(in) :: excitation
+       real(p) , intent(in) :: walker_pop
        integer :: bit_element1, bit_position1, bit_element2, bit_position2
        integer(i0) :: f(basis_length)
        integer :: n_up_plus
@@ -420,7 +445,7 @@ contains
        end if
 
        estimator_numerators(staggered_mag_index) = estimator_numerators(staggered_mag_index) + &
-                                  (real(total_sum)/real(nsites**2))*walker_population(1,idet)
+                                  (real(total_sum)/real(nsites**2))*walker_pop
 
    end subroutine dmqmc_stag_mag_heisenberg
 

src/fciqmc_data.f90
@@ -246,6 +246,15 @@ integer :: energy_squared_index = 0
 integer :: correlation_index = 0
 integer :: staggered_mag_index = 0
 
+! If this logical is true then the program runs the DMQMC algorithm with
+! importance sampling.
+! dmqmc_sampling_prob stores the factor by which the probabilities of
+! spawning to a larger excitation are reduced by. So, when spawning from
+! a diagonal element to a element with one excitation, the probability
+! of spawning is reduced by a factor dmqmc_sampling_prob.
+logical :: dmqmc_weighted_sampling
+real(p) :: dmqmc_sampling_prob = 1.0_p
+
 ! If true, then the reduced density matrix will be calulated
 ! for the subsystem A specified by the user.
 logical :: doing_reduced_dm = .false.

src/parse_input.F90
@@ -203,6 +203,9 @@ contains
             print *, correlation_sites
             case('DMQMC_STAGGERED_MAGNETISATION')
                 dmqmc_calc_type = dmqmc_calc_type + dmqmc_staggered_magnetisation
+            case('DMQMC_WEIGHTED_SAMPLING')
+                call readf(dmqmc_sampling_prob)
+                dmqmc_weighted_sampling = .true.
             ! Calculate a reduced density matrix
             case('REDUCED_DENSITY_MATRIX')
                 doing_reduced_dm = .true.
@@ -588,6 +591,8 @@ contains
         call mpi_bcast(vary_shift_from_proje, 1, mpi_logical, 0, mpi_comm_world, ierr)
         call mpi_bcast(target_particles, 1, mpi_integer8, 0, mpi_comm_world, ierr)
         call mpi_bcast(doing_reduced_dm, 1, mpi_logical, 0, mpi_comm_world, ierr)
+        call mpi_bcast(dmqmc_weighted_sampling, 1, mpi_logical, 0, mpi_comm_world, ierr)
+        call mpi_bcast(dmqmc_sampling_prob, 1, mpi_preal, 0, mpi_comm_world, ierr)
         option_set = .false.
         if (parent) option_set = allocated(subsystem_A_list)
         call mpi_bcast(option_set, 1, mpi_logical, 0, mpi_comm_world, ierr)

src/qmc.F90
@@ -595,6 +595,8 @@ contains
                 create_spawned_particle_dm_ptr => create_spawned_particle_truncated_density_matrix
             else if (doing_calc(hdmqmc_calc)) then
                 create_spawned_particle_dm_ptr => create_spawned_particle_half_density_matrix
+            else if (dmqmc_weighted_sampling) then
+                create_spawned_particle_dm_ptr => create_spawned_particle_weighted_density_matrix
             else
                 create_spawned_particle_dm_ptr => create_spawned_particle_density_matrix
             end if

src/spawning.F90
@@ -1346,6 +1346,114 @@ contains
 
     end subroutine create_spawned_particle_density_matrix
 
+    subroutine create_spawned_particle_weighted_density_matrix(f1, f2, connection, nspawn, spawning_end)
+
+        ! Create a spawned walker in the spawned walkers lists.
+        ! The current position in the spawning array is updated.
+
+        ! This routine will reject certain new psips from being created
+        ! with a certain probability in order to apply importance sampling 
+        ! to the density matrix. Alternatively, it can also increase the number
+        ! of psips created if the associated probability is greater than 1.
+
+        ! In:
+        !    f1: bitstring corresponding to the end which is currently
+        !         being spawned from.
+        !    f2: bitstring corresponding to the 'inactive' end, which
+        !         was not involved in the current spawning step.
+        !    connection: excitation connecting the current determinant to its
+        !        offspring.  Note that the perm field is not used.
+        !    nspawn: the (signed) number of particles to create on the
+        !        spawned determinant.
+        !    spawning_end: Specifies which 'end' we are spawning from
+        !        currently, ie, if the elements of the density matrix are
+        !        \rho_{i,j}, are we spawning from the i end, 1, or the j end, 2.
+
+        ! Note that particle type, ie specifying hellman-feynman properties, is
+        ! not currently considered, as the density matrix formulation (hopefully!)
+        ! won't require it.
+
+        use basis, only: basis_length, total_basis_length
+        use dSFMT_interface, only:  genrand_real2
+        use excitations, only: excit, create_excited_det, get_excitation_level
+        use fciqmc_data, only: spawned_walkers, spawning_head, dmqmc_sampling_prob
+        use fciqmc_data, only: spawned_parent, spawned_pop, truncate_space, truncation_level
+        use hashing
+        use parallel, only: iproc, nprocs
+
+        integer(i0), intent(in) :: f1(basis_length), f2(basis_length)
+        integer, intent(in) :: nspawn
+        integer, intent(in) :: spawning_end
+
+        type(excit), intent(in) :: connection
+        integer(i0) :: f_new(basis_length)
+        integer(i0) :: f_new_tot(total_basis_length)
+        integer :: excitation_level_old, excitation_level_new
+        integer :: nspawn_new
+        real(p) :: pkeep
+
+#ifndef PARALLEL
+        integer, parameter :: iproc_spawn = 0
+#else
+        integer :: iproc_spawn
+#endif
+
+        ! Find the relative number of excitations between the two
+        ! ends of the old bitstring which was spawned from.
+        excitation_level_old = get_excitation_level(f1,f2)
+
+        ! Create bitstring of new determinant. The entire two-ended
+        ! bitstring is eventually stored in f_new_tot.
+        call create_excited_det(f1, connection, f_new)
+        f_new_tot = 0
+        if (spawning_end==1) then
+            f_new_tot(:basis_length) = f_new
+            f_new_tot((basis_length+1):(total_basis_length)) = f2
+        else
+            f_new_tot(:basis_length) = f2
+            f_new_tot((basis_length+1):(total_basis_length)) = f_new
+        end if
+   
+        ! Find the relative number of excitations between the two
+        ! ends of the new bitstring which is spawned to.
+        excitation_level_new = get_excitation_level(f_new,f2)
+
+        ! For each excitation level, there is a factor of dmqmc_sampling_prob
+        ! contributing to the probability of keeping the new psip. If spawning
+        ! to higher excitations, pkeep < 1, if spawning to lower excitations
+        ! (towards the diagonal) then pkeep > 1.
+        pkeep = dmqmc_sampling_prob**(excitation_level_old-excitation_level_new)
+
+        ! Find how many psips will be spawned for certain.
+        nspawn_new = nspawn*int(pkeep)
+        ! If pkeep is non-integer, we want to spawn nspawn more psips some of the
+        ! time, as given by the probability below.
+        if ((pkeep-int(pkeep)) > genrand_real2()) nspawn_new = nspawn_new + nspawn
+
+        ! If the new psips are to be kept, add them to the spawned walkers list.
+        if (abs(nspawn_new) > 0) then
+
+#ifdef PARALLEL
+            ! (Extra credit for parallel calculations)
+            ! Need to determine which processor the spawned walker should be sent
+            ! to.  This communication is done during the annihilation process, after
+            ! all spawning and death has occured..
+            iproc_spawn = modulo(murmurhash_bit_string(f_new_tot, (total_basis_length)), nprocs)
+#endif
+
+            ! Move to the next position in the spawning array.
+            spawning_head(iproc_spawn) = spawning_head(iproc_spawn) + 1
+
+            ! Set info in spawning array.
+            ! Zero it as not all fields are set.
+            spawned_walkers(:,spawning_head(iproc_spawn)) = 0
+            spawned_walkers(:(total_basis_length),spawning_head(iproc_spawn)) = f_new_tot
+            spawned_walkers((total_basis_length)+1,spawning_head(iproc_spawn)) = nspawn_new
+
+        end if
+
+    end subroutine create_spawned_particle_weighted_density_matrix
+
     subroutine create_spawned_particle_half_density_matrix(f1, f2, connection, nspawn, spawning_end)
 
         ! Create a spawned walker in the spawned walkers lists.
@@ -1427,7 +1535,6 @@ contains
         spawned_walkers((total_basis_length)+1,spawning_head(iproc_spawn)) = nspawn
 
     end subroutine create_spawned_particle_half_density_matrix
-
 
     subroutine create_spawned_particle_truncated_density_matrix(f1, f2, connection, nspawn, spawning_end)
 

commit 3a61520d67c5e6c0b0429e867c3e48468a25440e

Match intent(in) in interface for update_dmqmc procedures.
This corrects a bug (which caused compilation errors on strict
compilers) introduced in d7b07536.

src/proc_pointers.f90
@@ -26,7 +26,7 @@ abstract interface
         implicit none
         integer, intent(in) :: idet
         type(excit), intent(in) :: excitation
-        real(p) :: walker_pop
+        real(p), intent(in) :: walker_pop
     end subroutine i_update_dmqmc_estimators
     subroutine i_spawner(d, parent_sign, nspawned, connection)
         import :: det_info, excit