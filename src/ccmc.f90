b65d06df78481be3bd9ea874a7a648433cc5b927
James Spencer <j.spencer@imperial.ac.uk>
2012-02-13 10:56:33 +0000

    WIP: cluster selection

--- src/ccmc.f90
+++ src/ccmc.f90
@@ -6,7 +6,7 @@ module ccmc
 ! (especially the spawning, death and annihilation).  As a result, the structure
 ! of do_ccmc is remarkably similar to the other do_*mc routines.
 
-use const, only: p
+use const, only: p, lint
 
 implicit none
 
@@ -24,6 +24,7 @@ contains
 
         use annihilation, only: direct_annihilation
         use basis, only: basis_length, nbasis
+        use calc, only: truncation_level
         use determinants, only: det_info, alloc_det_info, dealloc_det_info
         use excitations, only: excit, get_excitation_level
         use energy_evaluation, only: update_energy_estimators
@@ -32,6 +33,7 @@ contains
         use fciqmc_restart, only: dump_restart
         use interact, only: fciqmc_interact
         use proc_pointers
+        use spawning, only: create_spawned_particle_truncated
 
         integer :: ireport, icycle
         integer(lint) :: iattempt, nattempts, nparticles_old(sampling_size)
@@ -79,20 +81,17 @@ contains
                 ! Reset death counter
                 ndeath = 0
 
-                ! Allow one spawning & death attempt for each walker on the
+                ! Allow one spawning & death attempt for each excip on the
                 ! processor.
                 do iattempt = 1, nparticles(1)
 
-                    ! TODO: select cluster size
-
-                    ! TODO: select cluster
-
-                    ! TODO: collapse cluster (return also excitation_level)
+                    ! TODO: initiator.
+                    call select_cluster(nparticles(1), cdet, excitation_level, cluster_amplitude, pcluster)
 
                     if (excitation_level <= truncation_level+2) then
 
                         ! TODO: projected estimator.
-!                        call update_proj_energy_ptr(cdet, amp)
+!                        call update_proj_energy_ptr(cdet, 1, amp)
 
                         call spawner_ccmc(cdet, cluster_amplitude, pcluster, nspawned, connection)
 
@@ -158,18 +157,117 @@ contains
 
     end subroutine do_ccmc
 
+    subroutine select_cluster(nattempts, cdet, excitation_level, amplitude, pcluster)
+
+        ! TODO: interface comments
+
+        use calc, only: truncation_level
+        use determinants, only: det_info
+        use dSFMT_interface, only: genrand_real2
+        use fciqmc_data, only: f0, D0_population, tot_walkers, &
+                               walker_population
+        use proc_pointers, only: decoder_ptr
+        use utils, only: factorial
+
+        integer(lint), intent(in) :: nattempts
+        type(det_info), intent(inout) :: cdet
+        integer, intent(out) :: excitation_level
+        real(p), intent(out) :: amplitude, pcluster
+
+        real(p) :: rand, psize
+        integer :: cluster_size, i
+        logical :: valid_cluster
+        integer :: excitor(truncation_level+1)
+
+        ! We shall accumulate the factors which comprise pcluster as we go.
+        !   pcluster = n_sel p_size p_clust
+        ! where
+        !   n_sel   is the number of cluster selections made (by this
+        !           processor?);
+        !   p_size  is the probability of choosing a cluster of that size;
+        !   p_clust is the probability of choosing a specific cluster given
+        !           the choice of size.
+
+        pcluster = nattempts
+
+        ! Select the cluster size, i.e. the number of excitors in a cluster.
+        ! For a given truncation_level, only clusters containing at most
+        ! truncation_level+1 excitors.
+        ! Following the process described by Thom in 'Initiator Stochastic
+        ! Coupled Cluster Theory' (unpublished), each size, n_s, has probability
+        ! p(n_s) = 1/2^(n_s+1), n_s=0,truncation_level and p(truncation_level+1)
+        ! is such that \sum_{n_s=0}^{truncation_level+1} p(n_s) = 1.
+        rand = genrand_real2()
+        psize = 0.0_p
+        cluster_size = -1
+        do i = 0, truncation_level
+            psize = psize + 1.0_p/2**(i+1)
+            if (rand < psize) then
+                ! Found size!
+                cluster_size = i
+                pcluster = pcluster/2**(i+1)
+                exit
+            end if
+        end do
+        ! If not set, then must be the largest possible cluster
+        if (cluster_size < 0) then
+            cluster_size = truncation_level + 1
+            pcluster = pcluster/(1.0_p - psize)
+        end if
+
+        valid_cluster = .true.
+        select case(cluster_size)
+        case(0)
+            ! Must be the reference.
+            cdet%f = f0
+            excitation_level = 0
+            amplitude = D0_population ! TODO: don't accumulate D0 over report loops
+            ! Only one cluster of this size to choose => p_clust = 1
+        case default
+            ! Select cluster from the excitors on the current processor in
+            ! a uniform manner.
+            amplitude = 1.0_p
+            do i = 1, cluster_size
+                ! Select a position between in the excitors list.
+                excitor(i) = int(genrand_real2()*tot_walkers) + 1
+                amplitude = amplitude*walker_population(1,excitor(i))
+            end do
+            ! Normalisation factor for amplitudes...
+            amplitude = amplitude/(D0_population**(cluster_size-1)) ! TODO: don't accumulate D0 over report loops
+
+            ! TODO: collapse cluster (return also excitation_level)
+
+            ! We chose excitors uniformly.
+            !  -> prob. of each excitor is 1./(number of excitors on processor).
+            ! Overall probability is the product of this times the number of
+            ! ways the cluster could have been selected, ie
+            !   n_s!/n_excitors^n_s
+            pcluster = (pcluster*factorial(cluster_size))/(tot_walkers**cluster_size)
+        end select
+
+        ! Fill in information about the cluster if required.
+        if (valid_cluster) then
+            call decoder_ptr(cdet%f, cdet)
+        else
+            ! Simply set excitation level to a too high (fake) level to avoid
+            ! this cluster being used.
+            excitation_level = huge(0)
+        end if
+
+    end subroutine select_cluster
+
     subroutine spawner_ccmc(cdet, amplitude, pcluster, nspawn, connection)
 
         ! Attempt to spawn a new particle on a connected excitor with
         ! probability
         !     \tau |<D'|H|D_s> A_s|
         !   -------------------------
-        !   p_sel p_s p_clust p_excit
+        !   n_sel p_s p_clust p_excit
         ! where |D_s> is the determinant formed by applying the excitor to the
         ! reference determinant, A_s is the amplitude and D' is the determinant
         ! formed from applying a connected excitor to the reference determinant.
-        ! See comments in TODO about p_sel, p_s and p_clust.  p_excit is the
-        ! probability of choosing D' given D_s.
+        ! See comments in select_cluster about n_sel, p_s and p_clust.  p_excit
+        ! is the probability of choosing D' given D_s.
 
         ! This is just a thin wrapper around a system-specific excitation
         ! generator and a utility function.  We need to modify the spawning
@@ -182,7 +280,7 @@ contains
         !        from.
         !    amplitude: amplitude of cluster.
         !    pcluster: Overall probabilites of selecting this cluster, ie
-        !        p_sel.p_s.p_clust.
+        !        n_sel.p_s.p_clust.
         ! Out:
         !    nspawn: number of particles spawned.  0 indicates the spawning
         !        attempt was unsuccessful.
@@ -224,17 +322,17 @@ contains
         ! with probability
         !    \tau |<D_s|H|D_s> A_s|
         !    ----------------------
-        !       p_sel p_s p_clust
+        !       n_sel p_s p_clust
         ! where |D_s> is the determinant formed by applying the excitor to the
-        ! reference determinant and A_s is the amplitude.  See comments in TODO
-        ! about the probabilities.
+        ! reference determinant and A_s is the amplitude.  See comments in
+        ! select_cluster about the probabilities.
 
         ! In:
         !    cdet: info on the current excitor (cdet) that we will spawn
         !        from.
         !    amplitude: amplitude of cluster.
         !    pcluster: Overall probabilites of selecting this cluster, ie
-        !        p_sel.p_s.p_clust.
+        !        n_sel.p_s.p_clust.
 
         use determinants, only: det_info
         use fciqmc_data, only: tau, shift, spawned_pop, H00
