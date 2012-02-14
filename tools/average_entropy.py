446edb156f16392968e0ba1bbc2074762f329025
Thomas Rogers <thomas.rogers08@imperial.ac.uk>
2012-02-14 23:25:44 +0000

    Changed average_entropy.py so that is can now calculate statistics fo concurrence.

--- tools/average_entropy.py
+++ tools/average_entropy.py
@@ -6,9 +6,10 @@ import scipy
 import scipy.stats
 
 def extract_data(data_files):
-    data = []
+    concurrence = []
+    entropy = []
     entropy_regex = '^ # Von-Neumann Entropy='
-
+    concurrence_regex = '^ # Concurrence='
     for data_file in data_files:
 
         f = open(data_file)
@@ -17,17 +18,24 @@ def extract_data(data_files):
         for line in f:
             if re.match(entropy_regex, line):
                 words = line.split()
-                entropy = float(words[3])
-                data.append(entropy)
+                entropy_i = float(words[3])
+                entropy.append(entropy_i)
+            if re.match(concurrence_regex, line):
+                words = line.split()
+                concurrence_i = float(words[2])
+                concurrence.append(concurrence_i)
         f.close()
-    return data
+    return entropy, concurrence
 
 
 
 if __name__ == '__main__':
 
     data_files = sys.argv[1:]
-    data = extract_data(data_files)
-    mean = scipy.mean(data)
-    se = scipy.stats.sem(data)
-    print "Average Von Neumann Entropy = ", mean, "    s.e. = ", se, "   beta loops = ", len(data)
+    entropy, concurrence = extract_data(data_files)
+    entropy_mean = scipy.mean(entropy)
+    entropy_se = scipy.stats.sem(entropy)
+    concurrence_mean = scipy.mean(concurrence)
+    concurrence_se = scipy.stats.sem(concurrence)
+    if len(entropy) > 0: print "Average Von Neumann Entropy = ", entropy_mean, "    s.e. = ", entropy_se, "   beta loops = ", len(entropy)
+    if len(concurrence) > 0: print "Average concurrence = ", concurrence_mean, "   s.e. = ", concurrence_se, "   beta loops = ", len(concurrence) 
