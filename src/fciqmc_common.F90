hande-qmc/hande

commit c23a04bcf6ea061648c0d34c5705900014828426

Change update_proj_energy interface.
Pass in determinant info and population rather than the position in the
walker_* arrays.  This means we can call the update_proj_energy_*
procedures for objects which don't exist in the arrays (e.g. multiple
clusters in coupled cluster).

src/fciqmc_common.F90
@@ -249,10 +249,14 @@ contains
         ! distribution (either via a restart or as set during initialisation)
         ! and print out.
 
+        use determinants, only: det_info
+        use fciqmc_data, only: walker_dets, walker_data
         use parallel
         use proc_pointers, only: update_proj_energy_ptr
+
         integer :: idet
         integer(lint) :: ntot_particles
+        type(det_info) :: cdet
 #ifdef PARALLEL
         integer :: ierr
         real(p) :: proj_energy_sum
@@ -264,7 +268,11 @@ contains
         proj_energy = 0.0_p
         D0_population = 0
         do idet = 1, tot_walkers
-            call update_proj_energy_ptr(idet)
+            cdet%f = walker_dets(:,idet)
+            cdet%data => walker_data(:,idet)
+            ! WARNING!  We assume only the bit string and data field are
+            ! required to update the projected estimator.
+            call update_proj_energy_ptr(cdet, walker_population(1,idet))
         end do
 
 #ifdef PARALLEL

src/ct_fciqmc.F90
@@ -90,7 +90,7 @@ contains
                 tmp_pop = walker_population(1,idet)
 
                 ! Evaluate the projected energy.
-                call update_proj_energy_ptr(idet)
+                call update_proj_energy_ptr(cdet, walker_population(1,idet))
 
                 ! Loop over each walker on the determinant.
                 do iparticle = 1, abs(walker_population(1,idet))

src/energy_evaluation.F90
@@ -135,7 +135,7 @@ contains
 
     end subroutine update_hf_shift
 
-    subroutine update_proj_energy_hub_k(idet)
+    subroutine update_proj_energy_hub_k(cdet, pop)
 
         ! Add the contribution of the current determinant to the projected
         ! energy.
@@ -150,32 +150,36 @@ contains
         ! efficient.
         ! This procedure is for the Hubbard model in momentum space only.
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string field needs to be set.
+        !    pop: population on current determinant.
 
-        use fciqmc_data, only: walker_dets, walker_population, f0, D0_population, proj_energy
+        use determinants, only: det_info
+        use fciqmc_data, only: f0, D0_population, proj_energy
         use excitations, only: excit, get_excitation
         use hamiltonian, only: slater_condon2_hub_k
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
         type(excit) :: excitation
         real(p) :: hmatel
 
-        excitation = get_excitation(walker_dets(:,idet), f0)
+        excitation = get_excitation(cdet%f, f0)
 
         if (excitation%nexcit == 0) then
             ! Have reference determinant.
-            D0_population = D0_population + walker_population(1,idet)
+            D0_population = D0_population + pop
         else if (excitation%nexcit == 2) then
             ! Have a determinant connected to the reference determinant: add to
             ! projected energy.
             hmatel = slater_condon2_hub_k(excitation%from_orb(1), excitation%from_orb(2), &
                                        & excitation%to_orb(1), excitation%to_orb(2),excitation%perm)
-            proj_energy = proj_energy + hmatel*walker_population(1,idet)
+            proj_energy = proj_energy + hmatel*pop
         end if
 
     end subroutine update_proj_energy_hub_k
 
-    subroutine update_proj_energy_hub_real(idet)
+    subroutine update_proj_energy_hub_real(cdet, pop)
 
         ! Add the contribution of the current determinant to the projected
         ! energy.
@@ -190,31 +194,35 @@ contains
         ! efficient.
         ! This procedure is for the Hubbard model in real space only.
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string field needs to be set.
+        !    pop: population on current determinant.
 
-        use fciqmc_data, only: walker_dets, walker_population, f0, D0_population, proj_energy
+        use determinants, only: det_info
+        use fciqmc_data, only: f0, D0_population, proj_energy
         use excitations, only: excit, get_excitation
         use hamiltonian, only: slater_condon1_hub_real
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
         type(excit) :: excitation
         real(p) :: hmatel
 
-        excitation = get_excitation(walker_dets(:,idet), f0)
+        excitation = get_excitation(cdet%f, f0)
 
         if (excitation%nexcit == 0) then
             ! Have reference determinant.
-            D0_population = D0_population + walker_population(1,idet)
+            D0_population = D0_population + pop
         else if (excitation%nexcit == 1) then
             ! Have a determinant connected to the reference determinant: add to
             ! projected energy.
             hmatel = slater_condon1_hub_real(excitation%from_orb(1), excitation%to_orb(1), excitation%perm)
-            proj_energy = proj_energy + hmatel*walker_population(1,idet)
+            proj_energy = proj_energy + hmatel*pop
         end if
 
     end subroutine update_proj_energy_hub_real
 
-    subroutine update_proj_energy_mol(idet)
+    subroutine update_proj_energy_mol(cdet, pop)
 
         ! Add the contribution of the current determinant to the projected
         ! energy.
@@ -231,28 +239,31 @@ contains
         ! FCIDUMP file).
         !
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string field needs to be set.
+        !    pop: population on current determinant.
 
         use basis, only: basis_fns
-        use determinants, only: decode_det
+        use determinants, only: decode_det, det_info
         use excitations, only: excit, get_excitation
-        use fciqmc_data, only: walker_dets, walker_population, f0, D0_population, proj_energy
+        use fciqmc_data, only: f0, D0_population, proj_energy
         use hamiltonian_molecular, only: slater_condon1_mol_excit, slater_condon2_mol_excit
         use point_group_symmetry, only: cross_product_pg_basis
         use system, only: nel
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
 
         type(excit) :: excitation
         real(p) :: hmatel
         integer :: occ_list(nel), ij_sym, ab_sym
 
-        excitation = get_excitation(walker_dets(:,idet), f0)
+        excitation = get_excitation(cdet%f, f0)
 
         select case(excitation%nexcit)
         case (0)
             ! Have reference determinant.
-            D0_population = D0_population + walker_population(1,idet)
+            D0_population = D0_population + pop
         case(1)
             ! Have a determinant connected to the reference determinant by
             ! a single excitation: add to projected energy.
@@ -260,10 +271,10 @@ contains
             ! Is excitation symmetry allowed?
             if (basis_fns(excitation%from_orb(1))%Ms == basis_fns(excitation%to_orb(1))%Ms .and. &
                     basis_fns(excitation%from_orb(1))%sym == basis_fns(excitation%to_orb(1))%sym) then
-                call decode_det(walker_dets(:,idet), occ_list)
+                call decode_det(cdet%f, occ_list)
                 hmatel = slater_condon1_mol_excit(occ_list, excitation%from_orb(1), excitation%to_orb(1), &
                                                   excitation%perm)
-                proj_energy = proj_energy + hmatel*walker_population(1,idet)
+                proj_energy = proj_energy + hmatel*pop
             end if
         case(2)
             ! Have a determinant connected to the reference determinant by
@@ -277,7 +288,7 @@ contains
                     hmatel = slater_condon2_mol_excit(excitation%from_orb(1), excitation%from_orb(2), &
                                                       excitation%to_orb(1), excitation%to_orb(2),     &
                                                       excitation%perm)
-                    proj_energy = proj_energy + hmatel*walker_population(1,idet)
+                    proj_energy = proj_energy + hmatel*pop
                 end if
             end if
         end select

src/fciqmc.f90
@@ -89,7 +89,7 @@ contains
                     ! It is much easier to evaluate the projected energy at the
                     ! start of the i-FCIQMC cycle than at the end, as we're
                     ! already looping over the determinants.
-                    call update_proj_energy_ptr(idet)
+                    call update_proj_energy_ptr(cdet, walker_population(1,idet))
 
                     ! Is this determinant an initiator?
                     call set_parent_flag_ptr(walker_population(1,idet), cdet%f, cdet%initiator_flag)

src/heisenberg_estimators.F90
@@ -6,7 +6,7 @@ implicit none
 
 contains
 
-    subroutine update_proj_energy_heisenberg_basic(idet)
+    subroutine update_proj_energy_heisenberg_basic(cdet, pop)
 
         ! Add the contribution of the current determinant to the projected
         ! energy.
@@ -21,24 +21,27 @@ contains
         ! efficient.
         ! This procedure is for the Heisenberg model only
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string field needs to be set.
+        !    pop: population on current determinant.
 
-        use fciqmc_data, only: walker_dets, walker_population, f0, D0_population, &
-                               proj_energy
+        use determinants, only: det_info
+        use fciqmc_data, only: f0, D0_population, proj_energy
         use excitations, only: excit, get_excitation
         use basis, only: bit_lookup
         use system, only: J_coupling
         use hubbard_real, only: connected_orbs
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
         type(excit) :: excitation
         integer :: bit_position, bit_element
 
-        excitation = get_excitation(walker_dets(:,idet), f0)
+        excitation = get_excitation(cdet%f, f0)
 
         if (excitation%nexcit == 0) then
             ! Have reference determinant.
-            D0_population = D0_population + walker_population(1,idet)
+            D0_population = D0_population + pop
         else if (excitation%nexcit == 1) then
             ! Have a determinant connected to the reference determinant: add to
             ! projected energy.
@@ -47,12 +50,12 @@ contains
             bit_element = bit_lookup(2,excitation%from_orb(1))
 
             if (btest(connected_orbs(bit_element, excitation%to_orb(1)), bit_position)) &
-                     proj_energy = proj_energy - 2.0_p*J_coupling*walker_population(1,idet)
+                     proj_energy = proj_energy - 2.0_p*J_coupling*pop
         end if
 
     end subroutine update_proj_energy_heisenberg_basic
 
-    subroutine update_proj_energy_heisenberg_positive(idet)
+    subroutine update_proj_energy_heisenberg_positive(cdet, pop)
 
         ! Add the contribution of the current basis fucntion to the
         ! projected energy.
@@ -64,22 +67,24 @@ contains
         ! are positive, and hence we get a good overlap.
         ! This procedure is for the Heisenberg model only.
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string and data fields need to be set.
+        !    pop: population on current determinant.
 
-        use fciqmc_data, only: walker_dets, walker_population, walker_data, &
-                               proj_energy, D0_population
+        use determinants, only: det_info
+        use fciqmc_data, only: proj_energy, D0_population
         use system, only: J_coupling, nbonds
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
 
-        proj_energy = proj_energy + (J_coupling*nbonds+2*walker_data(1,idet))* &
-                                     walker_population(1,idet)
+        proj_energy = proj_energy + (J_coupling*nbonds+2*cdet%data(1))*pop
 
-        D0_population = D0_population + walker_population(1,idet)
+        D0_population = D0_population + pop
 
     end subroutine update_proj_energy_heisenberg_positive
 
-    subroutine update_proj_energy_heisenberg_neel_singlet(idet)
+    subroutine update_proj_energy_heisenberg_neel_singlet(cdet, pop)
 
         ! Add the contribution of the current basis fucntion to the
         ! projected energy.
@@ -92,18 +97,21 @@ contains
         ! K. Runge, Phys. Rev. B 45, 7229 (1992).
         ! This procedure is for the Heisenberg model only.
         ! In:
-        !    idet: index of current determinant in the main walker list.
+        !    cdet: info on the current determinant (cdet) that we will spawn
+        !        from.  Only the bit string and data fields need to be set.
+        !    pop: population on current determinant.
 
-        use fciqmc_data, only: walker_dets, walker_population, walker_data, &
-                               sampling_size, proj_energy, neel_singlet_amp, D0_population
+        use determinants, only: det_info
+        use fciqmc_data, only: sampling_size, proj_energy, neel_singlet_amp, D0_population
         use system, only: nbonds, ndim, J_coupling, guiding_function, neel_singlet_guiding
 
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: cdet
+        integer, intent(in) :: pop
         integer :: n, lattice_1_up, lattice_2_up
         real(dp) :: importance_sampling_factor = 1.0
 
-        n = nint(walker_data(sampling_size+1,idet))
-        lattice_1_up = nint(walker_data(sampling_size+2,idet))
+        n = nint(cdet%data(sampling_size+1))
+        lattice_1_up = nint(cdet%data(sampling_size+2))
 
         ! If importance sampling is applied then the psip amplitudes, n_i,
         ! will represent the quantities
@@ -123,7 +131,7 @@ contains
         ! This means we can avoid calculating n(0-1) again, which is expensive.
         ! We know the number of 0-1 bonds where the 1 (the spin up) is on sublattice 1,
         ! so can then deduce the number where the 1 is on sublattice 2.
-        lattice_2_up = ((nbonds) + nint(walker_data(1,idet)/J_coupling))/2 - lattice_1_up
+        lattice_2_up = ((nbonds) + nint(cdet%data(1)/J_coupling))/2 - lattice_1_up
 
         ! There are three contributions to add to the projected energy from
         ! the current basis function. Consider the Neel singlet state:
@@ -134,8 +142,8 @@ contains
 
         ! Firstly, consider the diagonal term:
         ! We have <D_j|H|D_j> stored, so this is simple:
-        proj_energy = proj_energy + (neel_singlet_amp(n) * walker_data(1,idet) * &
-                                          walker_population(1,idet) * importance_sampling_factor)
+        proj_energy = proj_energy + (neel_singlet_amp(n) * cdet%data(1) * &
+                                          pop * importance_sampling_factor)
 
         ! Now, to find all other basis functions connected to |D_j>, we find 0-1 bonds
         ! and then flip both of these spins. The resulting basis function, |D_i> will be
@@ -151,11 +159,11 @@ contains
 
         ! From 0-1 bonds where the 1 is on sublattice 1, we have:
         proj_energy = proj_energy - (2 * J_coupling * lattice_1_up * &
-                    walker_population(1,idet) * neel_singlet_amp(n-1) * importance_sampling_factor)
+                    pop * neel_singlet_amp(n-1) * importance_sampling_factor)
 
         ! And from 1-0 bond where the 1 is on sublattice 2, we have:
         proj_energy = proj_energy - (2 * J_coupling * lattice_2_up * &
-                    walker_population(1,idet) * neel_singlet_amp(n+1) * importance_sampling_factor)
+                    pop * neel_singlet_amp(n+1) * importance_sampling_factor)
 
         ! Now we just need to find the contribution to the denominator. The total
         ! denominator is
@@ -163,7 +171,7 @@ contains
         ! Hence from this particular basis function, |D_j>, we just add (a_j * n_j)
 
         D0_population = D0_population + &
-                          walker_population(1,idet)*neel_singlet_amp(n)*importance_sampling_factor
+                          pop*neel_singlet_amp(n)*importance_sampling_factor
 
     end subroutine update_proj_energy_heisenberg_neel_singlet
 

src/proc_pointers.f90
@@ -17,9 +17,11 @@ abstract interface
         integer(i0), intent(in) :: f(basis_length)
         type(det_info), intent(inout) :: d
     end subroutine i_decoder
-    subroutine i_update_proj_energy(idet)
+    subroutine i_update_proj_energy(d, pop)
+        import :: det_info
         implicit none
-        integer, intent(in) :: idet
+        type(det_info), intent(in) :: d
+        integer, intent(in) :: pop
     end subroutine i_update_proj_energy
     subroutine i_update_dmqmc_estimators(idet,excitation)
         import :: excit

commit 766cec8ded1b92d9d1b1c6bdf4e97a45076c700f

Bug fix for calculating initial projected estimator.
Need to allocate the components of a det_info variable before using
them.

This bug was introduced in c23a04bcf6ea061648c0d34c5705900014828426.

src/fciqmc_common.F90
@@ -249,7 +249,7 @@ contains
         ! distribution (either via a restart or as set during initialisation)
         ! and print out.
 
-        use determinants, only: det_info
+        use determinants, only: det_info, alloc_det_info, dealloc_det_info
         use fciqmc_data, only: walker_dets, walker_data
         use parallel
         use proc_pointers, only: update_proj_energy_ptr
@@ -267,6 +267,7 @@ contains
         ! update_proj_energy.
         proj_energy = 0.0_p
         D0_population = 0
+        call alloc_det_info(cdet)
         do idet = 1, tot_walkers
             cdet%f = walker_dets(:,idet)
             cdet%data => walker_data(:,idet)
@@ -274,6 +275,7 @@ contains
             ! required to update the projected estimator.
             call update_proj_energy_ptr(cdet, real(walker_population(1,idet),p))
         end do
+        call dealloc_det_info(cdet)
 
 #ifdef PARALLEL
         call mpi_allreduce(proj_energy, proj_energy_sum, 1, mpi_preal, MPI_SUM, MPI_COMM_WORLD, ierr)