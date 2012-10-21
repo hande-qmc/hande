7b5fb3f1ecf0427e73bc0fc2fea3c0e2de4e6dd6
James Spencer <j.spencer@imperial.ac.uk>
2012-10-21 17:34:06 +0100

    Reformating of environment_report to match current standards of the rest of the code.

--- lib/local/environment_report.F90
+++ lib/local/environment_report.F90
@@ -1,16 +1,8 @@
 module report
 
-! James Spencer, CUC3, University of Cambridge.
+! James Spencer, Imperial College London.
 !
-! Copyright (c) 2009 James Spencer.
-!
-! environment_report prints out a summary of the environment:
-!   * when the code was compiled;
-!   * the VCS BASE repository version (there's no guarantee that the code wasn't
-!     changed!);
-!   * whether the working directory contains local changes;
-!   * the working directory;
-!   * the host computer.
+! Copyright (c) 2012 James Spencer.
 !
 ! Permission is hereby granted, free of charge, to any person
 ! obtaining a copy of this software and associated documentation
@@ -37,149 +29,149 @@ contains
 
     subroutine environment_report(io)
 
-    ! In:
-    !    io (optional): unit to which the environment information is written.
-    !                   Default: 6.
-    !
-    ! Print out a summary of the environment:
-    !   * when the code was compiled;
-    !   * the VCS BASE repository version (there's no guarantee that the code wasn't
-    !     changed!);
-    !   * whether the working directory contains local changes;
-    !   * the working directory;
-    !   * the host computer.
+        ! In:
+        !    io (optional): unit to which the environment information is written.
+        !                   Default: 6.
+        !
+        ! Print out a summary of the environment:
+        !   * when the code was compiled;
+        !   * the VCS BASE repository version (there's no guarantee that the code wasn't
+        !     changed!);
+        !   * whether the working directory contains local changes;
+        !   * the working directory;
+        !   * the host computer.
 
-    ! The VCS information is passed to environment_report via c-preprocessing.  The
-    ! following definitions are used:
-    !
-    ! * NAGF95: define if using the nag compiler so that certain nag-only modules are
-    !   used.
-    ! * _WORKING_DIR_CHANGES: define if the working directory contains local
-    !   (uncommitted) changes.
-    ! * _VCS_VERSION: set to a (quoted!) string containing the VCS revision id of the current
-    !   commit.
-    !
-    ! The VCS information can be simply obtained via shell commands in the makefile:
-    ! see the example provided for the commands needed for git and subversion
-    ! repositories.  For instance, if a subversion repository is being used, then the
-    ! version id can be obtained using::
-    !
-    !     VCS_VERSION:=$(shell echo -n '"'$(svn info | awk '/Revision/{print $NF}')'"')
-    !
-    ! and if the working directory contains local changes, then the command::
-    !
-    !     svn st -q | xargs -i test -z {}
-    !
-    ! gives a non-zero return code.  Hence the variable WORKING_DIR_CHANGES is
-    ! equal to -D_WORKING_DIR_CHANGES or is a null string depending on whether there
-    ! are local changes or not::
-    !
-    !    WORKING_DIR_CHANGES:=$(shell svn st -q | xargs -i test -z {} || echo -n "-D_WORKING_DIR_CHANGES")
-    !
-    ! environment_report.F90 can thus be appropriately compiled with the command::
-    !
-    !    $(FC) $(WORKING_DIR_CHANGES) -D_VCS_VERSION='$(VCS_VERSION)' -c environment_report.F90 -o environment_report.o
-    !
-    ! where $(FC) is defined to be the desired fortran compiler.
-    !
-    ! Similarly for git::
-    !
-    !     VCS_VERSION:=$(shell echo -n '"' && git log --max-count=1 --pretty=format:%H && echo -n '"')
-    !     WORKING_DIR_CHANGES := $(shell git diff-index --quiet --cached HEAD
-    !                             --ignore-submodules -- && git diff-files --quiet
-    !                             --ignore-submodules || echo -n "-D_WORKING_DIR_CHANGES") # on one line.
-    !
-    ! environment_report has been tested with gfortran, g95, ifort, pgf90, nag and
-    ! pathscale.  Note that hostnm and getcwd are intrinsic functions (at least
-    ! according to gnu documentation), unless you have to use nag.  There should also
-    ! exist the intrinsic subroutines, but pgf90 and ifort gave segmentation faults
-    ! when they were used, so we use intrinsic functions unless using nag.
+        ! The VCS information is passed to environment_report via c-preprocessing.  The
+        ! following definitions are used:
+        !
+        ! * NAGF95: define if using the nag compiler so that certain nag-only modules are
+        !   used.
+        ! * _WORKING_DIR_CHANGES: define if the working directory contains local
+        !   (uncommitted) changes.
+        ! * _VCS_VERSION: set to a (quoted!) string containing the VCS revision id of the current
+        !   commit.
+        !
+        ! The VCS information can be simply obtained via shell commands in the makefile:
+        ! see the example provided for the commands needed for git and subversion
+        ! repositories.  For instance, if a subversion repository is being used, then the
+        ! version id can be obtained using::
+        !
+        !     VCS_VERSION:=$(shell echo -n '"'$(svn info | awk '/Revision/{print $NF}')'"')
+        !
+        ! and if the working directory contains local changes, then the command::
+        !
+        !     svn st -q | xargs -i test -z {}
+        !
+        ! gives a non-zero return code.  Hence the variable WORKING_DIR_CHANGES is
+        ! equal to -D_WORKING_DIR_CHANGES or is a null string depending on whether there
+        ! are local changes or not::
+        !
+        !    WORKING_DIR_CHANGES:=$(shell svn st -q | xargs -i test -z {} || echo -n "-D_WORKING_DIR_CHANGES")
+        !
+        ! environment_report.F90 can thus be appropriately compiled with the command::
+        !
+        !    $(FC) $(WORKING_DIR_CHANGES) -D_VCS_VERSION='$(VCS_VERSION)' -c environment_report.F90 -o environment_report.o
+        !
+        ! where $(FC) is defined to be the desired fortran compiler.
+        !
+        ! Similarly for git::
+        !
+        !     VCS_VERSION:=$(shell echo -n '"' && git log --max-count=1 --pretty=format:%H && echo -n '"')
+        !     WORKING_DIR_CHANGES := $(shell git diff-index --quiet --cached HEAD
+        !                             --ignore-submodules -- && git diff-files --quiet
+        !                             --ignore-submodules || echo -n "-D_WORKING_DIR_CHANGES") # on one line.
+        !
+        ! environment_report has been tested with gfortran, g95, ifort, pgf90, nag and
+        ! pathscale.  Note that hostnm and getcwd are intrinsic functions (at least
+        ! according to gnu documentation), unless you have to use nag.  There should also
+        ! exist the intrinsic subroutines, but pgf90 and ifort gave segmentation faults
+        ! when they were used, so we use intrinsic functions unless using nag.
 
 #ifdef NAGF95
-    use f90_unix_dir, only: getcwd
-    use f90_unix_env, only: gethostname
+        use f90_unix_dir, only: getcwd
+        use f90_unix_env, only: gethostname
 #endif
 
-    implicit none
-    integer, intent(in), optional :: io
-    integer :: stat,io_unit
+        implicit none
+        integer, intent(in), optional :: io
+        integer :: stat,io_unit
 #ifndef NAGF95
-    integer :: getcwd,hostnm
+        integer :: getcwd,hostnm
 #endif
-    character(255) :: dirname,host
-    integer :: date_values(8)
+        character(255) :: dirname,host
+        integer :: date_values(8)
 
-    if (present(io)) then
-        io_unit = io
-    else
-        io_unit = 6
-    end if
+        if (present(io)) then
+            io_unit = io
+        else
+            io_unit = 6
+        end if
 
-    write (io_unit,'(1X,64("="))')
+        write (io_unit,'(1X,64("="))')
 
 #ifndef NAGF95
-    ! Stupid nag: it uses fpp, which does not define __DATE__ and __TIME__ like cpp
-    ! does.
-    write (io_unit,'(a13,a,a4,a)') 'Compiled on ',__DATE__,'at ',__TIME__
+        ! Stupid nag: it uses fpp, which does not define __DATE__ and __TIME__ like cpp
+        ! does.
+        write (io_unit,'(a13,a,a4,a)') 'Compiled on ',__DATE__,'at ',__TIME__
 #endif
-    write (io_unit,'(a16,a)') 'Compiled using ', _CONFIG
+        write (io_unit,'(a16,a)') 'Compiled using ', _CONFIG
 
-    write (io_unit,'(a29,/,5X,a)') 'VCS BASE repository version:',_VCS_VERSION
+        write (io_unit,'(a29,/,5X,a)') 'VCS BASE repository version:',_VCS_VERSION
 #ifdef _WORKING_DIR_CHANGES
-    write (io_unit,'(a42)') 'Working directory contains local changes.'
+        write (io_unit,'(a42)') 'Working directory contains local changes.'
 #endif
 
 #ifdef NAGF95
-    call getcwd(dirname,errno=stat)
+        call getcwd(dirname,errno=stat)
 #else
-    stat = getcwd(dirname)
+        stat = getcwd(dirname)
 #endif
 
-    if (stat.eq.0) then
-        write (io_unit,'(a20)') 'Working directory: '
-        write (io_unit,'(5X,a)') trim(dirname)
-    end if
+        if (stat.eq.0) then
+            write (io_unit,'(a20)') 'Working directory: '
+            write (io_unit,'(5X,a)') trim(dirname)
+        end if
 
 #ifdef NAGF95
-    call gethostname(host)
-    stat = 0
+        call gethostname(host)
+        stat = 0
 #else
-    stat = hostnm(host)
+        stat = hostnm(host)
 #endif
 
-    call date_and_time(VALUES=date_values)
+        call date_and_time(VALUES=date_values)
 
-    write (6,'(1X,a18,1X,i2.2,"/",i2.2,"/",i4.4,1X,a2,1X,i2.2,2(":",i2.2))') &
-               "Started running on", date_values(3:1:-1), "at", date_values(5:7)
+        write (6,'(1X,a18,1X,i2.2,"/",i2.2,"/",i4.4,1X,a2,1X,i2.2,2(":",i2.2))') &
+                   "Started running on", date_values(3:1:-1), "at", date_values(5:7)
 
-    write (io_unit,'(1X,64("="),/)')
+        write (io_unit,'(1X,64("="),/)')
 
-    return
+        return
 
     end subroutine environment_report
 
     subroutine end_report(elapsed_time, io)
 
-    real, intent(in) :: elapsed_time
-    integer, intent(in), optional :: io
-    integer :: date_values(8)
+        real, intent(in) :: elapsed_time
+        integer, intent(in), optional :: io
+        integer :: date_values(8)
 
-    if (present(io)) then
-        io_unit = io
-    else
-        io_unit = 6
-    end if
+        if (present(io)) then
+            io_unit = io
+        else
+            io_unit = 6
+        end if
 
-    write (io_unit,'(1X,64("="))')
+        write (io_unit,'(1X,64("="))')
 
-    call date_and_time(VALUES=date_values)
+        call date_and_time(VALUES=date_values)
 
-    write (io_unit,'(1X,a19,1X,i2.2,"/",i2.2,"/",i4.4,1X,a2,1X,i2.2,2(":",i2.2))') &
-               "Finished running on", date_values(3:1:-1), "at", date_values(5:7)
-    write (io_unit,'(1X,a17,10X,f14.2,a1)') "Calculation took:", elapsed_time, "s"
+        write (io_unit,'(1X,a19,1X,i2.2,"/",i2.2,"/",i4.4,1X,a2,1X,i2.2,2(":",i2.2))') &
+                   "Finished running on", date_values(3:1:-1), "at", date_values(5:7)
+        write (io_unit,'(1X,a17,10X,f14.2,a1)') "Calculation took:", elapsed_time, "s"
 
-    write (io_unit,'(1X,64("="),/)')
+        write (io_unit,'(1X,64("="),/)')
 
     end subroutine end_report
 
-end module report
+    end module report
