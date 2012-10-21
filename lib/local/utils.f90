c3aa2da1049b7384d5805979358f7e9062592f1b
James Spencer <j.spencer@imperial.ac.uk>
2012-10-21 17:50:46 +0100

    Utility functions for working with strings when calling C.
    C strings are equivalent for Fortran character arrays rather than
    Fortran character strings, so we cannot blindly call a C procedure and
    pass it a Fortran string.
    
    carray_to_fstring converts from a C string (array) to a Fortran string
    and fstring_to_carray does the opposite.

--- lib/local/utils.f90
+++ lib/local/utils.f90
@@ -274,4 +274,56 @@ contains
 
     end function tri_ind_reorder
 
+    pure function fstring_to_carray(string_f03) result(array_c)
+
+        ! Convert a Fortran string into a C string.  The result can be passed to
+        ! C procedures which require a *char object, which corresponds to an
+        ! array in Fortran.
+
+        ! In:
+        !    string_f03: Fortran character string.
+        ! Returns:
+        !    The equivalent (null-terminated) C string in a character array.
+
+        use, intrinsic :: iso_c_binding, only: c_char, c_null_char
+
+        character(len=*), intent(in) :: string_f03
+        character(kind=c_char) :: array_c(len(string_f03)+1)
+
+        integer :: i
+
+        do i = 1, len(string_f03)
+            array_c(i) = string_f03(i:i)
+        end do
+        array_c(i) = c_null_char
+
+    end function fstring_to_carray
+
+    pure function carray_to_fstring(array_c) result(string_f03)
+
+        ! Convert a C string into a Fortran string.  The input can be from a
+        ! C procedure which uses a *char object (which corresponds to an array
+        ! in Fortran) and the output is the equivalent standard Fortran string.
+
+        ! In:
+        !    array_c: null-terminated C string in a character array.
+        ! Returns:
+        !    The equivalent Fortran character string (without null termination).
+
+        use, intrinsic :: iso_c_binding, only: c_char, c_null_char
+
+        character(kind=c_char), intent(in) :: array_c(:)
+        character(len=size(array_c)-1) :: string_f03
+
+        integer :: i
+
+        ! Don't copy any (presumably garbage) from beyond the null character.
+        string_f03 = ''
+        do i = 1, size(array_c)-1
+            if (array_c(i) == c_null_char) exit
+            string_f03(i:i) = array_c(i)
+        end do
+
+    end function carray_to_fstring
+
 end module utils
