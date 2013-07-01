f62ae2a5a5b811ea780ecac2d27de3438a0bc161
Nick Blunt <nsb37@cam.ac.uk>
2013-07-01 15:23:37 +0100

    average_rdm.py script updated for new output.

--- tools/average_rdm.py
+++ tools/average_rdm.py
@@ -5,7 +5,6 @@ import re
 import sys
 import scipy
 import scipy.stats
-import linecache
 
 def extract_data(data_file):
     trace = []
@@ -13,66 +12,42 @@ def extract_data(data_file):
     elem_mean = []
     elem_se = []
     covariances = []
-    
-    first_line_regex = '^# Number of elements in each RDM row:'
-    trace_regex = '^# RDM trace'
-    
-    for data_file in data_file:
 
+    trace_regex = '^Trace'
+    n = 0
+
+    for data_file in data_file:
+    
         f = open(data_file)
 
-        # Get the system size from the first line of the file.
-        line = f.readline()
-        if not re.match(first_line_regex, line):
-            print 'First line of file is not as expected.'
-        else:
-            words = line.split()
-            rdm_row_size = int(words[8])
-            # Number of elements above and including the diagonal.
-            rdm_unique_elems = rdm_row_size*(rdm_row_size+1)/2
-            num_lines_per_loop = rdm_unique_elems + 3
+        for line in f:
+            if re.match(trace_regex, line):
+                values = line.split()
+                for value in values[1:]:
+                   trace.append(float(value)) 
+                trace_mean = scipy.mean(trace)
+                trace_se = scipy.stats.sem(trace)
+            else:
+                elements = []
+                values = line.split()
+                for value in values[1:]:
+                   elements.append(float(value)) 
+                elem_mean.append(scipy.mean(elements))
+                elem_se.append(scipy.stats.sem(elements))
+                covariances.append(calculate_covariance(elements, elem_mean[n-1], trace, trace_mean))
+            n += 1
 
-        # First, get the trace mean and se.
-        have_data = True
-        num_beta_loops = 0
-        # The line of the first RDM trace.
-        line_num = 4
-        while have_data:
-            line = linecache.getline(data_file,line_num)
-            linecache.clearcache()
-            if not re.match(trace_regex, line):
-                have_data = False
-            if have_data:
-                words = line.split()    
-                trace.append(float(words[3]))
-                num_beta_loops = num_beta_loops + 1
-            # The line of the next RDM trace.
-            line_num = line_num + num_lines_per_loop
-        trace_mean = scipy.mean(trace)
-        trace_se = scipy.stats.sem(trace)
+    # We want the first line of the output to hold the number of elements in each row of the RDM.
+    # The number of elements above and including the diagonal of this d-by-d RDM is n-1.
+    # It is also true that n-1 = d*(d-1)/2. Putting these together and solving for d (taking
+    # the positive solution) gives the following expression.
+    rdm_row_size = int(-0.5 + 0.5*math.sqrt(1 + 8*(n-1)))
 
-        # Next get the mean and se for each RDM element.
+    # For the first line of the output, print the row size.
+    print rdm_row_size
 
-        # Over all RDM elements in a particular loop.
-        for i in range(1, rdm_unique_elems+1):
-            elements = []
-            # Over all beta loops for a given element.
-            for j in range(1, num_beta_loops+1):
-                line_num = num_lines_per_loop*(j-1) + i + 4
-                line = linecache.getline(data_file,line_num)
-                linecache.clearcache()
-                if len(line) == 0:
-                    print "Unexpected empty line found."
-                words = line.split()
-                elements.append(float(words[0]))
-            elem_mean.append(scipy.mean(elements))
-            elem_se.append(scipy.stats.sem(elements))
-            covariances.append(calculate_covariance(elements, elem_mean[i-1], trace, trace_mean))
+    f.close()
 
-        # For the first line of the output, print the row size.
-        print rdm_row_size
-
-        f.close()
     return trace_mean, trace_se, elem_mean, elem_se, covariances
 
 def calculate_covariance(numerator, numerator_mean, trace, trace_mean):
