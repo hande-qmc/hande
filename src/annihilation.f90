4c2ed7553bd50ea8c1937d790927253a3d213bfe
Fionn Malone <f.malone13@imperial.ac.uk>
2013-11-30 15:55:45 +0000

    Moved call to load balancing to here from fciqmc.f90.

--- src/annihilation.f90
+++ src/annihilation.f90
@@ -27,11 +27,17 @@ contains
         use spawn_data, only: annihilate_wrapper_spawn_t
         use sort, only: qsort
         use system, only: sys_t
+        use loadbal_data
+        use load_balancing, only: do_load_balancing
 
         type(sys_t), intent(in) :: sys
         logical, intent(in) :: tinitiator
 
         integer, parameter :: thread_id = 0
+        
+        if(doing_load_balancing) then
+            call do_load_balancing(proc_map)
+        end if 
 
         call annihilate_wrapper_spawn_t(qmc_spawn, tinitiator)
 
