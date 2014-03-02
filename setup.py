b1c74af942fbcdb05ee66dad867f879049f0fa7a
James Spencer <j.spencer@imperial.ac.uk>
2014-03-02 22:10:42 +0000

    setup.py for installing pyblock (eventually from PyPI).

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyblock',
+    version='1.0',
+    author='James Spencer',
+    url='http://github.com/jsspencer/pyblock',
+    packages=['pyblock',],
+    license='Modified BSD license',
+    description='Reblocking analysis tools for correlated data',
+    #long_description=open('README.txt').read(),
+)
