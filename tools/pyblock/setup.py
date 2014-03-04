7b5d894906c5c3d6c00290e57a66ac94d04c10db
James Spencer <j.spencer@imperial.ac.uk>
2014-03-04 01:15:36 +0000

    Requirements for pyblock installation.

--- tools/pyblock/setup.py
+++ tools/pyblock/setup.py
@@ -9,4 +9,5 @@ setup(
     license='Modified BSD license',
     description='Reblocking analysis tools for correlated data',
     #long_description=open('README.txt').read(),
+    requires=['numpy', 'pandas (>=0.12)'],
 )
