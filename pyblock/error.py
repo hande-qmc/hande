001b40204bb812ebf3f75e2323ad803997b9d78d
James Spencer <j.spencer@imperial.ac.uk>
2014-03-12 12:49:11 +0000

    Include (and fix) pyblock.error in the documentation.

--- pyblock/error.py
+++ pyblock/error.py
@@ -15,19 +15,21 @@ def ratio(stats_A, stats_B, cov_AB, data_len):
 
 Parameters
 ----------
-stats_A, stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+stats_A : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Statistics (containing at least the 'mean' and 'standard error' fields) for
-    variables :math:`A` and :math:`B` respectively.  The rows contain different
-    values of these statistics (e.g. from a reblocking analysis) if
-    :class:`pandas.DataFrame` are passed.
+    variable :math:`A`.  The rows contain different values of these statistics
+    (e.g. from a reblocking analysis) if :class:`pandas.DataFrame` are passed.
+stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+    Similarly for variable :math:`B`.
 cov_AB : float or :class:`pandas.Series`
-    Covariance between variables `A and B.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
-    the same index as ``stats_A`` and ``stats_B``.
+    Covariance between variables :math:`A` and :math:`B`.  If ``stats_A`` and
+    ``stats_B`` are :class:`pandas.DataFrame`, then this must be
+    a :class:`pandas.Series`, with the same index as ``stats_A`` and
+    ``stats_B``.
 data_len : int or :class:`pandas.Series`
     Number of data points ('observations') used to obtain the statistics given
     in ``stats_A`` and ``stats_B``.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
+    :class:`pandas.DataFrame`, then this must be a :class:`pandas.Series`, with
     the same index as ``stats_A`` and ``stats_B``.
 
 Returns
@@ -35,38 +37,39 @@ Returns
 ratio_stats : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Mean and standard error (and, if possible/relevant, optimal reblock
     iteration) for :math:`f = A/B`.  If ``stats_A``, ``stats_B`` are
-    :class:`pandas.DataFrames`, this is a :class:`pandas.DataFrame` with the
+    :class:`pandas.DataFrame`, this is a :class:`pandas.DataFrame` with the
     same index, otherwise a :class:`pandas.Series` is returned.
 '''
 
     return _quadratic(stats_A, stats_B, cov_AB, data_len, -1)
 
 def product(stats_A, stats_B, cov_AB, data_len):
-    '''Calculate the mean and standard error of :math:`f = A \times B`.
+    '''Calculate the mean and standard error of :math:`f = A \\times B`.
 
 Parameters
 ----------
-stats_A, stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+stats_A : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Statistics (containing at least the 'mean' and 'standard error' fields) for
-    variables :math:`A` and :math:`B` respectively.  The rows contain different
-    values of these statistics (e.g. from a reblocking analysis) if
-    :class:`pandas.DataFrame` are passed.
+    variable :math:`A`.  The rows contain different values of these statistics
+    (e.g. from a reblocking analysis) if :class:`pandas.DataFrame` are passed.
+stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+    Similarly for variable :math:`B`.
 cov_AB : float or :class:`pandas.Series`
     Covariance between variables `A and B.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
+    :class:`pandas.DataFrame`, then this must be a :class:`pandas.Series`, with
     the same index as ``stats_A`` and ``stats_B``.
 data_len : int or :class:`pandas.Series`
     Number of data points ('observations') used to obtain the statistics given
     in ``stats_A`` and ``stats_B``.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
+    :class:`pandas.DataFrame`, then this must be a :class:`pandas.Series`, with
     the same index as ``stats_A`` and ``stats_B``.
 
 Returns
 -------
 product_stats : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Mean and standard error (and, if possible/relevant, optimal reblock
-    iteration) for :math:`f = A \times B`.  If ``stats_A``, ``stats_B`` are
-    :class:`pandas.DataFrames`, this is a :class:`pandas.DataFrame` with the
+    iteration) for :math:`f = A \\times B`.  If ``stats_A``, ``stats_B`` are
+    :class:`pandas.DataFrame`, this is a :class:`pandas.DataFrame` with the
     same index, otherwise a :class:`pandas.Series` is returned.
 '''
 
@@ -77,19 +80,20 @@ def _quadratic(stats_A, stats_B, cov_AB, data_len, sign):
 
 Parameters
 ----------
-stats_A, stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+stats_A : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Statistics (containing at least the 'mean' and 'standard error' fields) for
-    variables :math:`A` and :math:`B` respectively.  The rows contain different
-    values of these statistics (e.g. from a reblocking analysis) if
-    :class:`pandas.DataFrame` are passed.
+    variable :math:`A`.  The rows contain different values of these statistics
+    (e.g. from a reblocking analysis) if :class:`pandas.DataFrame` are passed.
+stats_B : :class:`pandas.Series` or :class:`pandas.DataFrame`
+    Similarly for variable :math:`B`.
 cov_AB : float or :class:`pandas.Series`
     Covariance between variables `A and B.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
+    :class:`pandas.DataFrame`, then this must be a :class:`pandas.Series`, with
     the same index as ``stats_A`` and ``stats_B``.
 data_len : int or :class:`pandas.Series`
     Number of data points ('observations') used to obtain the statistics given
     in ``stats_A`` and ``stats_B``.  If ``stats_A`` and ``stats_B`` are
-    :class:`pandas.DataFrames`, then this must be a :class:`pandas.Series`, with
+    :class:`pandas.DataFrame`, then this must be a :class:`pandas.Series`, with
     the same index as ``stats_A`` and ``stats_B``.
 sign : int
     :math:`g(A,B) = A*B` for sign=1 and :math:`f = A/B` for sign=-1.
@@ -99,7 +103,7 @@ Returns
 func_data : :class:`pandas.Series` or :class:`pandas.DataFrame`
     Mean and standard error (and, if possible/relevant, optimal reblock
     iteration) for :math:`f = g(A,B)`.  If ``stats_A``, ``stats_B`` are
-    :class:`pandas.DataFrames`, this is a :class:`pandas.DataFrame` with the
+    :class:`pandas.DataFrame`, this is a :class:`pandas.DataFrame` with the
     same index, otherwise a :class:`pandas.Series` is returned.
 
 Raises
@@ -131,7 +135,7 @@ ValueError
             else:
                 stats['optimal block'] = stats_B['optimal block']
     except ValueError:
-        # Was given a single data point rather than a set.
+        # Were given a single data point rather than a set.
         stats = pd.Series(stats_dict)
 
     return stats
