686040997f215f086b4cc15007c7114b98f4e6eb
James Spencer <j.spencer@imperial.ac.uk>
2014-04-02 05:48:44 +0100

    Bundle tests with pyblock as well.
    This makes Travis-CI easier to handle and also allows users who don't install from the
    git repository to run the tests.

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version='1.0',
     author='James Spencer',
     url='http://github.com/jsspencer/pyblock',
-    packages=['pyblock',],
+    packages=('pyblock', 'pyblock.tests'),
     license='Modified BSD license',
     description='Reblocking analysis tools for correlated data',
-    #long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     requires=['numpy', 'pandas (>=0.13)', 'matplotlib'],
 )
