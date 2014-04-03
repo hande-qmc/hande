ea504f1b5d2bd87b0e0ab7ef4a15fe785ce1cf14
Nick Blunt <nsb37@cam.ac.uk>
2014-04-03 21:36:06 +0100

    Restructured script, and HANDE DMQMC output improved.

--- tools/dmqmc/finite_temp_analysis.py
+++ tools/dmqmc/finite_temp_analysis.py
@@ -12,32 +12,88 @@ import optparse
 import numpy as np
 import scipy.interpolate
 
-def perform_averaging(beta_values, estimates, nsamples, options):
-    '''Perform analysis of DMQMC data from a HANDE calculation.
+def analyse_observables(means, covariances, nsamples):
+    '''Calculate all mean and error estimates of the form Tr(\rho O)/Tr(\rho).
 
 Parameters
 ----------
-beta_values : :class:`pandas.Series`
-    The beta values at which data was accumulated (for at least one beta loop, but
-    not necessarily all loops).
-estimates : :class:`pandas.DataFrame`
-    All of the estimates from all beta loops, which are to be combined and analysed.
+means : :class:`pandas.DatFrame`
+    The mean estimates, obtained by averaging over beta loops, as a function of
+    beta, which is used as the index.
+covariances : :class:`pandas.DataFrame`
+    Estimates of the covariance matrix as a function of the beta, which is used
+    as the index.
 nsamples : :class:`pandas.Series`
-    The number of samples at the various values in beta_values.
-options : :class:`OptionParser`
-    Options read in from command line.
+    The number of samples contributing to the various beta values.
 
 Returns
 -------
-final_estimates : :class:`pandas.Series`
-    All of the final mean and error estimates as a function of beta, ready for output.
+results : :class:`pandas.DataFrame`
+    Contains all of the final mean and error estimates.
+None.
+'''
+    columns = list(means.columns.values)
+    beta_values = means.index.values
+
+    # The keys hold the names to be output and the values hold the possible
+    # columns names in the means and covariances DataFrames.
+    observables = dict([
+        ('Tr[Hp]/Tr[p]','\\sum\\rho_{ij}H_{ji}'), 
+        ('Tr[H2p]/Tr[p]','\\sum\\rho_{ij}H2{ji}'),
+        ('Tr[Sp]/Tr[p]','\\sum\\rho_{ij}S_{ji}'),
+        ('Tr[Mp]/Tr[p]','\\sum\\rho_{ij}M2{ji}'),
+    ])
+
+    # DataFrame to hold the final mean and error estimates.
+    results = pd.DataFrame(index=beta_values)
+    # DataFrame for the numerator.
+    num = pd.DataFrame(columns=['mean','standard error'], index=beta_values)
+    # DataFrame for the trace from the first replica.
+    tr1 = pd.DataFrame(columns=['mean','standard error'], index=beta_values)
+
+    tr1['mean'] = means['Trace']
+    tr1['standard error'] = np.sqrt(covariances.xs('Trace',level=1)['Trace']/nsamples)
+
+    for (k,v) in observables.items():
+        if v in columns:
+            num['mean'] = means[v]
+            num['standard error'] = np.sqrt(covariances.xs(v,level=1)[v]/nsamples)
+            cov_AB = covariances.xs('Trace',level=1)[v]
+
+            stats = pyblock.error.ratio(num, tr1, cov_AB, nsamples)
+
+            results[k] = stats['mean']
+            results[k+' error'] = stats['standard error']
+
+    return results
+
+def analyse_renyi_entropy(means, covariances, nsamples):
+    '''Calculate the mean and error estimates for the Renyi entropy (S2), for
+       all subsystems, including the entire system if present.
+
+Parameters
+----------
+means : :class:`pandas.DatFrame`
+    The mean estimates, obtained by averaging over beta loops, as a function of
+    beta, which is used as the index.
+covariances : :class:`pandas.DataFrame`
+    Estimates of the covariance matrix as a function of the beta, which is used
+    as the index.
+nsamples : :class:`pandas.Series`
+    The number of samples contributing to the various beta values.
+
+Returns
+-------
+results : :class:`pandas.DataFrame`
+    Contains all of the final mean and error estimates.
 None.
 '''
 
-    columns = list(estimates.columns.values)
+    columns = list(means.columns.values)
+    beta_values = means.index.values
 
-    #DataFrame for all of the final estimates to be printed.
-    final_estimates = pd.DataFrame(index=beta_values)
+    # DataFrame to hold the final mean and error estimates.
+    results = pd.DataFrame(index=beta_values)
     # DataFrame for the numerator.
     num = pd.DataFrame(columns=['mean','standard error'], index=beta_values)
     # DataFrame for the trace from the first replica.
@@ -45,60 +101,23 @@ None.
     # DataFrame for the trace from the second replica.
     tr2 = pd.DataFrame(columns=['mean','standard error'], index=beta_values)
 
-    means = estimates.groupby(level=2).mean()
-    covariances = estimates.groupby(level=2).cov()
-
-    # The keys hold the names to be output and the values hold the possible
-    # columns names in the estimates DataFrame.
-    observables = dict([
-        ('Tr[Hp]/Tr[p]','\\sum\\rho_{ij}H_{ji}'), 
-        ('Tr[Sp]/Tr[p]','\\sum\\rho_{ij}S_{ji}'),
-        ('Tr[Cp]/Tr[p]','\\sum\\rho_{ij}C_{ji}'),
-        ('Tr[Mp]/Tr[p]','\\sum\\rho_{ij}M2{ji}'),
-        ('Tr[H2p]/Tr[p]','\\sum\\rho_{ij}H2{ji}')
-    ])
-
-    tr1['mean'] = means['Trace']
-    tr1['standard error'] = np.sqrt(covariances.xs('Trace',level=1)['Trace']/nsamples)
-
-    # If requested, add the shift and traces to the final_estimates DataFrame, to be output later.
-    if options.with_shift:
-        final_estimates['Shift'] = means['Shift']
-        final_estimates['Shift s.d.'] = np.sqrt(covariances.xs('Shift',level=1)['Shift'])
-    if options.with_trace:
-        final_estimates['Trace'] = tr1['mean']
-        final_estimates['Trace s.d.'] = np.sqrt(covariances.xs('Trace',level=1)['Trace'])
-        if 'Trace_2' in columns:
-            final_estimates['Trace 2'] = means['Trace_2']
-            final_estimates['Trace 2 s.d.'] = np.sqrt(covariances.xs('Trace_2',level=1)['Trace_2'])
-
-    # Compute the mean and standard error estimates for all quantities of the form Tr(\rho O)/Tr(\rho).
-    for (k,v) in observables.items():
-        if v in columns:
-            num['mean'] = means[v]
-            num['standard error'] = np.sqrt(covariances.xs(v,level=1)[v]/nsamples)
-            cov_AB = covariances.xs('Trace',level=1)[v]
-            stats = pyblock.error.ratio(num, tr1, cov_AB, nsamples)
-
-            final_estimates[k] = stats['mean']
-            final_estimates[k+' error'] = stats['standard error']
-
-    # Compute the mean and standard error estimates for the Renyi entropy (S2), for all subsystems.
+    # Compute the mean and standard error estimates for the Renyi entropy (S2),
+    # for all subsystems.
     nrdms = 0
     for column in columns:
         have_s2 = False
-        if 'Renyi_2_numerator' in column: # RDM S2
+        if 'RDM' in column and 'S2' in column: # RDM S2
             nrdms += 1
             have_s2 = True
             num_col = column
-            tr1_col = 'RDM'+str(nrdms)+'_trace_1'
-            tr2_col = 'RDM'+str(nrdms)+'_trace_2'
+            tr1_col = 'RDM'+str(nrdms)+' trace 1'
+            tr2_col = 'RDM'+str(nrdms)+' trace 2'
             out_str = 'RDM'+str(nrdms)+' S2'
-        elif 'Full_R2_numerator' in column: # Full S2
+        elif 'Full S2' in column: # Full S2
             have_s2 = True
             num_col = column
             tr1_col = 'Trace'
-            tr2_col = 'Trace_2'
+            tr2_col = 'Trace 2'
             out_str = 'Full S2'
 
         if have_s2:
@@ -109,16 +128,16 @@ None.
             tr1['standard error'] = np.sqrt(covariances.xs(tr1_col,level=1)[tr1_col]/nsamples)
             tr2['standard error'] = np.sqrt(covariances.xs(tr2_col,level=1)[tr2_col]/nsamples)
 
-            # A denotes the numerator, B denotes the first trace and C denotes the second trace.
+            # A denotes the numerator, B denotes the first trace and C denotes
+            # the second trace.
             cov_AB = covariances.xs(num_col,level=1)[tr1_col]
             cov_AC = covariances.xs(num_col,level=1)[tr2_col]
             cov_BC = covariances.xs(tr1_col,level=1)[tr2_col]
 
-            final_estimates[out_str], final_estimates[out_str+' error'] = \
+            results[out_str], results[out_str+' error'] = \
                 calc_S2(num, tr1, tr2, cov_AB, cov_AC, cov_BC, nsamples)
 
-    return final_estimates
-
+    return results
 
 def calc_S2(stats_A, stats_B, stats_C, cov_AB, cov_AC, cov_BC, data_len):
     '''Calculate the mean and standard error of :math:`f = -log(A/BC)`.
@@ -128,20 +147,20 @@ Parameters
 stats_A : :class:`pandas.DataFrame`
     Statistics (containing at least the 'mean' and 'standard error' fields) for
     variable :math:`A`.  The rows contain different values of these statistics
-    for different Beta values.
+    for different beta values.
 stats_B : :class:`pandas.DataFrame`
     Similarly for variable :math:`B`.
 stats_C : :class:`pandas.DataFrame`
     Similarly for variable :math:`C`.
 cov_AB : :class:`pandas.Series`
-    Covariance between variables A and B.
+    Covariance between variables :math:`A` and :math:`B`.
 cov_AC : :class:`pandas.Series`
-    Covariance between variables A and C.
+    Covariance between variables :math:`A` and :math:`C`.
 cov_BC : :class:`pandas.Series`
-    Covariance between variables B and C.
+    Covariance between variables :math:`B` and :math:`C`.
 data_len : :class:`pandas.Series`
-    Number of Beta loops used to obtain the statistics given in ``stats_A``, ``stats_B``
-    and ``stats_C``.
+    Number of beta loops used to obtain the statistics given in ``stats_A``,
+    ``stats_B`` and ``stats_C``.
 
 Returns
 -------
@@ -170,8 +189,8 @@ Parameters
 column: string
     Column name of estimate for which to find a spline fit.
 estimates : :class:`pandas.DataFrame`
-    Must contain a column with the above name, and another with the
-    name column+' error'. The indices are used for the beta values.
+    Must contain a column with the above name, and another with the name
+    column+' error'. The indices are used for the beta values.
 
 Returns
 -------
@@ -186,7 +205,7 @@ spline_fit : :class:`pandas.Series`
     tck = scipy.interpolate.splrep(beta_values, values, weights, k=3)
     spline_fit = scipy.interpolate.splev(beta_values, tck)
     
-    return pd.Series(spline_fit)
+    return pd.Series(spline_fit, index=beta_values)
 
 def parse_args(args):
     '''Parse command-line arguments.
@@ -199,7 +218,7 @@ args : list of strings
 Returns
 -------
 filenames : list of strings
-    list of QMC output files
+    list of HANDE DMQMC output files.
 options : :class:`OptionParser`
     Options read in from command line.
 '''
@@ -223,6 +242,7 @@ options : :class:`OptionParser`
 
     return (filenames, options)
 
+
 def main(args):
     '''Run data analysis on finite-temperature HANDE output.
 
@@ -244,22 +264,47 @@ None.
     data.rename(columns={'iterations' : 'Beta'}, inplace=True)
     data['Beta'] = data['Beta']*tau
 
+    # Make the Beta column a MultiIndex.
     data.set_index('Beta', inplace=True, append=True)
     # The number of beta loops contributing to each beta value.
     nsamples = data['Trace'].groupby(level=2).count()
-    beta_values = pd.Series(nsamples.index)
+    beta_values = nsamples.index.values
+    # The data that we are going to use.
     estimates = data.loc[:,'Shift':'# H psips']
+    # Compute the mean of all estimates across all beta loops.
+    means = estimates.groupby(level=2).mean()
+    # Compute the covariances between all pairs of columns.
+    covariances = estimates.groupby(level=2).cov()
 
-    final_estimates = perform_averaging(beta_values, estimates, nsamples, options)
+    columns = list(estimates.columns.values)
+
+    # results will hold all of the final values to be printed.
+    results = pd.DataFrame({'Beta' : pd.Series(beta_values, index=beta_values)})
+    results = results.join(analyse_observables(means, covariances, nsamples))
+    results = results.join(analyse_renyi_entropy(means, covariances, nsamples))
+
+    # If requested, add the averaged shift profile to results.
+    if options.with_shift:
+        results['Shift'] = means['Shift']
+        results['Shift s.d.'] = np.sqrt(covariances.xs('Shift',level=1)['Shift'])
+
+    # If requested, add the averaged trace profiles to results.
+    if options.with_trace:
+        results['Trace'] = means['Trace']
+        results['Trace s.d.'] = np.sqrt(covariances.xs('Trace',level=1)['Trace'])
+        if 'Trace 2' in columns:
+            results['Trace 2'] = means['Trace 2']
+            results['Trace 2 s.d.'] = np.sqrt(covariances.xs('Trace 2',level=1)['Trace 2'])
 
     # If requested, calculate a spline fit for all mean estimates.
     if options.with_spline:
-        columns = list(final_estimates.columns.values)
-        for column in columns:
+        results_columns = list(results.columns.values)
+        for column in results_columns:
             if ('Tr[p]' in column or 'S2' in column) and (not 'error' in column):
-                final_estimates[column+' spline'] = calc_spline_fit(column, final_estimates)
+                results[column+' spline'] = calc_spline_fit(column, results)
 
-    print final_estimates.to_string()
+    # Finally, output the results!
+    print results.to_string(index=False)
 
 if __name__ == '__main__':
 
