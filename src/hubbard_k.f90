c27f9ed012200ceea77caa81c99937237513dea1
James Spencer <j.spencer@imperial.ac.uk>
2014-05-04 12:19:18 +0100

    Remove get_one_e_int_k.
    get_one_e_int_k calculated <i|h|a> for the Hubbard model in reciprocal space but it's
    completely unused as we only need the diagonal elements, which can be trivially
    accessed directly.

--- src/hubbard_k.f90
+++ src/hubbard_k.f90
@@ -8,28 +8,6 @@ implicit none
 
 contains
 
-    pure function get_one_e_int_k(phi1, phi2) result(one_e_int)
-
-        ! In:
-        !    phi1: index of a momentum-space basis function.
-        !    phi2: index of a momentum-space basis function.
-        ! Returns:
-        !    <phi1 | T | phi2> where T is the kinetic energy operator.
-
-        use basis
-
-        real(p) :: one_e_int
-        integer, intent(in) :: phi1, phi2
-
-        ! T is diagonal in the basis of momentum-space functions.
-        if (phi1 == phi2) then
-            one_e_int = basis_fns(phi1)%sp_eigv
-        else
-            one_e_int = 0.0_p
-        end if
-
-    end function get_one_e_int_k
-
     elemental function get_two_e_int_hub_k(sys, phi1, phi2, phi3, phi4) result(two_e_int)
 
         ! In:
