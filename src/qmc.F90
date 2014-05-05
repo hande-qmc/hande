fa2bc07311a6d6d2b86c655c1b417423a1955931
Fionn Malone <f.malone13@imperial.ac.uk>
2014-05-05 18:10:20 +0100

    Remove preprocessor flags around allocation of nparticles_proc.

--- src/qmc.F90
+++ src/qmc.F90
@@ -196,15 +196,9 @@ contains
         ! [review] - JSS: please check but nprocs is set to 1 if compiled without MPI.
         ! [review] - JSS: remove conditional compilation code.
         ! [reply] - FM: nprocs is set to 1, will remove.
-#ifdef PARALLEL
         ! [review] - JSS: you should be able to use this bit even without MPI.
         allocate(nparticles_proc(sampling_size, nprocs), stat=ierr)
         call check_allocate('nparticles_proc', nprocs*sampling_size, ierr)
-#else
-        ! [review] - JSS: these two lines unnecessary.
-        allocate(nparticles_proc(sampling_size, 1), stat=ierr)
-        call check_allocate('nparticles_proc', sampling_size*1, ierr)
-#endif
 
         ! Allocate spawned walker lists and spawned walker times (ct_fciqmc only)
         if (mod(spawned_walker_length, nprocs) /= 0) then
