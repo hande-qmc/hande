649f90be4cab24d59c85f217ddcc96ff4774cf7d
James Spencer <j.spencer@imperial.ac.uk>
2014-09-17 14:36:28 +0100

    Extract non-QMC data as a list of separate objects.
    This is so we can use the same framework for (e.g.) the test suite as
    we do for data analysis.

--- tools/pyhande/extract.py
+++ tools/pyhande/extract.py
@@ -35,7 +35,9 @@ See Also
     data = []
     metadata = []
     for filename in filenames:
-        (calc_metadata, calc_data) = extract_data(filename, offset, temp_file)
+        # For now, ignore output from any other calculations.
+        (calc_metadata, calc_data, junk) = extract_data(filename, offset,
+                                                        temp_file)
         data.append(calc_data)
         metadata.append(calc_metadata)
         offset += data[-1].index.levshape[0]
@@ -67,15 +69,18 @@ Returns
 metadata : :class:`pandas.Series`
     metadata (i.e. calculation information, parameters and settings) extracted
     from output file..
-data : :class:`pandas.DataFrame`
+qmc_data : :class:`pandas.DataFrame`
     HANDE QMC data.  Multiple loops over iterations (e.g. in DMQMC) are labelled
     using a hierarchical index, starting from the supplied offset, otherwise the
     entire data set is given the offset as the hierarchical index.
+calc_data : list of `:class:`pandas.Series`
+    data from other HANDE calculations (i.e. diagonalisation, Hilbert space
+    estimation).
 '''
 
     md_regex = dict(
         UUID = '^ Calculation UUID:',
-        calc_type = '^ *(fciqmc|ccmc|ifciqmc|iccmc|dmqmc|idmqmc) *$',
+        calc_type = '^ *(simple_fciqmc|fciqmc|ccmc|ifciqmc|iccmc|dmqmc|idmqmc) *$',
         sym = r'\bsym\b +\d+',
         ms = r'\bms\b +-*\d+',
         nel = r'\bnel\b|\belectrons\b',
@@ -93,7 +98,6 @@ data : :class:`pandas.DataFrame`
         target = 'varyshift_target',
         shift_damping = 'shift_damping',
         mc_cycles = 'mc_cycles',
-        hilbert_space = 'Monte-Carlo estimate of size of space is:',
         min_psips_per_mpi_process = 'Min # of particles on a processor:',
         max_psips_per_mpi_process = 'Max # of particles on a processor:',
         mean_psips_per_mpi_process = 'Mean # of particles on a processor:',
@@ -109,11 +113,11 @@ data : :class:`pandas.DataFrame`
     md_int = ['sym', 'ms', 'nel', 'nbasis', 'truncation', 'seed',
          'bit_length',  'min_dets_per_mpi_process', 'max_dets_per_mpi_process']
 
-    md_float = ['tau', 'ref_energy', 'psingle', 'pdouble', 'init_pop', 
+    md_float = ['tau', 'ref_energy', 'psingle', 'pdouble', 'init_pop',
          'min_psips_per_mpi_process', 'mean_psips_per_mpi_process',
          'mean_dets_per_mpi_process', 'mean_communication_time', 'wall_time',
          'cpu_time', 'min_communication_time', 'max_communication_time']
-    
+
     for (k,v) in md_regex.items():
         md_regex[k] = re.compile(v, re.IGNORECASE)
     input_regex = re.compile('Input options')
@@ -127,6 +131,9 @@ data : :class:`pandas.DataFrame`
     unseen_calc = True
     have_git_hash_next = False
     have_input = 0
+    calc_data = []
+    calc_titles = ['diagonalisation results', 'Hilbert space']
+    column_names = []
     for line in f:
         start_line += 1
         # extract metadata.
@@ -156,6 +163,10 @@ data : :class:`pandas.DataFrame`
                     metadata[k] = float(line.split()[7])
                 else:
                     metadata[k] = extract_last_field(line, k, md_int, md_float)
+        if any(k in line for k in calc_titles):
+            (cdata, nread) = _extract_calc_data(f, line)
+            calc_data.append(cdata)
+            start_line += nread
 
         # Hunt for start of data table.
         if ' # iterations' in line:
@@ -165,83 +176,86 @@ data : :class:`pandas.DataFrame`
             break
     f.close()
 
-    # Hunt for the end of the table.
-    skip_footer = 0
-    end_lines = _get_last_lines(filename)
-    for line in end_lines[::-1]:
-        if re.match('  *\d\d?', line):
-            break
+    if column_names:
+        # Hunt for the end of the table.
+        skip_footer = 0
+        end_lines = _get_last_lines(filename)
+        for line in end_lines[::-1]:
+            if re.match('  *\d\d?', line):
+                break
+            else:
+                skip_footer += 1
+
+        # Extract meta data from the end of the calulation.
+        for line in end_lines[-skip_footer:]:
+            for (k,v) in md_regex.items():
+                if v.search(line):
+                    metadata[k] = extract_last_field(line, k, md_int, md_float)
+
+        if float(os.path.getsize(filename))/1024 < 8000 or not temp_file:
+            # Read table --- only read the first N columns, where N is the
+            # number of column names found.
+            qmc_data = pd.io.parsers.read_table(filename, sep='\s+',
+                       engine='python', skiprows=start_line,
+                       skipfooter=skip_footer, names=column_names, comment='#')
+            # Remove comment lines and convert all columns to numeric data.
+            # Lines starting with a comment have been set to NaN in the
+            # iterations column.
+            try:
+                qmc_data.dropna(subset=['iterations'], inplace=True)
+            except TypeError:
+                # Be slightly less efficient if using pandas version < 0.13.
+                qmc_data = qmc_data.dropna(subset=['iterations'])
+            qmc_data.reset_index(drop=True, inplace=True)
         else:
-            skip_footer += 1
+            # Work around pandas slow and very memory-hungry pure-python parser
+            # by converting the data table into CSV format (and stripping out
+            # comments whilst we're at it) and reading that in.
+            tmp_csv = _convert_to_csv(filename, start_line)
+            qmc_data = pd.io.parsers.read_csv(tmp_csv, names=column_names)
+            os.remove(tmp_csv)
 
-    # Extract meta data from the end of the calulation.
-    for line in end_lines[-skip_footer:]:
-        for (k,v) in md_regex.items():
-            if v.search(line):
-                metadata[k] = extract_last_field(line, k, md_int, md_float)
+        qmc_data = qmc_data.convert_objects(convert_numeric=True, copy=False)
 
-    if float(os.path.getsize(filename))/1024 < 8000 or not temp_file:
-        # Read table --- only read the first N columns, where N is the number of
-        # column names found.
-        data = pd.io.parsers.read_table(filename, sep='\s+', engine='python',
-                   skiprows=start_line, skipfooter=skip_footer,
-                   names=column_names, comment='#')
-        # Remove comment lines and convert all columns to numeric data.
-        # Lines starting with a comment have been set to NaN in the iterations
-        # column.
-        try:
-            data.dropna(subset=['iterations'], inplace=True)
-        except TypeError:
-            # Be slightly less efficient if using pandas version < 0.13.
-            data = data.dropna(subset=['iterations'])
-        data.reset_index(drop=True, inplace=True)
+        unique_iterations = qmc_data['iterations'].unique()
+        # if repeated iterations:
+        #     assume there's a full calculation (i.e. DMQMC style) followed by
+        #     a subsequent ones (the final one might not be complete), e.g.
+        #         iteration  label_1 label_2 ...
+        #            10       ....
+        #            20       ....
+        #            ..       ....
+        #           300       ....
+        #            10       ....
+        #            20       ....
+        #            ..       ....
+        #           300       ....
+        #            10       ....
+        #            20       ....
+        #     then label each set of iterations with X.
+        # Otherwise just label the data just read in by the same X.
+        niterations = len(qmc_data)
+        nunique = len(unique_iterations)
+        nitems = int(numpy.ceil(float(niterations)/nunique))
+        qmc_data = [ qmc_data[i*nunique:(i+1)*nunique] for i in range(nitems) ]
+        multi_keys = [i+offset for i in range(len(qmc_data))]
+        qmc_data = pd.concat(qmc_data, keys=multi_keys)
+        qmc_data.index.names = ['calc', '']
+        metadata = pd.concat([metadata], keys=[multi_keys[0]])
+        metadata.index.names = ['calc', '']
+
+        # Do we have an old table?  If so, rename the headings to the new ones
+        # for convenience...
+        qmc_data.rename(inplace=True, columns={
+                'Instant shift': 'Shift',
+                '\sum H_0j Nj': '\sum H_0j N_j',
+                '# D0': 'N_0',
+                '# particles': '# H psips',
+            })
     else:
-        # Work around pandas slow and very memory-hungry pure-python parser by
-        # converting the data table into CSV format (and stripping out comments whilst we're at it)
-        # and reading that in.
-        tmp_csv = _convert_to_csv(filename, start_line)
-        data = pd.io.parsers.read_csv(tmp_csv, names=column_names)
-        os.remove(tmp_csv)
+        qmc_data = pd.DataFrame()
 
-    data = data.convert_objects(convert_numeric=True, copy=False)
-
-    unique_iterations = data['iterations'].unique()
-    # if repeated iterations:
-    #     assume there's a full calculation (i.e. DMQMC style) followed by
-    #     a subsequent ones (the final one might not be complete), e.g.
-    #         iteration  label_1 label_2 ...
-    #            10       ....
-    #            20       ....
-    #            ..       ....
-    #           300       ....
-    #            10       ....
-    #            20       ....
-    #            ..       ....
-    #           300       ....
-    #            10       ....
-    #            20       ....
-    #     then label each set of iterations with X.
-    # Otherwise just label the data just read in by the same X.
-    niterations = len(data)
-    nunique = len(unique_iterations)
-    nitems = int(numpy.ceil(float(niterations)/nunique))
-    data = [ data[i*nunique:(i+1)*nunique] for i in range(nitems) ]
-    multi_keys = [i+offset for i in range(len(data))]
-    data = pd.concat(data, keys=multi_keys)
-    data.index.names = ['calc', '']
-    metadata = pd.concat([metadata], keys=[multi_keys[0]])
-    metadata.index.names = ['calc', '']
-
-    # Do we have an old table?  If so, rename the headings to the new ones for
-    # convenience...
-    data.rename(inplace=True, columns={
-            'Instant shift': 'Shift',
-            '\sum H_0j Nj': '\sum H_0j N_j',
-            '# D0': 'N_0',
-            '# particles': '# H psips',
-        })
-
-    return (metadata, data)
+    return (metadata, qmc_data, calc_data)
 
 def extract_last_field(line, key, md_int=None, md_float=None):
     '''Extract the final field from the last (space-separated) entry in a line.
@@ -290,7 +304,7 @@ filename : string
     name of file.
 bytes : int
     number of bytes from the end of file to examine.
-    
+
 Returns
 -------
 last_lines : list
@@ -343,3 +357,63 @@ temp_file_name : string
     tmp.close()
 
     return tmp.name
+
+def _extract_calc_data(fh, line):
+    '''Extract non-QMC calculation output from a HANDE output file.
+
+Parameters
+----------
+
+fh : file handle
+    Open file handle to a HANDE output file, positioned at the start of a
+    section containing the results from a non-QMC calculation.
+line : string
+    Contents of the current line in fh.
+
+Returns
+-------
+data : `:class:`pandas.Series`
+    Calculation data found.
+nread : int
+    Number of lines read.
+'''
+    nread = 0
+    if 'Exact' in line or 'Lanczos' in line:
+        if 'Exact' in line:
+            title = 'FCI (LAPACK)'
+        elif 'Lanczos' in line:
+            title = 'FCI (Lanczos)'
+        for line in fh:
+            nread += 1
+            if 'State' in line:
+                break
+        eigvals = []
+        for line in fh:
+            nread += 1
+            if not line.split():
+                break
+            eigvals.append(float(line.split()[-1]))
+        data = pd.Series(eigvals, index=list(range(1,len(eigvals)+1)))
+        data.index.name = 'Eigenvalue'
+        data.name = title
+    elif 'Hilbert space' in line:
+        for line in fh:
+            nread += 1
+            if 'Monte-Carlo estimate of size of space is' in line:
+                if '+/-' in line:
+                    estimate = float(line.split()[-3])
+                    std_err = float(line.split()[-1])
+                    data = {'Monte Carlo estimate': estimate,
+                            'standard error': std_err}
+                else:
+                    estimate = float(line.split()[-1])
+                    data = {'Monte Carlo estimate': estimate}
+                data = pd.Series(data)
+                data.name = 'Hilbert space'
+                break
+            elif 'Size of space is' in line:
+                # Deterministic value rather than MC estimate.
+                data = pd.Series({'size': float(line.split()[-1])})
+                data.name = 'Hilbert space'
+                break
+    return (data, nread)
