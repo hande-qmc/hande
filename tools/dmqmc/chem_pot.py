bf60af8bb171707659560fa71894495f50cb4b1a
Fionn Malone <f.malone13@imperial.ac.uk>
2015-03-02 16:55:44 +0000

    Make changes to chemical potential script.

--- tools/dmqmc/chem_pot.py
+++ tools/dmqmc/chem_pot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # [review] - JSS: rename to emphasise UEG-specific?  ueg_chemical_potential.py?
 # [reply] - FDM: In principle this can be adapted for any system, all that needs to be updated
 # [reply] - is the system class a bit and the energy eigenvalues/kpoints. I just haven't done this yet
@@ -5,7 +6,6 @@
 # [review] - JSS: would be nice to meet PEP-8 standards where possible in python
 # [review] - JSS: scripts.
 # [review] - FDM: I'll try to fix some of these.
-#!/usr/bin/env python
 
 import sys
 import scipy as sc
@@ -14,9 +14,11 @@ import math
 import numpy as np
 import argparse
 
+
 class System:
     '''
-    UEG system class. Slight overkill but this code is copied from a bigger script.
+    UEG system class. Slight overkill but this code is copied from a bigger
+    script.
 
     Everything is measured in Hartree atomic units.
 
@@ -42,9 +44,11 @@ class System:
     kfac : float
         kspace grid spacing.
     spval : numpy array
-        containing single particle eigenvalues, sorted in increasing value of kinetic energy.
-    deg_e : numpy array
-        compressed list of spval containing unique eigenvalues and their degeneracy.
+        containing single particle eigenvalues, sorted in increasing value of
+        kinetic energy.
+    deg_e : list of lists
+        compressed list of spval containing unique eigenvalues and their
+        degeneracy.
     de : float
         epsilon value for comparing floats.
     '''
@@ -73,18 +77,19 @@ class System:
 
     def sp_energies(self, kfac, ecut):
         '''
-        Calculate the allowed single particle eigenvalues which can fit in the sphere in kspace determined by ecut.
+        Calculate the allowed single particle eigenvalues which can fit in the
+        sphere in kspace determined by ecut.
 
         Params
         ------
-        kfac: float
+        kfac : float
             kspace grid spacing.
-        ecut: float
+        ecut : float
             energy cutoff.
 
         Returns
         -------
-        spval: numpy array.
+        spval : list.
             list containing single particle eigenvalues.
         '''
 
@@ -111,18 +116,18 @@ class System:
 
     def compress_spval(self, spval):
         '''
-        Compress the single particle eigenvalues so that we only consider unique
-        values which vastly speeds up the k-space summations required.
+        Compress the single particle eigenvalues so that we only consider
+        unique values which vastly speeds up the k-space summations required.
         [todo] - Look at more clever optimisations (stars).
 
         Params
         ------
-        spval: list
+        spval : list
             list containing single particle eigenvalues.
 
         Returns
         -------
-        spval: numpy array.
+        spval : list of lists.
             list containing single particle eigenvalues.
         '''
 
@@ -139,14 +144,15 @@ class System:
             if eval2 == eval1:
                 j += 1
             else:
-                deg_e.append([j,eval1])
+                deg_e.append([j, eval1])
                 i += j
                 j = 1
             it += 1
 
-        deg_e.append([j,eval1])
+        deg_e.append([j, eval1])
 
         return deg_e
+
 
 def nav_sum(mu, ne, spval, beta, pol):
     '''Calculate average number of particles in the system.
@@ -160,7 +166,7 @@ ne : int
 # [review] - JSS: unclear if this is an list of lists/tuples or a numpy array,
 # [review] - JSS: based on comments here and in compress_spval.
 # [reply] - FDM: Looks like a list of lists. It probably should be a numpy array at some point.
-spval : list of floats
+spval : list of lists
     single particle energies and their degeneracies.
 beta : float
     inverse temperature.
@@ -174,7 +180,6 @@ N : float
 '''
 
     # [review] - JSS: simpler code:
-    #   N = sum(degen/np.exp(-beta*(mu-eigv)) for (degen,eigv) in spval)
     # [review] - JSS: ?
     # [review] - FDM: Yep.
     # [review] - JSS: could also investigate doing numpy array operations for
@@ -182,15 +187,15 @@ N : float
     # [reply] - FDM: Maybe in the future, speed isn't an issue at the moment.
     N = 0
 
-    for speig in range(0,len(spval)):
-
-        N = N + spval[speig][0]/(np.exp(-beta*(mu-spval[speig][1]))+1)
+    N = sum(degen/(np.exp(-beta*(mu-eigv))+1) for (degen, eigv) in spval)
 
     return (2.0/pol)*N
 
+
 def nav_diff(mu, ne, spval, beta, pol):
-    '''Calculate the difference between current estimate for N and the desired number of particles.
-Needed as a separate function for use with Scipy root finding.
+    '''Calculate the difference between current estimate for N and the desired
+    number of particles.  Needed as a separate function for use with Scipy root
+    finding.
 
 Parameters
 ----------
@@ -213,8 +218,10 @@ diff : float
 
     return nav_sum(mu, ne, spval, beta, pol) - ne
 
+
 def chem_pot_sum(system, beta):
-    '''Find the correct value of the chemical potential which results in a system of ne particles at temperature beta.
+    '''Find the correct value of the chemical potential which results in a
+    system of ne particles at temperature beta.
 
 Parameters
 ----------
@@ -225,11 +232,13 @@ beta : float
 
 Returns
 -------
-chemical potential: float
+chemical potential : float
     the chemical potential.
 '''
 
-    return sc.optimize.fsolve(nav_diff, system.ef, args=(system.ne, system.deg_e, beta, system.pol))[0]
+    return (sc.optimize.fsolve(nav_diff, system.ef, args=(system.ne,
+                               system.deg_e, beta, system.pol))[0])
+
 
 def parse_args(args):
     '''Parse command-line arguments.
@@ -245,17 +254,21 @@ args : :class:`ArgumentParser`
     Arguments read in from command line.
 '''
 
-    parser = argparse.ArgumentParser(usage = __doc__)
+    parser = argparse.ArgumentParser(usage=__doc__)
     parser.add_argument('rs', type=float, help='Wigner-Seitz radius.')
     parser.add_argument('ne', type=int, help='Number of electrons.')
-    parser.add_argument('ecutoff', type=float, help='Plane wave cutoff in units of 0.5*(2\pi/L)**2.')
-    parser.add_argument('pol', type=int, help='Polarisation, = 1 for unpolarised system and 2 for polarised system.')
+    parser.add_argument('ecutoff', type=float, help='Plane wave cutoff '
+                        'in units of 0.5*(2\pi/L)**2.')
+    parser.add_argument('pol', type=int, help='Polarisation, = 1 for '
+                        'unpolarised system and 2 for polarised system.')
     parser.add_argument('beta', type=float, help='Inverse temperature.')
-    parser.add_argument('-t','--use--fermi', action='store_true', dest='fermi_temperature',
-                      default=False, help='Interpret input beta as Beta = T_F/T')
+    parser.add_argument('-t', '--use--fermi', action='store_true',
+                        dest='fermi_temperature', default=False,
+                        help='Interpret input beta as Beta = T_F/T')
     args = parser.parse_args(args)
 
     return args
+
 
 def main(args):
     '''Calculate the chemical potential for the UEG.
@@ -270,18 +283,20 @@ Returns
 None.
 '''
     args = parse_args(args)
+
     if args.beta == 0:
         # [review] - JSS: python 2 specific.  Use print(...) instead.
-        print "beta must be greater than zero."
+        print("beta must be greater than zero.")
         sys.exit(1)
     else:
         sys_pars = [args.rs, args.ne, args.ecutoff, args.pol]
         system = System(sys_pars)
         # [review] - JSS: avoid putting code blocks on a single line.
-        if args.fermi_temperature: args.beta = args.beta / system.ef
+        if args.fermi_temperature:
+            args.beta = args.beta / system.ef
         chem_pot = chem_pot_sum(system, args.beta)
 
-    print chem_pot
+    print(chem_pot)
 
 if __name__ == '__main__':
 
