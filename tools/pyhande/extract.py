f5051cb2c24cb83a0e5c16d7543d32378f762621
Fionn Malone <f.malone13@imperial.ac.uk>
2015-03-14 20:40:04 +0000

    Add test for kinetic energy evaluation and benchmark fb72a4b.
    Needed to update pyhande to include estimate_canonical_kinetic_energy
    in calculation types.

--- tools/pyhande/extract.py
+++ tools/pyhande/extract.py
@@ -88,7 +88,8 @@ calc_data : list of `:class:`pandas.Series`
 
     # ... input (echoed in output)
     md_input = dict(
-        calc_type = '^ *(simple_fciqmc|fciqmc|ccmc|ifciqmc|iccmc|dmqmc|idmqmc) *$',
+        calc_type = '^ *(simple_fciqmc|fciqmc|ccmc|ifciqmc|iccmc|dmqmc|idmqmc|\
+                         |estimate_canonical_kinetic_energy) *$',
         sym = r'\bsym\b +\d+',
         ms = r'\bms\b +-*\d+',
         nel = r'\bnel\b|\belectrons\b',
