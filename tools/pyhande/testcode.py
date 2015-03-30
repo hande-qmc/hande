5d9030f6aa3675da3e67e6ff0399f58b9c3ed883
James Spencer <j.spencer@imperial.ac.uk>
2015-03-30 19:03:19 +0100

    Don't underscore titles if using pyhande.testcode.testcode_data.

--- tools/pyhande/testcode.py
+++ tools/pyhande/testcode.py
@@ -2,13 +2,15 @@
 
 import pyhande.extract
 
-def extract_test_data(fname):
+def extract_test_data(fname, underscore=True):
     '''Extract data from a HANDE file and select a desired subset to be tested.
 
 Parameters
 ----------
 fname : string
     filename containing HANDE calculation output.
+underscore : boolean
+    if true, replace spaces in the key names with underscores.
 
 Returns
 -------
@@ -23,7 +25,8 @@ output : dict
 
     output = {}
     if not qmc_data.empty:
-        qmc_data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+        if underscore:
+            qmc_data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
         # Compare every 1/4 of the calculation...
         indxs = [0] + [int((i*len(qmc_data))/4)-1 for i in range(1,5)]
         test_data = qmc_data.ix[indxs]
@@ -33,10 +36,12 @@ output : dict
             # Compare at most the first 5 eigenvalues (just to be quick/minimise
             # test output/handle different orders of states within Lanczos):
             select_data = calc[:min(len(calc),5)].to_frame().T
-            select_data.rename(columns=lambda x: 'eigv_%s' % (x,), inplace=True)
+            if underscore:
+                select_data.rename(columns=lambda x: 'eigv_%s' % (x,), inplace=True)
         elif 'Hilbert space' in calc.name:
             select_data = calc.to_frame().T
-            select_data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
+            if underscore:
+                select_data.rename(columns=lambda x: x.replace(' ', '_'), inplace=True)
         output[calc.name] = select_data
 
     return output
@@ -63,7 +68,7 @@ See Also
 '''
 
     test_data = {}
-    for (key, dat) in extract_test_data(fname).items():
+    for (key, dat) in extract_test_data(fname, False).items():
         dat_dict = dat.to_dict('list')
         for dat_key, dat_entry in dat_dict.items():
             if dat_key in test_data:
