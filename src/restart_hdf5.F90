bed9a9583d99a5ef4b278fc3e753fe541f677cb7
James Spencer <j.spencer@imperial.ac.uk>
2015-04-06 15:24:33 +0100

    Similar fix (to previous commit) for writing restart files.

--- src/restart_hdf5.F90
+++ src/restart_hdf5.F90
@@ -310,7 +310,7 @@ Module restart_hdf5
                         call hdf5_write(subgroup_id, dnspawn, head(0,0))
                     end associate
                 end if
-                call hdf5_write(subgroup_id, dproc_map, kinds, shape(qs%par_info%load%proc_map), qs%par_info%load%proc_map%map)
+                call hdf5_write(subgroup_id, dproc_map, kinds, shape(qs%par_info%load%proc_map%map), qs%par_info%load%proc_map%map)
 
                 ! Can't use c_loc on a assumed shape array.  It's small, so just
                 ! copy it.
