9f8e780799fe7c542f93b9a92a6eaf6990e29ace
James Spencer <j.spencer@imperial.ac.uk>
2015-04-19 23:15:22 +0100

    Correct broadcast of input data for older versions of GCC.
    Ensure that the length of the character string containing input options
    is set in older versions of GCC (ie which don't use allocatable
    character strings).  This was previously undefined in such versions,
    resulting in MPI broadcast errors.

--- src/interact.F90
+++ src/interact.F90
@@ -106,11 +106,11 @@ contains
             end do
 
 #ifdef PARALLEL
+            call mpi_bcast(buf_len, 1, MPI_INTEGER, proc, mpi_comm_world, ierr)
 #if ! defined(__GNUC__) || __GNUC__ > 4 || (__GNUC__ == 4 && (__GNUC_MINOR__ > 7))
-            call mpi_bcast(buf_len, 1, MPI_INTEGER, 0, mpi_comm_world, ierr)
             if (.not.parent) allocate(character(len=buf_len) :: buffer)
 #endif
-            call mpi_bcast(buffer, buf_len, MPI_CHARACTER, 0, mpi_comm_world, ierr)
+            call mpi_bcast(buffer, buf_len, MPI_CHARACTER, proc, mpi_comm_world, ierr)
 #endif
 
             ! Only print out the HANDE.COMM file from the parent processor;
