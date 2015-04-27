699f6ee0a8a68e07f11844c1c8877c3d3f44b8c4
Fionn Malone <f.malone13@imperial.ac.uk>
2015-04-27 15:47:31 +0100

    Add some comments + fix formatting in fthf script.

--- tools/dmqmc/analyse_canonical.py
+++ tools/dmqmc/analyse_canonical.py
@@ -12,6 +12,13 @@ import numpy as np
 
 
 def main(filename):
+    ''' Analyse the output from a canonical kinetic energy calculation.
+
+Parameters
+----------
+filename : list of strings
+        files to be analysed.
+'''
 
     if len(filename) < 1:
         print("Usage: ./analyse_canonical.py files")
@@ -50,8 +57,14 @@ def main(filename):
     results['E_HF0-Error'] = [np.sqrt(covariances['E_HF0']['E_HF0']/nsamples)]
     results['E_THF'] = list(e_thf['mean'])
     results['E_THF-Error'] = list(e_thf['standard error'])
-
-    print(results.to_string(index=False))
+    try:
+        float_fmt = '{0:-#.8e}'.format
+        float_fmt(1.0)
+    except ValueError:
+        # GAH.  Alternate formatting only added to format function after
+        # python 2.6..
+        float_fmt = '{0:-.8e}'.format
+    print(results.to_string(index=False, float_format=float_fmt))
 
 
 if __name__ == '__main__':
