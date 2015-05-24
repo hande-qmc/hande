hande-qmc/hande

commit 81aec5f2cca562b51278336e7ab709dcbbd19ba4

Also handle errors from reading input file just on parent.
(Note: this removes the accidentally introduced requirement that all
processors can open the input file, even though only the parent actually
reads it...)

src/lua_hande.F90
@@ -84,17 +84,13 @@ contains
         logical :: t_exists
 
         if (command_argument_count() > 0) then
-            ! Input file specified on the command line.
-            call get_command_argument(1, inp_file)
-            inquire(file=inp_file, exist=t_exists)
-            if (.not.t_exists) then
-                call stop_all('read_input','File does not exist:'//trim(inp_file))
-            end if
 
-            lua_state = fluL_newstate()
-            call register_lua_hande_api(lua_state)
-
+            ! Read input file on parent and broadcast to all other processors.
             if (parent) then
+                call get_command_argument(1, inp_file)
+                inquire(file=inp_file, exist=t_exists)
+                if (.not.t_exists) call stop_all('run_hande_lua','File does not exist:'//trim(inp_file))
+
                 write (6,'(a14,/,1X,13("-"),/)') 'Input options'
                 call read_file_to_buffer(buffer, inp_file)
                 write (6,'(A)') trim(buffer)
@@ -113,10 +109,12 @@ contains
 
             ! Attempt to run script.  If it fails (ie lua_err is non-zero) then try
             ! parsing it as traditional input file for now.
+            lua_state = fluL_newstate()
+            call register_lua_hande_api(lua_state)
             call open_config_file(lua_state, inp_file, lua_err, err_string)
             if (lua_err == 0) then
                 call flu_close(lua_state)
-            else
+            else if (parent) then
                 write (6,*) 'aotus/lua error code:', lua_err
                 call stop_all('run_lua_hande', trim(err_string))
             end if
@@ -125,7 +123,7 @@ contains
             call mpi_barrier(mpi_comm_world, ierr)
 #endif
 
-        else
+        else if (parent) then
             call stop_all('run_lua_hande', 'No input file supplied.')
         end if
 

commit 8db47b032601c894789104763e0678c2027ba87e

Correct parsing of the input file with MPI.
81aec5f2cc accidentally broke this due to the (mistaken) belief that
only the parent processor read the input file.  This was certainly the
intention (given the parent processor read the input file into a buffer
and broadcast it) but then we used open_config_file from AOTUS, which
reads in the input file on all processors.  This failed as the input
filename is now only defined on the parent processor.  Worse, as I had
also changed a lua error to only trigger a stop_all on the parent
processor, a deadlock resulted (as the parent processor was fine...).

The simple fix is to do what was intended: use open_config_chunk and the
(carefully constructed!) buffer containing the input file contents.

NOTE: despite the comments in 81aec5f2cc, this genuinely removes the
requirement for each processor to have access to the input file.

src/lua_hande.F90
@@ -65,7 +65,7 @@ contains
 
         ! Generic entry point from which a lua script is run.
 
-        use aotus_module, only: open_config_file
+        use aotus_module, only: open_config_chunk
         use flu_binding, only: flu_State, fluL_newstate, flu_close
 
         use errors, only: stop_all, warning
@@ -110,7 +110,7 @@ contains
             ! Attempt to run lua script.
             lua_state = fluL_newstate()
             call register_lua_hande_api(lua_state)
-            call open_config_file(lua_state, inp_file, lua_err, err_string)
+            call open_config_chunk(lua_state, buffer, lua_err, err_string)
             if (lua_err == 0) then
                 call flu_close(lua_state)
             else if (parent) then