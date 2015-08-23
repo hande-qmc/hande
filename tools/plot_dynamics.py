d74f0265ed43b713dea8439a89e03f42e5a01d33
Ruth Franklin <rstf2@cam.ac.uk>
2015-08-23 09:10:38 +0100

    Update plot_dynamics for new data extraction.

--- tools/plot_dynamics.py
+++ tools/plot_dynamics.py
@@ -12,52 +12,53 @@ import argparse
 
 def main(datafile, plotfile):
 
-    data = pyhande.extract.extract_data(datafile)[1]
-    shoulder = pyhande.analysis.plateau_estimator(data)
+    hande_out = pyhande.extract.extract_data(datafile)
+    for (metadata, data) in hande_out:
+        shoulder = pyhande.analysis.plateau_estimator(data)
 
-    # Plot the total population over the entire range
-    pyplot.subplot(3,1,1)
-    pyplot.plot(data['iterations'], data['# H psips'])
-    pyplot.xlabel('iteration')
-    pyplot.ylabel('Total Population')
+        # Plot the total population over the entire range
+        pyplot.subplot(3,1,1)
+        pyplot.plot(data['iterations'], data['# H psips'])
+        pyplot.xlabel('iteration')
+        pyplot.ylabel('Total Population')
 
-    # Plot the energy estimators over the entire range
-    pyplot.subplot(3,1,2)
-    pyplot.plot(data['iterations'], data['\sum H_0j N_j']/data['N_0'], label='Proj. Energy')
-    pyplot.plot(data['iterations'], data['Shift'], label='Shift')
-    pyplot.xlabel('iteration')
-    pyplot.ylabel('Energy / $E_{h}$')
-    pyplot.legend()
+        # Plot the energy estimators over the entire range
+        pyplot.subplot(3,1,2)
+        pyplot.plot(data['iterations'], data['\sum H_0j N_j']/data['N_0'], label='Proj. Energy')
+        pyplot.plot(data['iterations'], data['Shift'], label='Shift')
+        pyplot.xlabel('iteration')
+        pyplot.ylabel('Energy / $E_{h}$')
+        pyplot.legend()
 
-    pyplot.subplot(3,1,3)
-    # Plot the total population up to the shoulder
-    height = shoulder['mean']['shoulder height']
-    data_around_shoulder = data[np.logical_and(data['# H psips'] < 1.1*height, 
-                           data['# H psips'] > 0.9*height) ]
-    pyplot.plot(data_around_shoulder['iterations'], 
-           data_around_shoulder['# H psips'], label='Total Population')
-    x_points = [min(data_around_shoulder['iterations']), 
-                max(data_around_shoulder['iterations'])]
-    pyplot.plot(x_points, [height, height], label='Shoulder Height') 
-    pyplot.xlabel('iteration')
-    pyplot.ylabel('Population')
-    pyplot.legend(loc=2)
-    pyplot.draw()
-    if plotfile == '-':
-        pyplot.show()
-    else:
-        pyplot.savefig(plotfile)
+        pyplot.subplot(3,1,3)
+        # Plot the total population up to the shoulder
+        height = shoulder['mean']['shoulder height']
+        data_around_shoulder = data[np.logical_and(data['# H psips'] < 1.1*height, 
+                               data['# H psips'] > 0.9*height) ]
+        pyplot.plot(data_around_shoulder['iterations'], 
+               data_around_shoulder['# H psips'], label='Total Population')
+        x_points = [min(data_around_shoulder['iterations']), 
+                    max(data_around_shoulder['iterations'])]
+        pyplot.plot(x_points, [height, height], label='Shoulder Height') 
+        pyplot.xlabel('iteration')
+        pyplot.ylabel('Population')
+        pyplot.legend(loc=2)
+        pyplot.draw()
+        if plotfile == '-':
+            pyplot.show()
+        else:
+            pyplot.savefig(plotfile)
 
-    # Also print out the information about the shoulder
-    # Stealing from reblock_hande.py
-    try:
-        float_fmt = '{0:-#.8e}'.format
-        float_fmt(1.0)
-    except ValueError:
-        # GAH.  Alternate formatting only added to format function after
-        # python 2.6..
-        float_fmt = '{0:-.8e}'.format
-    print(shoulder.to_string(float_format=float_fmt, line_width=80))
+        # Also print out the information about the shoulder
+        # Stealing from reblock_hande.py
+        try:
+            float_fmt = '{0:-#.8e}'.format
+            float_fmt(1.0)
+        except ValueError:
+            # GAH.  Alternate formatting only added to format function after
+            # python 2.6..
+            float_fmt = '{0:-.8e}'.format
+        print(shoulder.to_string(float_format=float_fmt, line_width=80))
 
 def parse_args(args):
 
