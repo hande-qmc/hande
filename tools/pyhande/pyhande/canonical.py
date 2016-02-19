ab2b1903722aad983e7fdee9f5488c14000b7e70
Fionn Malone <f.malone13@imperial.ac.uk>
2016-02-19 13:18:43 +0000

    Don't bother printing out chemical potential.

--- tools/pyhande/pyhande/canonical.py
+++ tools/pyhande/pyhande/canonical.py
@@ -100,12 +100,6 @@ results : :class:`pandas.DataFrame`
     else:
         # Hope to find it in the input file...
         results['Beta'] = pyhande.legacy.extract_input(metadata, 'beta')
-    if 'chem_pot' in metadata:
-        # New, richer JSON-based metadata.
-        results['mu'] = [metadata['chem_pot']]
-    else:
-        # Hope to find it in the input file...
-        results['mu'] = pyhande.legacy.extract_input(metadata, 'chem_pot')
     # Free estimates contain no denominator so the error is
     # just the standard error.
     results['U_0'] = [means['U_0']]
