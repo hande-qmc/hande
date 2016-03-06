eeb3a6ba2875031e781a9122aceb2edaf5d6f697
James Spencer <j.spencer@imperial.ac.uk>
2016-03-06 20:56:06 +0000

    Change components of base types from allocatable to pointers.
    This allows them to be passed to C nicely and is a prelude to shared
    memory allocation.

--- lib/local/base_types.f90
+++ lib/local/base_types.f90
@@ -6,28 +6,28 @@ use const
 
 implicit none
 
-! Allocatable 1D array.  Useful for creating triangular arrays etc.
+! pointer 1D array.  Useful for creating triangular arrays etc.
 
 type alloc_int1d
-    integer, allocatable :: v(:)
+    integer, pointer :: v(:)
 end type
 
 type alloc_rp1d
-    real(p), allocatable :: v(:)
+    real(p), pointer :: v(:)
 end type
 
 type alloc_rdp1d
-    real(dp), allocatable :: v(:)
+    real(dp), pointer :: v(:)
 end type
 
-! Allocatable 2D array.  Useful for creating triangular spin arrays etc.
+! pointer 2D array.  Useful for creating triangular spin arrays etc.
 
 type alloc_int2d
-    integer, allocatable :: v(:,:)
+    integer, pointer :: v(:,:)
 end type
 
 type alloc_rp2d
-    real(p), allocatable :: v(:,:)
+    real(p), pointer :: v(:,:)
 end type
 
 contains
