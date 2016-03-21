7c1591d7b9f1b185a5b34b65bde65d752b58b789
Charles Scott <cs675@cam.ac.uk>
2016-03-21 16:31:02 +0000

    Fixed bug in quadrature initiator setting due to bit string zero-indexing.

--- src/ifciqmc.f90
+++ src/ifciqmc.f90
@@ -32,7 +32,7 @@ contains
         do i = 1, size(parent_population)
             if (.not.(abs(parent_population(i)) > initiator_pop .or. determ_flag == 0)) then
                 ! Isn't an initiator.
-                parent_flag = ibset(parent_flag, i)
+                parent_flag = ibset(parent_flag, i - 1)
             ! Otherwise has a high enough population to be an initiator in this space,
             ! or is a deterministic state (which must always be an initiator).
             end if
@@ -51,6 +51,8 @@ contains
         ! Out:
         !    parent_flag: set to 0 if the determinant is an initiator and 1 otherwise.
 
+        use const, only: p
+
         real(p), intent(in) :: parent_population(:), initiator_pop
         integer, intent(in) :: determ_flag
         integer, intent(out) :: parent_flag
@@ -58,11 +60,11 @@ contains
 
         parent_flag = 0_int_p
         do i = 1, size(parent_population), 2
-            if (.not.((sqrt(parent_population(i) ** 2 + parent_population(i+1) ** 2) > initiator_pop) .or.&
-                    (determ_flag == 0))) then
+            if ((.not. ((parent_population(i) ** 2) + (parent_population(i+1) ** 2)) > &
+                    initiator_pop ** 2) .or. (.not.(determ_flag == 0))) then
                 ! Isn't an initiator.
+                parent_flag = ibset(parent_flag, i - 1)
                 parent_flag = ibset(parent_flag, i)
-                parent_flag = ibset(parent_flag, i + 1)
             end if
             ! Otherwise has a high enough population to be an initiator in this space,
             ! or is a deterministic state (which must always be an initiator).
