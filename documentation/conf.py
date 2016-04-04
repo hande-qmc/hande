eae91eb653f1607ce6ed200c43932a2364cd1991
James Spencer <james.spencer@gmail.com>
2016-04-05 00:14:57 +0100

    Update doc copyright date.

--- documentation/conf.py
+++ documentation/conf.py
@@ -71,7 +71,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'HANDE QMC'
 
-copyright = u'2014, HANDE developers'
+copyright = u'2016, HANDE developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
