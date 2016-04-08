8bbd0291b538cc141d65d23f632c35145481b0c4
Ruth Franklin <rstf2@cam.ac.uk>
2016-04-08 16:59:53 +0100

    Development version

--- lib/local/environment_report.F90
+++ lib/local/environment_report.F90
@@ -35,7 +35,7 @@ implicit none
 character(*), parameter :: HANDE_VCS_VERSION = _VCS_VERSION
 ! HANDE version.  When tagging a commit, update this (directly on master is probably best).  In the immediate commit after the tag,
 ! append -dev to it to 're-open' the code base for further development.
-character(*), parameter :: HANDE_VERSION = '1.0'
+character(*), parameter :: HANDE_VERSION = '1.0-dev'
 
 ! Global uuid
 character(36) :: GLOBAL_UUID
