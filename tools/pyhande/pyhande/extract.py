511be018dfc7c1fd89edb7b2abe88c6f7a2c6a56
Verena Neufeld <van26@cam.ac.uk>
2016-11-21 13:54:55 +0000

    update comments

--- tools/pyhande/pyhande/extract.py
+++ tools/pyhande/pyhande/extract.py
@@ -440,7 +440,8 @@ parse_comments : boolean
 
 .. note::
 
-    We assume the table finishes at the next blank line.
+    We assume the table finishes at the next blank line or before a comment
+    that does not start with a hash key.
 
 Returns
 -------
