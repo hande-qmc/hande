d6f1ffbfc547e89a6c58252b094e4e50d72e3e11
Ruth Franklin <rstf2@cam.ac.uk>
2017-07-11 15:24:00 +0100

    Fix for compatibility with pandas 0.20

--- tools/reblock_hande.py
+++ tools/reblock_hande.py
@@ -198,7 +198,11 @@ reblock_plot : string
     filename for the reblock convergence plot output.
 '''
 
-    cols = pd.util.terminal.get_terminal_size()[0]
+    try:
+        cols = pd.util.terminal.get_terminal_size()[0]
+    except AttributeError:
+        # terminal module moved in pandas 0.20
+        cols = pd.io.formats.terminal.get_terminal_size()[0]
     if not sys.stdout.isatty():
         cols = -1
 
