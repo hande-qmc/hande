7cb9eb04ec0fc7dabd40de0d6f578f84e3db2b27
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-01-09 14:58:23 +0100

    Add default compiler flags

---
+++ cmake/custom/compilers/CFlags.cmake
@@ -0,0 +1,3 @@
+include(GNU.C)
+include(Clang.C)
+include(Intel.C)
---
+++ cmake/custom/compilers/GNU.C.cmake
@@ -0,0 +1,7 @@
+if(NOT DEFINED ENV{CFLAGS})
+  if(CMAKE_C_COMPILER_ID MATCHES GNU)
+    set(CMAKE_C_FLAGS         "${CMAKE_C_FLAGS} -std=c99 -DRESTRICT=restrict -DFUNDERSCORE=1 -fPIC")
+    set(CMAKE_C_FLAGS_DEBUG   "-O0 -g3 -DDEBUG -Wall -Wextra -Winit-self -Wuninitialized -Wmissing-declarations -Wwrite-strings -Wno-sign-compare")
+    set(CMAKE_C_FLAGS_RELEASE "-O3 -DNDEBUG")
+  endif()
+endif()
