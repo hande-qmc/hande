bd506912c56da0d8dbf33d4df4d4dc5cff048021
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-01-19 15:13:43 +0100

    Update dev environment

--- .default.nix
+++ .default.nix
@@ -4,39 +4,52 @@ let
     owner = "NixOS";
     repo = "nixpkgs-channels";
     rev = "nixos-unstable";
-    sha256 = "0r92p2blm9k2dhy5xv41k9asqf9xfd6if4c801ab1wjzy8wsf1wi";
+    sha256 = "15fcl29a97f68j1pjywmrjm31rdh1a21jz9airlsbzpl4lc3zhfi";
   });
 in
   with import nixpkgs {
     overlays = [(self: super:
-    {
-      hdf5 = super.hdf5.override {
-        gfortran = super.gfortran;
-        mpi = super.openmpi;
-      };
-    }
+      {
+        hdf5 = super.hdf5.override {
+          gfortran = super.gfortran;
+          mpi = super.openmpi;
+        };
+        python3 = super.python3.override {
+          packageOverrides = py-self: py-super: {
+            matplotlib = py-super.matplotlib.override {
+              enableTk = true;
+              enableQt = true;
+            };
+          };
+        };
+      }
     )];
   };
   stdenv.mkDerivation {
     name = "HANDE";
     buildInputs = [
-      atlas
       cmake
       exa
       gcc
       gdb
       gfortran
+      gfortran.cc.lib
       hdf5
       liblapack
       libuuid
       lua5_3
       openmpi
+      python3Full
       python3Packages.matplotlib
       python3Packages.numpy
       python3Packages.pandas
       python3Packages.pyyaml
-      python3Packages.virtualenvwrapper
       python3Packages.scipy
+      stdenv
       valgrind
     ];
+    src = null;
+    shellHook = ''
+    SOURCE_DATE_EPOCH=$(date +%s)
+    '';
   }
