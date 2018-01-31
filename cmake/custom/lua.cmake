439a99534cbf687800b0963e60da90c5a56adb7d
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-01-31 17:02:14 +0100

    Use CMAKE_PREFIX_PATH in Lua detection

--- cmake/custom/lua.cmake
+++ cmake/custom/lua.cmake
@@ -11,4 +11,8 @@
 #   docopt: "--lua=<LUA_ROOT> Specify the path to the Lua installation to use [default: '']."
 #   define: "'-DLUA_ROOT=\"{0}\"'.format(arguments['--lua'])"
 
+option_with_default(LUA_ROOT "Specify the path to the Lua installation to use" "")
+if(LUA_ROOT)
+  list(APPEND CMAKE_PREFIX_PATH ${LUA_ROOT})
+endif()
 find_package(Lua 5.3 EXACT REQUIRED)
