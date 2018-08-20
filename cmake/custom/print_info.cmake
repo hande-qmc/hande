2549db6316ce38a6eb347162f3403d9063785fbc
Roberto Di Remigio <roberto.diremigio@gmail.com>
2018-08-20 17:38:11 -0400

    Generate print_info.c and git_info.c in build dir
    In this way the two build systems do not get confused.
    print_info.cmake now handles properly depedencies. That is,
    whenever the template files git_info.f90.in and print_info.c.in are
    modified, the corresponding .f90 and .c files are regenerated
    and trigger a recompilation.

--- cmake/custom/print_info.cmake
+++ cmake/custom/print_info.cmake
@@ -9,34 +9,32 @@ function(generate_info_header)
     set(_mpi_launcher ${MPIEXEC})
   endif()
 
-  file(COPY ${PROJECT_SOURCE_DIR}/tools/configurator.py DESTINATION ${CMAKE_CURRENT_BINARY_DIR})
-  set(_config_script
-"
-import os, sys
-sys.path.append('${CMAKE_CURRENT_BINARY_DIR}')
+  file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/config_script.py
+"import os, sys
+sys.path.append('${PROJECT_SOURCE_DIR}/tools')
 import configurator as cf
 conf_dict = cf.prepare_configuration_dictionary(cmake_version='${CMAKE_VERSION}',
-            cmake_generator='${CMAKE_GENERATOR}',
+            cmake_generator='${CMAKE_GENERATOR}', 
             Fortran_compiler='${CMAKE_Fortran_COMPILER}',
             C_compiler='${CMAKE_C_COMPILER}',
             build_type='${CMAKE_BUILD_TYPE}',
             mpi_launcher='${_mpi_launcher}',
             lua_version='${LUA_VERSION_STRING}',
-            hdf5_version='${HDF5_VERSION}')
-cf.configure_file(conf_dict, 'print_info.c', in_path=os.path.join('${PROJECT_SOURCE_DIR}', 'lib/local'), suffix='.in')
-cf.configure_file(conf_dict, 'git_info.f90', in_path=os.path.join('${PROJECT_SOURCE_DIR}', 'lib/local'), suffix='.in')
+hdf5_version='${HDF5_VERSION}')
+cf.configure_file(conf_dict, 'print_info.c', in_path='${CMAKE_CURRENT_SOURCE_DIR}', out_path='${CMAKE_CURRENT_BINARY_DIR}', suffix='.in')
+cf.configure_file(conf_dict, 'git_info.f90', in_path='${CMAKE_CURRENT_SOURCE_DIR}', out_path='${CMAKE_CURRENT_BINARY_DIR}', suffix='.in')
 ")
 
-  execute_process(
+  add_custom_command(
+    OUTPUT
+      print_info.c
+      git_info.f90
     COMMAND
-    ${PYTHON_EXECUTABLE} "-c" ${_config_script}
-    )
-
-  add_custom_target(
-    build_info
-    ALL DEPENDS
-      ${CMAKE_CURRENT_BINARY_DIR}/configurator.py
-      ${PROJECT_SOURCE_DIR}/lib/local/print_info.c
-      ${PROJECT_SOURCE_DIR}/lib/local/git_info.f90
+      ${PYTHON_EXECUTABLE} config_script.py
+    WORKING_DIRECTORY
+      ${CMAKE_CURRENT_BINARY_DIR}
+    DEPENDS
+      ${CMAKE_CURRENT_SOURCE_DIR}/print_info.c.in
+      ${CMAKE_CURRENT_SOURCE_DIR}/git_info.f90.in
     )
 endfunction()
