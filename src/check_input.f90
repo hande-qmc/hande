b9727876a2eba0f3942f46663a892e72ec35f87e
Andreea Filip <maf63@cam.ac.uk>
2019-10-23 09:17:37 +0100

    Check for UCC input

--- src/check_input.f90
+++ src/check_input.f90
@@ -399,6 +399,55 @@ contains
 
     end subroutine check_dmqmc_opts
 
+    subroutine check_uccmc_opts(sys, uccmc_in, qmc_in)
+
+        ! Check the UCCMC input options
+
+        ! In:
+        !   sys: system being studied.
+        !   uccmc_in: UCCMC options
+        !   qmc_in: QMC options
+
+
+        use qmc_data, only: uccmc_in_t, qmc_in_t
+        use qmc_data, only: excit_gen_no_renorm, excit_gen_renorm
+        use system, only: sys_t, read_in
+        use errors, only: stop_all
+
+        type(sys_t), intent(in) :: sys
+        type(uccmc_in_t), intent(in) :: uccmc_in
+        type(qmc_in_t), intent(in) :: qmc_in
+
+        character(*), parameter :: this = 'check_uccmc_opts'
+
+        if (uccmc_in%move_freq >= 32) then
+            call stop_all(this, "move_frequency must be less than 32")
+        else if (uccmc_in%move_freq < 0) then
+            call stop_all(this, "move_frequency must be non-negative")
+        end if
+
+        if (uccmc_in%cluster_multispawn_threshold <= 0) then
+            call stop_all(this, "cluster_multispawn_threshold must be positive")
+        end if
+
+        if (uccmc_in%density_matrices .and. sys%system /= read_in) then
+            call stop_all(this, "CCMC density matrices not implemented for this system type.")
+        end if
+
+        if (sys%read_in%comp .and. uccmc_in%linked) call stop_all(this, 'Linked complex CCMC not yet implemented')
+        if (sys%read_in%comp .and. uccmc_in%density_matrices) &
+                call stop_all(this, 'RDMS not yet implemented for complex CCMC.')
+
+        if (sys%basis%info_string_len /= 0) call stop_all(this, &
+            'Additional space allocated in bit strings for no reason. Something has gone wrong.')
+
+        if ((uccmc_in%linked) .and. .not.((qmc_in%excit_gen == excit_gen_no_renorm) .or. &
+                                         (qmc_in%excit_gen == excit_gen_renorm))) then
+            call stop_all(this, 'Excitation Generators other than no_renorm and renorm not yet implemented for linked CCMC.')
+        end if
+
+    end subroutine check_uccmc_opts
+
     subroutine check_ccmc_opts(sys, ccmc_in, qmc_in)
 
         ! Check the CCMC input options
