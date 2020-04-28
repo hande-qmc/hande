f3ab46a052b3bbb54edc57dd838071c337d3d2e3
Verena Neufeld <van26@cam.ac.uk>
2020-04-28 16:34:21 +0200

    pandas Series no longer has .as_matrix()

--- tools/pyhande/pyhande/lazy.py
+++ tools/pyhande/pyhande/lazy.py
@@ -139,7 +139,7 @@ info : :func:`collections.namedtuple`
     #list = calc_tr['Proj. Energy'].as_matrix()
     #n_data = len(list)
     
-    list_org = calc_tr['Proj. Energy'].as_matrix()
+    list_org = calc_tr['Proj. Energy'].values
     n_data = len(list_org) // batch_size
     list = [0]*n_data
     for i in range(n_data):
