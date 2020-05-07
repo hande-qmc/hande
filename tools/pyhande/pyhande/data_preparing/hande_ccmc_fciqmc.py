d8f492241a208068c8ef82c48c98dae93ae8d565
Verena Neufeld <veri7neuf@gmail.com>
2020-05-07 15:15:20 +0200

    first try with replica tricks

--- tools/pyhande/pyhande/data_preparing/hande_ccmc_fciqmc.py
+++ tools/pyhande/pyhande/data_preparing/hande_ccmc_fciqmc.py
@@ -90,7 +90,26 @@ class PrepareHandeCcmcFciqmc(AbsDataPreparator):
             print(self._not_extracted_message)
             raise
 
-    def _gen_complex_cols(self, col_key: str) -> Dict[str, str]:
+    @staticmethod
+    def _gen_replica_col(col_key: str, replica_id: int) -> str:
+        """Takes column key and replica_id and creates replica column.
+
+        Parameters
+        ----------
+        col_key : str
+            Name of column.
+        replica_id : int
+            ID of replica, i.e. first one has 0, next 1, etc.
+
+        Returns
+        -------
+        str
+            transformed `col_key` for that replica.
+        """
+        return col_key+"_"+str(replica_id)
+
+    @staticmethod
+    def _gen_complex_cols(col_key: str) -> Dict[str, str]:
         """Parses columns key in, returns real and imag col keys.
 
         Parameters
@@ -105,7 +124,78 @@ class PrepareHandeCcmcFciqmc(AbsDataPreparator):
         """
         return {'real': 'Re{'+col_key+'}', 'imag': 'Im{'+col_key+'}'}
 
-    def _add_complex_obs(self):
+    def _add_replica_grouping(self, max_replica_id: int) -> None:
+        """Add extra column for replica id which can be grouped by.
+
+        Parameters
+        ----------
+        max_replica_id : int
+            Highest id of replica.  Assume that replica ids are
+            consecutive.  With two replicas, ids are 1 and 2, so
+            `max_replica_id` would be 2.
+        """
+        for i in range(len(self.data)):
+            replica_dats = []
+            for replica_id in range(1, max_replica_id+1):
+                dat = pd.concat([
+                    pd.DataFrame({
+                        'replica id': len(self.data[i])*[replica_id]
+                    }), self.data[i]
+                ], axis=1)
+                cols_to_drop = [
+                    self._gen_replica_col(col_name, rep_id)
+                    for rep_id in list(range(1, max_replica_id+1))
+                    for _, col_name in self.observables.items()
+                    if (self._gen_replica_col(col_name, rep_id) in dat and
+                        rep_id != replica_id)
+                ]
+                dat.drop(columns=cols_to_drop, inplace=True)
+                dat.rename(
+                    columns={
+                        self._gen_replica_col(col_name, replica_id): col_name
+                        for _, col_name in self.observables.items()
+                    }, inplace=True
+                )
+                replica_dats.append(dat)
+            self._data[i] = pd.concat(replica_dats, ignore_index=True)
+
+    def _check_add_replica(self) -> None:
+        """Check if replica exist, if yes, add replica col and group.
+
+        Replica columns contain the index of the replica (see
+        `gen_replica_col`).  Here, reduce this column back to original
+        form but each row will only contain one's replica's observables.
+        That replica is identified in the extra replica column.
+
+        i.e. df with two columns `cols_1` and `cols_2` and one row
+        gets changed to df with two columns `replica id` and `cols` with
+        two rows, one for each of the two replicas.
+        """
+        # Assume `ref_key` would be affected and assume
+        # Then add extra column for i so that replicas can be groupedby.
+        if any(self._gen_replica_col(self.observables['ref_key'], 1)
+               in dat for dat in self.data):
+            # Find highest replica_id.
+            max_id = 0
+            while (any(
+                self._gen_replica_col(self.observables['ref_key'], max_id+1)
+                in dat for dat in self.data
+            )):
+                max_id += 1
+            # Check that this highest replica_id exist in all data.
+            if not all(
+                    self._gen_replica_col(self.observables['ref_key'],
+                                          max_id) in dat for dat in self.data
+            ):
+                raise ValueError(f"Some but not all data have {max_id} "
+                                 "replicas! Make sure that number of replicas "
+                                 "is identical!")
+            self._add_replica_grouping(max_id)
+            self._replica_data = True
+        else:
+            self._replica_data = False
+
+    def _add_complex_obs(self) -> None:
         """Add (neg) magnitude of ref and sum to data.
 
         Take magnitude of real and imaginary part of `sum_key` and
@@ -148,7 +238,31 @@ class PrepareHandeCcmcFciqmc(AbsDataPreparator):
         self._observables['sum_key'] = mag_sum_key
         self._observables['ref_key'] = mag_ref_key
 
-    def _add_inst_proje(self):
+    def _check_add_complex(self) -> None:
+        """Check whether complex, if yes, add magnitude.
+
+        Complex calculations have ref and sum columns in Re{key},
+        Im{key} format. If complex, take (negative) magnitudes and add
+        those as columns to each self._data elements respectively.
+        """
+        if any(self._gen_complex_cols(self.observables['ref_key'])['real']
+               in dat for dat in self.data):
+            # Probably complex calculations!
+            # Test that all are complex.
+            if not all(
+                    self._gen_complex_cols(self.observables['ref_key'])['real']
+                    in dat for dat in self.data
+            ):
+                raise ValueError("Some but not all data is complex! Either "
+                                 "pass data where all calculations are either "
+                                 "complex or not complex.")
+            self._add_complex_obs()
+            self._complex_data = True
+        else:
+            # Not complex.
+            self._complex_data = False
+
+    def _add_inst_proje(self) -> None:
         """Add inst. projected energy, maybe required for analysis.
 
         Note that its mean is not the true mean of the projected energy
@@ -177,24 +291,16 @@ class PrepareHandeCcmcFciqmc(AbsDataPreparator):
         """
         self._data = copy.deepcopy(data) if make_copy else data
 
-        # Complex?
-        if (self._gen_complex_cols(self.observables['ref_key'])['real']
-                in data[0]):
-            # Probably complex calculations!
-            # Test that all are complex.
-            if not all(
-                    self._gen_complex_cols(self.observables['ref_key'])['real']
-                    in dat for dat in data
-            ):
-                raise ValueError("Some but not all data is complex! Either "
-                                 "pass data where all calculations are either "
-                                 "complex or not complex.")
-            self._add_complex_obs()
-            self._complex_data = True
-        else:
-            # Not complex.
-            self._complex_data = False
+        # Replicas?
+        # i.e. do we have some repeated columns, same name expect for
+        # "_i" at the end where i is in list(range(1,#replicas+1))?
+        self._check_add_replica()
 
-        # Calculate instantaneous projected energy, required by some
-        # analysis schemes (e.g. hybrid).
+        # Complex?
+        # i.e. are the ref and sum columns in Re{key}, Im{key}?
+        # Then take (negative) magnitudes and add those as columns.
+        self._check_add_complex()
+
+        # Calculate instantaneous projected energy, ratio of sum to key
+        # columns, required by some analysis schemes (e.g. hybrid).
         self._add_inst_proje()
