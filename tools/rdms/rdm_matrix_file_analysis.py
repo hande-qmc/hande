5110fb7fcc9dc4560d58066326f4f6bf21ed797d
William Van Benschoten <vanbenschoten@uiowa.edu>
2022-05-19 00:13:04 -0500

    commented and refined RDM matrix averaging

--- tools/rdms/rdm_matrix_file_analysis.py
+++ tools/rdms/rdm_matrix_file_analysis.py
@@ -2,22 +2,25 @@
 ''' rdm_energy_estimate.py [options] RDM_1 RDM_2 ... RDM_N
 
 Average together RDM files produced during an FCIQMC calculation and
-perform additional analysis such as calculating the energy estimate from 
+perform additional analysis such as calculating the energy estimate from
 the RDM profile.
 '''
 
+import os
 import sys
 import pkgutil
 import argparse
-import numpy as np
+import pandas as pd
 
 try:
     from pyhande.extract import extract_data
+    from pyblock.error import ratio
 except ModuleNotFoundError:
     _script_dir = os.path.dirname(os.path.abspath(__file__))
     if not pkgutil.find_loader('pyhande'):
         sys.path.append(os.path.join(_script_dir, '../pyhande'))
     from pyhande.extract import extract_data
+    from pyblock.error import ratio
 
 
 def parse_arguments(arguments):
@@ -42,9 +45,6 @@ def parse_arguments(arguments):
 
     parser = argparse.ArgumentParser(usage=__doc__)
 
-    parser.add_argument('-o', '--output', default='csv',
-                        choices=['csv', 'txt'], help='Format for data table. '
-                        '  Default: %(default)s.')
     parser.add_argument('-mf', '--metadata_file', action='store', default=None,
                         type=str, dest='metadata_file', help='Define a file '
                         'name for which metadata related to the calculation '
@@ -71,20 +71,6 @@ def parse_arguments(arguments):
                         'the occupied orbitals used to define the reference '
                         'of the system. Required when using an occupation '
                         'vector different than the auto-filled N orbitals.')
-    parser.add_argument('-of', '--output_file', action='store', default=None,
-                        type=str, dest='output_file', help='Define a file '
-                        'name to store the results of averaging to.')
-    parser.add_argument('-ff', '--float-format', action='store',
-                        default=None, type=str, dest='float_format',
-                        help='Format the values from the resulting analysis '
-                        'before reporting, only used for the "txt" format. '
-                        'I.E., %%6.4f, %%12.8E, ..., etc.')
-    parser.add_argument('-tol', '--tolerance-number', action='store',
-                        default=None, type=int, dest='csv_tol', help='Set the '
-                        'tolerance of the reported data from analysis. For '
-                        'example, 12 would round the final data report '
-                        '(after analysis) to the 12th decimal place before '
-                        'reporting. Only used for the "csv" format.')
     parser.add_argument('filenames', nargs='+', help='HANDE files to analyse.')
     parser.parse_args(args=None if arguments else ['--help'])
 
@@ -94,6 +80,41 @@ def parse_arguments(arguments):
 
 
 def read_dump_file(dump):
+    ''' A very basic function for reading and storing the integrals from an
+    FCIDUMP. We assume that the format for the FCIDUMP file follows that of:
+    https://hande.readthedocs.io/en/latest/manual/integrals.html#fcidump-format
+
+    Warning we just store each spin symmetry so this can be very memory hungry.
+
+    Parameters
+    ----------
+    dump : str
+        The filename of the FCIDUMP to read in
+        system information and integrals.
+
+    Returns
+    -------
+    integrals : dictionary
+        The integrals for the system, we store each spin symmetry in
+        physicists notation.
+    nel : int
+        The number of electrons in the system.
+    norb : int
+        The number of spin orbitals in the system.
+    uhf : boolean
+        True if the integrals correspond to an unrestricted Hartree--Fock
+        calculations.
+
+    Raises
+    ------
+    RuntimeError
+        If the integrals are found to be in the form z = x + iy.
+    RuntimeError
+        If the FCIDUMP file has the integrals and orbital indices contained
+        on multiple lines.
+    RuntimeError
+        If one of nel or norb where not identified.
+    '''
 
     integrals = {}
     nel = None
@@ -122,7 +143,18 @@ def read_dump_file(dump):
                 if 'UHF' in line:
                     uhf = 'TRUE' in line
             elif header_complete:
+                if '(' in line and ')' in line:
+                    raise RuntimeError('Complex integral values are not '
+                                       'supported, please send patches!')
+
                 line_data = line.split()
+
+                if len(line_data) != 5:
+                    raise RuntimeError('Format of FCIDUMP file is not '
+                                       'recognized, please read the '
+                                       'documentation for a format outline '
+                                       'or send patches!')
+
                 i, a, j, b = (int(rhf_fac*int(k)) for k in line_data[1:])
                 integrals[i, j, a, b] = float(line_data[0])
 
@@ -137,15 +169,29 @@ def read_dump_file(dump):
                 elif i != 0:
                     integrals[i-1, j, a, b] = integrals[i, j, a, b]
 
+    if nel is None or norb is None:
+        raise RuntimeError('The number of electrons or number of orbitals '
+                           'was not found in the FCIDUMP, send patches!')
+
     return integrals, nel, norb, uhf
 
 
 def read_density_matrix_file(rdm):
+    ''' Read in the RDM file produces from an FCIQMC calculation and store
+    the values in a dictionary indexed by orbitals.
 
+    Parameters
+    ----------
+    rdm : str
+        A file containing the RDM values.
+
+    Returns
+    -------
+    gamma : dictionary
+        A dictionary with the RDM values with the spin orbitals as the keys.
+    '''
     gamma = {}
-
     with open(rdm, 'r') as stream:
-
         for line in stream:
             line_data = line.split()
             i, j, a, b = (int(k) for k in line_data[:-1])
@@ -156,7 +202,22 @@ def read_density_matrix_file(rdm):
 
 
 def orbital_index_from_pair_index(index_pair):
+    ''' A python version of the orbs_from_index function within HANDE.
+    For more information, see `orbs_from_index` in: lib/local/utils.F90
+    We always return the smallest index first.
 
+    Parameters
+    ----------
+    index_pair : int
+        A single index representing a pair of orbitals.
+
+    Returns
+    -------
+    index_one : int
+        The first index of the pair of orbitals.
+    index_two : int
+        The second index of the pair of orbitals.
+    '''
     index_one = int(1.5 + (2*index_pair-1.75)**0.5)
     index_two = index_pair - int(int((index_one-1)*(index_one-2))/2)
 
@@ -166,37 +227,74 @@ def orbital_index_from_pair_index(index_pair):
     return index_one, index_two
 
 
-def get_two_particle(integrals, i, j, k, l, bcomplex):
+def get_two_particle(integrals, i, j, a, b, bcomplex):
+    ''' Permute through the orbital symmetries for the given system to
+    identify a non-zero value within the given array. If no value is found
+    to be within, zero is returned.
 
-    # http://vergil.chemistry.gatech.edu/notes/permsymm/permsymm.html
+    For the permutational symmetries see:
+    1) http://vergil.chemistry.gatech.edu/notes/permsymm/permsymm.html
+    2) `read_in_integrals` in: src/read_in.F90
 
+    Parameters
+    ----------
+    integrals : dictionary
+        The tensor array being searched for non-zero values given some
+        pair of spin orbitals.
+    i, j, a, b : int
+        The spin orbitals
+    bcomplex : boolean
+        True if the integrals have a 4-permutation symmetry.
+
+    Returns
+    -------
+    float
+        Zero if no value is found, otherwise the non-zero value
+        corresponding to the 4 orbitals provided is returned.
+    '''
     if not bcomplex:
         symmetries = [
-                [i, j, k, l],
-                [j, i, l, k],
-                [k, l, i, j],
-                [l, k, j, i],
-                [k, j, i, l],
-                [l, i, j, k],
-                [i, l, k, j],
-                [j, k, l, i],
+                [i, j, a, b],
+                [j, i, b, a],
+                [a, b, i, j],
+                [b, a, j, i],
+                [a, j, i, b],
+                [b, i, j, a],
+                [i, b, a, j],
+                [j, a, b, i],
             ]
     else:
         symmetries = [
-                [i, j, k, l],
-                [j, i, l, k],
-                [k, l, i, j],
-                [l, k, j, i],
+                [i, j, a, b],
+                [j, i, b, a],
+                [a, b, i, j],
+                [b, a, j, i],
             ]
 
-    for ii, jj, kk, ll in symmetries:
-        if (ii, jj, kk, ll) in integrals.keys():
-            return integrals[ii, jj, kk, ll]
+    for ii, jj, aa, bb in symmetries:
+        if (ii, jj, aa, bb) in integrals.keys():
+            return integrals[ii, jj, aa, bb]
 
     return 0.0
 
 
 def search_twofold(integrals, i, a):
+    ''' Search the rank-4 dictionary for two free orbital indices corresponding
+    to a non-zero value.
+
+    Parameters
+    ----------
+    integrals : dictionary
+        A rank 4 dictionary containing the integral values for the system.
+    i, a : int
+        Orbital indices to permute.
+
+    Returns
+    -------
+    float
+        Zero if no value is found, otherwise the non-zero value
+        corresponding to the 2 orbitals provided is returned.
+    '''
 
     if (i, 0, a, 0) in integrals.keys():
         return integrals[i, 0, a, 0]
@@ -207,6 +305,30 @@ def search_twofold(integrals, i, a):
 
 
 def get_one_particle(integrals, a, b, Nfrozen, bcomplex):
+    ''' Finds a non-zero one-particle integral within a rank 4 dictionary.
+    If a CAS space is being used, calculate the appropriate one-particle
+    integral given the CAS space.
+
+    For more information on the one-particle calculation given a CAS space,
+    see comments within `read_in_integrals` in: src/read_in.F90
+
+    Parameters
+    ----------
+    integrals : dictionary
+        A rank 4 dictionary containing the integral values for the system.
+    a, b : int
+        Orbital indices corresponding to the one-particle integral.
+    Nfrozen : int
+        The number of frozen orbitals for the CAS, zero if not CAS is being
+        used in the calculation.
+    bcomplex : boolean
+        True if the integrals have a 4-permutation symmetry.
+
+    Returns
+    -------
+    hab : float
+        The one-particle Hamiltonian element.
+    '''
 
     hab = search_twofold(integrals, a, b)
 
@@ -221,7 +343,36 @@ def get_one_particle(integrals, a, b, Nfrozen, bcomplex):
 
 
 def calculate_density_matrix_energy(gamma, integrals, nel, norb, Mfrozen,
-                                    det, bcomplex, error=False):
+                                    det, bcomplex):
+    ''' Calculate the numerator and denominator of the energy expression
+    from the RDM file. This is simply a python implementation of the
+    `calc_rdm_energy` function in: src/replica_rdm.F90
+
+    Parameters
+    ----------
+    gamma : dictionary
+        A dictionary with the RDM values with the spin orbitals as the keys.
+    integrals : dictionary
+        A rank 4 dictionary containing the integral values for the system.
+    nel : int
+        The number of electrons in the system.
+    norb : int
+        The number of spin orbitals in the system.
+    Nfrozen : int
+        The number of frozen orbitals for the CAS, zero if not CAS is being
+        used in the calculation.
+    det : list of int
+        The occupied spin orbitals for the system.
+    bcomplex : boolean
+        True if the integrals have a 4-permutation symmetry.
+
+    Returns
+    -------
+    numerator : float
+        The numerator for the energy expression from the RDM.
+    trace : float
+        The denominator for the energy expression from the RDM.
+    '''
 
     numerator = 0.0
     trace = 0.0
@@ -299,6 +450,73 @@ def calculate_density_matrix_energy(gamma, integrals, nel, norb, Mfrozen,
     return numerator, trace
 
 
+def stdout_average_gamma(gamma):
+    ''' Calculate the average RDM matrix elements and the standard error
+    given a data frame containing matrix elements sampled in different
+    FCIQMC simulations. Report the average and error along with the spin
+    orbital indices for the RDM matrix element to stdout.
+
+    Parameters
+    ----------
+    gamma : :class:`pandas.DatFrame`
+        A data frame containing the 4 spin orbitals labeling the
+        matrix elements of the RDM, along with the matrix element sampled
+        in an FCIQMC simulation.
+
+    Returns
+    -------
+    None.
+    '''
+    grp = gamma.groupby('ijab')
+    out = grp.mean()
+    out['gamma_error'] = grp.sem()['gamma']
+
+    print()
+    print('Averaged RDM profile:')
+    print(f' {"i":>5} {"j":>5} {"a":>5} {"b":>5} '
+          f'{"gamma_kl":>24} {"gamma_kl_error":>24}')
+    for ijab in out.index:
+        ave, err = out.loc[ijab, 'gamma'], out.loc[ijab, 'gamma_error']
+        i, j, a, b = ijab.split()
+        print(f' {i:>5} {j:>5} {a:>5} {b:>5} {ave:> 24.12E} {err:>24.12E}')
+
+
+def stdout_average_energy(estimates):
+    ''' Calculate the average energy estimate from the RDM files produced
+    during an FCIQMC simulation. For more information, see the DMQMC analysis
+    function `analyse_observables` within: tools/pyhande/pyhande/dmqmc.py.
+    Report the final energy by printing to stdout.
+
+    Parameters
+    ----------
+    estimates : :class:`pandas.DatFrame`
+        A data frame containing the numerators and denominators calculated
+        with the read in RDM files.
+
+    Returns
+    -------
+    None.
+    '''
+    df = pd.DataFrame(estimates)
+    ave = df.mean()
+    sem = df.sem()
+    cov = df.cov()
+    N = df.count()['trace']
+
+    num = pd.DataFrame()
+    num['mean'] = [ave['numerator']]
+    num['standard error'] = [sem['numerator']]
+    tr = pd.DataFrame()
+    tr['mean'] = [ave['trace']]
+    tr['standard error'] = [sem['trace']]
+    covAB = cov.loc['numerator', 'trace']
+
+    energy = ratio(num, tr, covAB, N)
+    print()
+    print('Energy estimate from the averaged RDM profile:')
+    print(energy.to_string(float_format='%16.12E'))
+
+
 def main(arguments):
     ''' Run data analysis to produce an estimate of the energy from
     the RDM numerator and RDM denominator.
@@ -334,18 +552,7 @@ def main(arguments):
             path += s + '/'
         int_file = path + int_file
 
-    gamma_all = {}
-
-    for rdm in filenames:
-        integrals, nel, norb, uhf = read_dump_file(int_file)
-
-        gamma = read_density_matrix_file(rdm)
-
-        for k, v in gamma.items():
-            if k not in gamma_all.keys():
-                gamma_all[k] = []
-
-            gamma_all[k].append(v)
+    integrals, nel, norb, uhf = read_dump_file(int_file)
 
     if cas[0] > 0:
         N = cas[1]
@@ -354,28 +561,28 @@ def main(arguments):
         N = nel
         Mfrozen = 0
 
-    gamma_ave = {}
-    gamma_err = {}
+    gamma = {'ijab': [], 'gamma': []}
+    estimates = {'numerator': [], 'trace': []}
 
-    for k, v in gamma_all.items():
-        gamma_ave[k] = np.mean(v)
-        gamma_err[k] = (np.std(v)/np.sqrt(len(v)-1))**2.0
+    for rdm in filenames:
 
-    numerator, trace = calculate_density_matrix_energy(gamma_ave, integrals,
-                                                       N, norb, Mfrozen, det,
-                                                       bcomplex)
+        tmp_gamma = read_density_matrix_file(rdm)
 
-    sem_nume, sem_tr = calculate_density_matrix_energy(gamma_err, integrals,
-                                                       N, norb, Mfrozen, det,
-                                                       bcomplex)
+        numerator, trace = calculate_density_matrix_energy(
+                                    tmp_gamma, integrals, N, norb,
+                                    Mfrozen, det, bcomplex,
+                                )
 
-    sem_nume = abs(sem_nume)
+        estimates['numerator'].append(numerator)
+        estimates['trace'].append(trace)
 
-    energy = numerator/trace
-    error = ((sem_nume**0.5/numerator)**2 + (sem_tr**0.5/trace)**2)**0.5
-    error *= energy
+        for ijab, gamma_ijab in tmp_gamma.items():
+            i, j, a, b = ijab
+            gamma['ijab'].append(f'{i} {j} {a} {b}')
+            gamma['gamma'].append(gamma_ijab)
 
-    print(f' E_RDM: {energy:>18.12E} +/- {error:>18.12E}')
+    stdout_average_gamma(pd.DataFrame(gamma))
+    stdout_average_energy(pd.DataFrame(estimates))
 
 
 if __name__ == '__main__':
